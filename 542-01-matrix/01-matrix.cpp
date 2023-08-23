667ef37938fcf0ae97d680a14292eb9387abbb7a
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-08-23 21:33:03 +0530

    Time: 67 ms (50.54%) | Memory: 34.4 MB (29.59%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      vector<vector<int>> dist(mat.size(), vector<int>(mat[0].size(), -1));
-        queue<pair<int, int>> q;
-        for(int i=0;i<mat.size();i++) {
-            for(int j=0;j<mat[0].size();j++) {
-                if(mat[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({i, j});
-                }
-            }
-        }
-        int d = 0;
-        vector<vector<int>> dir({{-1,0}, {0, 1}, {1, 0}, {0, -1}});
-        while(!q.empty()) {
-            int n = q.size();
-            while(n--) {
-                auto &i = q.front();
-                for(int j=0;j<4;j++) {
-                    int x = i.first + dir[j][0];
-                    int y = i.second + dir[j][1];
-
-                    if(x >= 0 && y >= 0 && x < mat.size() && y < mat[0].size() && dist[x][y] == -1) {
-                        dist[x][y] = d+1;
-                        q.push({x, y});
-                    }
-                }
-                q.pop();
-            }
-            d++;
-        }
-
-        return dist;   
+    int r=mat.size();
+	  int c=mat[0].size();
+	  queue<pair<pair<int,int>,int>> q; //row,col,step
+	  vector<vector<int>> vis(r,vector<int>(c,0));
+	  vector<vector<int>> dist(r,vector<int>(c,0));
+	  
+	  for(int i=0;i<r;i++){
+	      for(int j=0;j<c;j++){
+	        if(mat[i][j]==0){
+	            q.push({{i,j},0});
+	            vis[i][j]=1;
+	        }}}
+	  int dx[]={-1,0,1,0};
+	  int dy[]={0,1,0,-1};
+	  while(!q.empty()){
+	    auto i=q.front();
+	    q.pop();
+	    int row=i.first.first;
+	    int col=i.first.second;
+	    int steps=i.second;
+	    dist[row][col]=steps;
+	    for(int i=0;i<4;i++){
+	        int new_r=row+dx[i];
+	        int new_c=col+dy[i];
+	        if(new_r>=0 && new_r<r && new_c>=0 && new_c<c && vis[new_r][new_c]==0){
+	         vis[new_r][new_c]=1;
+	         q.push({{new_r,new_c},steps+1});
+	        }
+	    }
+	  }
+	  return dist;
     }
 };
