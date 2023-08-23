c4da95251f3cce24997c66002c3cebe1bb851c31
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-08-23 11:04:33 +0530

    Time: 3 ms (51.26%) | Memory: 6.2 MB (46.07%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
     string reorganizeString(string s) {
+      unordered_map<char,int> map;
+      int n=s.length();
       int max_freq=0;
       char max_freq_char;
-      int n=s.length();
-      unordered_map<char,int> map;
       for(int i=0;i<n;i++){
-       map[s[i]]++;
-       if (map[s[i]]>max_freq){
-        max_freq=map[s[i]];
-        max_freq_char=s[i];
-       }}
+        map[s[i]]++;
+        if(max_freq<map[s[i]]){
+          max_freq=map[s[i]];
+          max_freq_char=s[i];
+        }
+      }
 
-      if (max_freq>((n+1)/2)){return "";}
-      
-      int even_index=0,odd_index=1;
-      for(even_index=0;even_index<n;even_index+=2){
-       if(max_freq >0){
-          s[even_index]=max_freq_char;
-          max_freq-=1;}
-      else{break;}
+      if(max_freq>((n+1)/2)){
+        return "";
       }
       
-      //put at even position if left else put at odd position
-     unordered_map<char,int> ::iterator it=map.begin();
-     while(it !=map.end()){
-      if(it->first !=max_freq_char){
-        while(it->second !=0){
-           if(even_index<n){
-             s[even_index]=it->first;
-             even_index+=2;}
-           else{
-             s[odd_index]=it->first;
-             odd_index+=2;}
-           it->second--;}
+      int even=0;
+      int odd=1;
+      for(even=0;even<n;even+=2){
+        if(max_freq>0)
+         {s[even]=max_freq_char;
+         max_freq-=1;}
+        else
+         {break;}
       }
-      it++;}
-      
-     return s;   
+
+      unordered_map<char,int> :: iterator it=map.begin();
+      while (it != map.end()){
+       if(it->first !=max_freq_char){
+        while(it ->second !=0){
+         if(even<n){
+           s[even]=it->first;
+           even+=2;}
+         else{
+           s[odd]=it->first;
+           odd+=2;}
+         it->second--;}}
+        it++;
+      }
+      return s;
     }
 };
