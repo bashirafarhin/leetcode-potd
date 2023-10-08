9f2c05501322f2753b8a91d4dd17a31397ed5998
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-10-08 13:02:25 +0530

    Time: 32 ms (41.55%) | Memory: 19.6 MB (15.61%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,14 +1,24 @@
 class Solution {
-    void dfs(int r,int c,vector<vector<int>> &visited,vector<vector<char>>& grid){
+    void bfs(int R,int C,vector<vector<int>> &visited,vector<vector<char>>& grid){
      int n=grid.size();
      int m=grid[0].size();
-     if(r<0 || r==n || c<0 || c==m || visited[r][c]==1 || grid[r][c]=='0'){return ;}
-     int dx[]={-1,0,1,0};
-     int dy[]={0,1,0,-1};
-     visited[r][c]=1;
-     for(int i=0;i<4;i++){
-         dfs(r+dx[i],c+dy[i],visited,grid);
-     }
+     queue<pair<int,int>> q;
+     q.push({R,C});
+     while(!q.empty()){
+      int r=q.front().first;
+      int c=q.front().second;
+      q.pop();
+      int dx[]={-1,0,1,0};
+      int dy[]={0,1,0,-1};
+      for(int i=0;i<4;i++){
+          int new_row=r+dx[i];
+          int new_col=c+dy[i];
+       if(new_row>=0 && new_row<n && new_col>=0 && new_col<m && !visited[new_row][new_col] && grid[new_row][new_col]=='1'){
+         q.push({new_row,new_col});
+         visited[new_row][new_col]=1;
+       }
+      }
+    }
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
@@ -20,7 +30,7 @@ public:
          for(int j=0;j<m;j++){
              if(!visited[i][j] &&  grid[i][j]=='1'){
                  ans++;
-                 dfs(i,j,visited,grid);
+                 bfs(i,j,visited,grid);
              }
          }
      }
