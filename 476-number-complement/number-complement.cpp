0ba241ebc0a8eae60e22b0a805a49b430dc305a4
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-13 22:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (79.65%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int findComplement(int num) {
-     int pos=0,i=0;
+     int pos=0;
      int n=num;
      while(n !=0){
-         if(n & 1){pos=i;}
-         i++;n>>=1;
+         pos++;n>>=1;
      }
      int result=0;
-     i=0;
-     while(pos>=0){
+     int i=0;
+     while(pos>0){
      if(!(num & 1)){result+=1<<i;}
      num>>=1;
      i++;
