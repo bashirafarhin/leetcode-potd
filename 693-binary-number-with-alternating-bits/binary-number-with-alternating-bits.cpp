a2aae42b72d1ec2f59e039902f2133b7280207cb
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-14 11:15:50 +0530

    Time: 2 ms (43.07%) | Memory: 6.3 MB (43.61%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -4,9 +4,7 @@ public:
      int flag=n & 1;
      while (n !=0 && flag==(n & 1)){
          flag=1-flag;
-         n=n>>1;
-     }
-     if (n !=0){return false;}
-     return true; 
+         n=n>>1;}
+     return n==0; 
     }
 };
