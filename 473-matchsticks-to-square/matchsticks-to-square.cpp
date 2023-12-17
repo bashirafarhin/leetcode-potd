b8b2af7714886cb38dbb9ee0775ec30856cbf434
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-17 18:55:32 +0530

    Time: 499 ms (14.61%) | Memory: 87.9 MB (5.03%) - LeetSync

---
+++ 473-matchsticks-to-square/matchsticks-to-square.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool f(vector<int>& arr,vector<int> &flagarr,int i,int sum,int times,int side){
+     if(sum==0){
+        if(times==0){return true;}
+         vector<int> arr1,arr2;
+         for(int i=0;i<arr.size()-1;i++){
+          if(flagarr[i]){arr1.push_back(arr[i]);}
+          else{arr2.push_back(arr[i]);}}
+          vector<int> flagarr1(arr1.size(),0);
+          vector<int> flagarr2(arr2.size(),0);
+          
+      return f(arr1,flagarr1,arr1.size()-1,side,times-1,side) && f(arr2,flagarr2,arr2.size()-1,side,times-1,side);
+      }
+
+
+     if(i<0 || sum<0){return false;}
+
+     bool take=false;
+
+     flagarr[i]=1;
+     take=f(arr,flagarr,i-1,sum-arr[i],times,side);
+     flagarr[i]=0;
+     bool nottake=f(arr,flagarr,i-1,sum,times,side);
+     return take || nottake;
+
+    }
+public:
+    bool makesquare(vector<int>& matchsticks) {
+    int n=matchsticks.size();
+    if(n<4){return false;}
+    int sum=0;
+    for(int num : matchsticks){sum+=num;}
+    if(sum%4 !=0){return false;}
+    vector<int>flagarr(n,0);
+    return f(matchsticks,flagarr,n-1,sum/2,1,sum/4);
+   
+    }
+};
