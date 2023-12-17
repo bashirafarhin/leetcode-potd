0a4bf32befab5d116c030071de436afd239da1c6
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-17 13:40:47 +0530

    Time: 411 ms (66.99%) | Memory: 155.7 MB (95.22%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,24 +1,21 @@
 class FoodRatings {
-    unordered_map< string,int >foodrating;
-    unordered_map< string,string>foodcuisine;
+    unordered_map< string,pair<int,string>>foodratingncuisine;
     unordered_map< string, set<pair<int,string>> >Cuisine;
 public:
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         for(int i=0;i<foods.size();i++){
            Cuisine[cuisines[i]].insert({-ratings[i],foods[i]});
-           foodrating[foods[i]]=ratings[i];
-           foodcuisine[foods[i]]=cuisines[i];
+           foodratingncuisine[foods[i]]={ratings[i],cuisines[i]};
         }
 
     }
     
     void changeRating(string food, int newRating) {
-        int oldrating=-1*foodrating[food];
-        string cuisine=foodcuisine[food];
-        auto it=Cuisine[cuisine].find({oldrating,food});
-        Cuisine[cuisine].erase(it);
+        int oldrating=foodratingncuisine[food].first;
+        string cuisine=foodratingncuisine[food].second;
+        Cuisine[cuisine].erase({-oldrating,food});
         Cuisine[cuisine].insert({-newRating,food});
-        foodrating[food]=newRating;
+        foodratingncuisine[food]={newRating,cuisine};
     }
     
     string highestRated(string cuisine) {
