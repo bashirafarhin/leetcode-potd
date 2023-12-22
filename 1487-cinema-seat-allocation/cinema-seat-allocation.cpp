6ecc1d774cfb1a833e52e8770c59dad1364f2ad5
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-22 14:20:04 +0530

    Time: 181 ms (56.16%) | Memory: 62.8 MB (63.66%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-    //max 2 families can be seated in a row if whole row is empty
-    unordered_map<int,vector<int>> mp;
-    int len=reservedSeats.size();
-    for(int i=0;i<len;i++){
-        mp[reservedSeats[i][0]].push_back(reservedSeats[i][1]);}
-    int ans=(n-mp.size())*2;
-    for(auto it : mp){
-            vector<int> contains(11,0);
-            for(auto col : it.second){contains[col]=1;}
-            int flag1=0;
-            int flag2=0;
-            if(!contains[2] && !contains[3] && !contains[4] && !contains[5]){ans++;flag1=1;}
-            if(!flag1 && !contains[4] && !contains[5] && !contains[6] && !contains[7]){ans++;flag2=1;}
-            if(!flag2 && !contains[6] && !contains[7] && !contains[8] && !contains[9]){ans++;}
-            }
-            //doing this as 2 families can seated only if seated in 2345 6789
-    return ans;
+     unordered_map<int,int> mp;
+     for(auto it : reservedSeats){
+         int row=it[0];
+         int col=it[1];
+         if(mp.find(row)==mp.end()){ mp[row]=0;}
+         mp[row]=mp[row] | (1<<col);} //marking 1 for the reserved seats
+     int ans=(n-mp.size())*2; //maximum 2 families can be put empty row;
+     for(auto it : mp){
+         int seat=it.second;
+         bool flag=true;
+         if((seat & 0b0000111100)==0 )//2345
+          {ans+=1; flag=false;}
+         if( (seat & 1111000000 )==0 )//6789
+          {ans+=1; flag=false;}
+        
+         if(flag && ((seat & 0b0011110000)==0) )//4567
+          {ans+=1;}
+     }
+     return ans;
     }
 };
+
+
