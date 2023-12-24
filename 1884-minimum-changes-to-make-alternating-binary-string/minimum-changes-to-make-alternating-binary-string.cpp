a7a09ce2746f012ae7968d7e14455cb6c97b1e61
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-24 10:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (86.65%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int minOperations(string s) {
     int n=s.length();
-    int c1=0,c2=0;
+    int evenPosZ=0;   //zero at even position
+    int oddPosZ=0;   //zero at odd position
+    int totalEvenPos= (n % 2)==0? n/2 : n/2+1;   //total even position
+    int totalOddPos=n-totalEvenPos;   //total odd position
     for(int i=0;i<n;i++){
-        if( !(i & 1) && s[i]=='1'){c1++;}
-        else if( (i & 1) && s[i]=='0'){c1++;}
+        if( (i & 1) && s[i]=='0'){oddPosZ++;}
+        else if( !(i & 1) && s[i]=='0'){evenPosZ++;}
     }
-    for(int i=0;i<n;i++){
-        if( !(i & 1) && s[i]=='0'){c2++;}
-        else if( (i & 1) && s[i]=='1'){c2++;}
-    }
-    return min(c1,c2);
+    int ans1=totalEvenPos-evenPosZ+oddPosZ;   //010101.....
+    int ans2=evenPosZ+totalOddPos-oddPosZ;   //101010.....
+    return min(ans1,ans2);
     }
 };
