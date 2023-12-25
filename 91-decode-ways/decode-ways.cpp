e761d5e922d14e2c7f019c4de8c6f2452922f0a3
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-25 11:39:18 +0530

    Time: 2 ms (50.15%) | Memory: 7.9 MB (18.34%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,16 +1,12 @@
 class Solution {
     int recur(int i,string s,vector<int>&dp){
         if(s[i]=='0'){return 0;}
-        if(i==s.length() || i==s.length()-1){return 1;}
-        
+        if(i==s.length()){return 1;}
         if(dp[i] !=-1){return dp[i];}
-        int ans1=recur(i+1,s,dp);
-        int ans2=0;
-        int num=(s[i]-'0')*10+(s[i+1]-'0');
-        if(num>0 && num<27)
-         {ans2=recur(i+2,s,dp);}
-         dp[i]=ans1+ans2;
-        return ans1+ans2;
+        dp[i]=recur(i+1,s,dp);
+        if(i+1<s.length() && (s[i]=='1' || s[i]=='2' && s[i+1]<='6'))
+         {dp[i]+=recur(i+2,s,dp);}
+        return dp[i];
     }
 public:
     int numDecodings(string s) {
