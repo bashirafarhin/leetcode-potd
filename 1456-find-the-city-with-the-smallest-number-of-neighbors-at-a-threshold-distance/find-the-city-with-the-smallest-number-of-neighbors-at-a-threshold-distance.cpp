457eb17b8b5d8b6ebd3b5ef4abe0f2183ed9c2de
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-26 21:30:28 +0530

    Time: 59 ms (83.88%) | Memory: 13.6 MB (28.42%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,50 +1,58 @@
 class Solution {
-    int dijkstra(int V, vector<vector<pair<int,int>>> adj, int S,int distanceThreshold)
-    {priority_queue<pair<int,int>,vector<pair<int,int>> ,greater<pair<int,int>>> pq;
-     vector<int> dist(V,1e9);
-     dist[S]=0;
-     pq.push({dist[S],S}); // dist,node
-     while(!pq.empty()){
-         int d=pq.top().first;
-         int s=pq.top().second;
-         pq.pop();
-         for(auto it : adj[s]){
-             int v=it.first;
-             int wt=it.second;
-             if(dist[v] >wt+d){
-                 dist[v]=wt+d;
-                 pq.push({dist[v],v});
-             }
-             
-         }
-     }
-     int ans=0;
-     for(int i=0;i<V;i++){
-         if(dist[i]<=distanceThreshold){ans++;}
-     }
-     return ans;
-    }
+     vector<vector<int>> shortest_distance(vector<vector<int>>&matrix){
+	 int n=matrix.size();
+	 vector<vector<int>> graph(n,vector<int>(n));
+	 
+	 for(int i=0;i<n;i++){
+	     for(int j=0;j<n;j++){
+	         if(matrix[i][j]==-1)
+	          {graph[i][j]=1e9;}
+	         else{
+	             graph[i][j]=matrix[i][j];}}}
+	  for(int src=0;src<n;src++){
+	    for(int row=0;row<n;row++){
+	        for(int col=0;col<n;col++){
+	            if(row !=col && row !=src && col !=src){
+	                if(graph[row][col]>graph[row][src]+graph[src][col]){
+	                    graph[row][col]=graph[row][src]+graph[src][col];
+	                }
+	            }
+	        }
+	    }
+	  }
+	  for(int i=0;i<n;i++){
+	     for(int j=0;j<n;j++){
+	         if(graph[i][j]==1e9)
+	          {matrix[i][j]=-1;}
+	         else{
+	             matrix[i][j]=graph[i][j];}}
+	  }
+      return  matrix;
+	 }
+
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-      vector<vector<pair<int,int>>> adj(n);
-        for(int i=0;i<edges.size();i++){
-          int u=edges[i][0];
-          int v=edges[i][1];
-          int wt=edges[i][2];
-          adj[u].push_back({v,wt});
-          adj[v].push_back({u,wt});}
-        
-        vector<int> arr(n,0);
-        for(int i=0;i<n;i++){
-         arr[i]=dijkstra(n,adj,i,distanceThreshold);}
-         
-        int ans=0;
-        int numCities=109;
-        for(int i=0;i<n;i++){
-         if(numCities>=arr[i]){
-             ans=i;
-             numCities=arr[i];}
+    vector<vector<int>> matrix(n,vector<int>(n,-1));
+    for(auto edge : edges){
+        if(edge[0]==edge[1]){matrix[edge[0]][edge[0]]=0;}
+        else{
+        matrix[edge[0]][edge[1]]=edge[2];
+        matrix[edge[1]][edge[0]]=edge[2];}}
+    
+    matrix=shortest_distance(matrix);
+    int ans=0;
+    int minCities=1e9;
+    for(int i=0;i<n;i++){
+        int numCities=0;
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]<=distanceThreshold){numCities++;}
         }
-        return  ans;   
+
+        if(minCities>=numCities){
+            ans=i;
+            minCities=numCities;
+        }
+    }
+     return ans;
     }
 };
