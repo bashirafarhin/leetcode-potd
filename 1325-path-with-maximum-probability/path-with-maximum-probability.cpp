d51d615d77e5b2b2c34b5d7ee805f2fc0e53e450
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-26 17:16:14 +0530

    Time: 151 ms (64.87%) | Memory: 66.8 MB (57.20%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -10,23 +10,23 @@ public:
           adj[u].push_back({v,p});
           adj[v].push_back({u,p});
         }
-        //Dijikstra algo , just taking maximum edge prob insetaed minimum
+        //Dijikstra algo , just taking maximum edge prob instead minimum
         priority_queue<pair<double,int>>pq;
-        // priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq;
+        vector<double> dist(n,0);
         pq.push({1,start_node});
-        vector<int>vis(n,0);
+        dist[start_node]=1;
         while(!pq.empty()){
             double curr_wt=pq.top().first;
             int node =pq.top().second;
             pq.pop();
-            vis[node]=1;
-            if(node==end_node){return curr_wt;}
             for(auto it : adj[node]){
                 int neigh=it.first;
                 double wt=it.second;
-                if(!vis[neigh]){pq.push({curr_wt*wt,neigh});}
+                if(dist[neigh]<dist[node]*wt)
+                { dist[neigh]=dist[node]*wt;
+                 pq.push({dist[node]*wt,neigh});}
             }
         }
-        return 0;
+        return dist[end_node];
     }
 };
