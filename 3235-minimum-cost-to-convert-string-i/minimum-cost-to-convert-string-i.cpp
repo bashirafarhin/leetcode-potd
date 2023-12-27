abef50e420bcb488a420a09c8f4a46891032f709
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-27 19:06:18 +0530

    Time: 295 ms (51.73%) | Memory: 95.1 MB (38.46%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,16 +1,8 @@
 class Solution {
 
 public:
-    vector<vector<int>> shortest_distance(vector<vector<int>>&matrix){
-	 int n=matrix.size();
-	 vector<vector<int>> graph(n,vector<int>(n));
-	 
-	 for(int i=0;i<n;i++){
-	     for(int j=0;j<n;j++){
-	         if(matrix[i][j]==-1)
-	          {graph[i][j]=1e9;}
-	         else{
-	             graph[i][j]=matrix[i][j];}}}
+    vector<vector<int>> shortest_distance(vector<vector<int>>&graph){
+	 int n=graph.size();
 	  for(int src=0;src<n;src++){
 	    for(int row=0;row<n;row++){
 	        for(int col=0;col<n;col++){
@@ -22,21 +14,14 @@ public:
 	        }
 	    }
 	  }
-	  for(int i=0;i<n;i++){
-	     for(int j=0;j<n;j++){
-	         if(graph[i][j]==1e9)
-	          {matrix[i][j]=-1;}
-	         else{
-	             matrix[i][j]=graph[i][j];}}
-	  }
-      return matrix;
+      return graph;
 	 }
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         //make directed graph
-        vector<vector<int>> graph(26,vector<int>(26,-1));
+        vector<vector<int>> graph(26,vector<int>(26,1e8));
         int len=original.size();
         for(int i=0;i<len;i++){
-            if(graph[original[i]-'a'][changed[i]-'a']==-1)
+            if(graph[original[i]-'a'][changed[i]-'a']==1e8)
               {graph[original[i]-'a'][changed[i]-'a']=cost[i];}
             else{
             graph[original[i]-'a'][changed[i]-'a']=min(cost[i],graph[original[i]-'a'][changed[i]-'a'] );}
@@ -49,7 +34,7 @@ public:
         int n=source.size();
         for(int i=0;i<n;i++){
             if(source[i] !=target[i]){
-             if(graph[source[i]-'a'][target[i]-'a'] ==-1){return -1;}
+             if(graph[source[i]-'a'][target[i]-'a'] ==1e8){return -1;}
              ans+=graph[source[i]-'a'][target[i]-'a'];
             }
         }
