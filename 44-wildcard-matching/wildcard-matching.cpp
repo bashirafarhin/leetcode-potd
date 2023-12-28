966a8130f2defdd197bbcfaacdfe4854ab152227
bashirafarhin <97461439+1231bashira1321@users.noreply.github.com>
2023-12-28 10:05:36 +0530

    Time: 864 ms (5.00%) | Memory: 27.6 MB (46.92%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,59 @@
+class Solution {
+    // bool func(int i,int j,string p,string s,vector<vector<int>>&dp){
+    //     // if(i==p.length() && j==s.length()){return true;}
+        // if(j==s.length()){
+        //     for(int x=i;x<p.length();x++){
+        //         if(p[x] !='*'){return false;}
+        //     }
+        //     return true;
+        // }
+        // if(i==p.length() || j==s.length()){return false;}
+        // if(dp[i][j] !=-1){return dp[i][j];}
+    //     if( (p[i]==s[j]) || (p[i]=='?') ){
+    //         return dp[i][j]=func(i+1,j+1,p,s,dp);
+    //     }
+    //     if(p[i]=='*'){
+    //     for(int k=j;k<=s.length();k++){
+    //         if(func(i+1,k,p,s,dp)){return dp[i][j]=true;}
+    //     }}
+    //     return dp[i][j]=false;
+    // }
+public:
+    bool isMatch(string s, string p) {
+
+        int ls=s.length();
+        int lp=p.length();
+        vector<vector<int>> dp(lp+1,vector<int>(ls+1,false));
+
+
+        for(int i=lp;i>=0;i--){
+            for(int j=ls;j>=0;j--){
+
+             if(i==lp && j==ls){
+                 dp[i][j]=true;}
+             
+             else if(j==ls){
+               int x=i;
+               for( x=i;x<lp;x++){
+                 if(p[x] !='*'){
+                  break;}
+                  }
+               if(x==lp) {dp[i][j]=true;}}
+
+             else if( (p[i]==s[j]) || (p[i]=='?') ){
+               dp[i][j]=dp[i+1][j+1];}
+
+             else if(p[i]=='*'){
+               for(int k=j;k<=s.length();k++){
+                if(dp[i+1][k]){
+                    dp[i][j]=true;
+                    break;}}
+               }
+            }
+        }
+
+
+
+        return dp[0][0];
+    }
+};
