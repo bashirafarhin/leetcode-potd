b17c9d3c796ed041a18c98727d5b0ec455378942
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2023-12-31 10:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        unordered_map<char,int> mp;
-        int n=s.length();
-        int ans=-1;
-        for(int i=0;i<n;i++){
-          if(mp.count(s[i]) !=0){
-              ans=max(ans,i-mp[s[i]]-1);
+      int arr[26];
+      memset(arr,-1,sizeof(arr));
+      int n=s.length();
+      int ans=-1;
+      for(int i=0;i<n;i++){
+          int index=s[i]-'a';
+          if(arr[index]==-1){ //if char not seen before put in array its index
+            arr[index]=i;
           } else {
-              mp[s[i]]=i;
+            ans=max(ans,i-arr[index]-1); //if char is seen before find distance between its previous index and current index
           }
-        }
-        return ans;
+      }
+      return ans;
     }
 };
