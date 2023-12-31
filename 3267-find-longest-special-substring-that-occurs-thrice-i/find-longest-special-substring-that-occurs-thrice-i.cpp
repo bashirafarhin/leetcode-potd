4b5c6bbfc645e466e78336cbde10b191b4296f23
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2023-12-31 11:48:25 +0530

    Time: 14 ms (20.00%) | Memory: 10 MB (80.00%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -10,17 +10,12 @@ public:
             if(j !=i && (s[i] !=s[j]) ){break;}
             mp[subStr]++;
        }}
-        int ans=-1;
+        int ans=0;
         for(auto it : mp){
-            if(it.second <3){continue;}
-            string str=it.first;
-            bool flag=true;
-            int n=str.length();
-            for(int i=0;i<n;i++){
-                if(str[i] !=str[0]){flag=false;break;}
-            }
-            if(flag && ans<n){ans=n;}
+            if( (it.second>2) && (ans < it.first.length()) ){
+                ans=it.first.length();}
         }
+        if(ans==0){return -1;}
         return ans;
     }
 };
