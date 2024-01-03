dee9277256ae25bb2334932a330338518024f273
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-04 00:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (19.61%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,31 +1,14 @@
 class Solution {
-    bool ispar(string x){
-        stack<char> s;
-         for(int i=0;i<x.length();i++){
-           if(x[i]=='('){
-            s.push(x[i]);
-           }  else if (!s.empty() && (x[i] == ')' && s.top() == '(')) {
-            s.pop();
-            } else {
-                return false; }
-        }
-    return s.empty();
-    }
-
-
     void solve(int open,int close,vector<string> &ans,string s,int n){
      if(open==n && close==n){
-         if(ispar(s)){ //check if isvalid parenthesis or not
              ans.push_back(s);}
-         return ;
-     }
-     
+
      if(open !=n){
          s+='(';
          solve(open+1,close,ans,s,n);
          s.pop_back();
      }
-     if(open !=0 && close !=n){
+     if(open>close){
          s+=')';
          solve(open,close+1,ans,s,n);
          s.pop_back();
