8bfd1c5ff960c0de91ebb99c54c222ead55857cf
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-03 21:35:49 +0530

    Time: 357 ms (16.97%) | Memory: 160.5 MB (11.26%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,26 +1,29 @@
 class Solution {
-    int func(string s,unordered_map<string,int>&map,int index,vector<int> &dp){
-        if(index>=s.size()){return 0;}
-        if(dp[index]!=-1){return dp[index];}
-        int count=0,ans=s.size();
-        string currstr="";
-        for(int i=index;i<s.length();i++){
-            currstr.push_back(s[i]);
-            if(map.find(currstr)==map.end()){
-                count=currstr.length()+func(s,map,i+1,dp);
-            }
-            else{
-                count=func(s,map,i+1,dp); 
-            }
-            ans=min(ans,count);
-        }
-        return dp[index]=ans;
+    int solve(string s,unordered_set<string>& dict,int i,vector<int>& dp){
+        if(i==s.length()){return 0;}
+        if(dp[i] !=-1){return dp[i];}
+        int n=s.length();
+        int minextrachar=n;
+        string currWord="";
+        for(int j=i;j<n;j++){
+         currWord+=s[j];
+         if(dict.find(currWord) != dict.end()){
+            minextrachar=min( minextrachar , solve(s,dict,j+1,dp));
+          } else {
+            int val=currWord.length()+solve(s,dict,j+1,dp);
+            minextrachar=min( minextrachar , val );}
+          }
+
+        return dp[i]=minextrachar;
+        
     }
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
-        unordered_map<string,int> map;
-        for(string word : dictionary){map[word]++;}
-        vector<int> dp(s.size(),-1);
-        return func(s,map,0,dp);
+       unordered_set<string> dict;
+       for(auto word : dictionary){
+           dict.insert(word);
+       }
+       vector<int> dp(s.length(),-1);
+       return solve(s,dict,0,dp); 
     }
 };
