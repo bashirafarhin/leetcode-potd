06a03cbf90db6607428f7fbf54e62687abbbb10e
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-03 18:40:11 +0530

    Time: 2 ms (46.82%) | Memory: 6.4 MB (44.88%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -3,13 +3,17 @@ public:
     int tribonacci(int n) {
         if(n<=1){return n;}
         if(n==2){return 1;}
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+    
+        int prev1=0;
+        int prev2=1;
+        int prev3=1;
+        int curr=0;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+           curr=prev1+prev2+prev3;
+           prev1=prev2;
+           prev2=prev3;
+           prev3=curr;
         }
-        return dp[n];
+        return curr;
     }
 };
