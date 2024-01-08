3231301dbdda24078663c1f750877f224890de72
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-08 22:35:28 +0530

    Time: 3 ms (78.13%) | Memory: 18.1 MB (25.28%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,33 +1,49 @@
 class Solution {
-public:
-    int solve(string A,string B,string C,int a,int b,vector<vector<int>> &dp){
-    
-    if(a==A.length()){
-     while(b<B.length()){
-      if(C[a+b] !=B[b]){return 0;}
-      b++;}
-     return 1;}
-    if(b==B.length()){
-     while(a<A.length()){
-      if(C[a+b] !=A[a]){return 0;}
-      a++;}
-     return 1;}
-    
-    int x=0,y=0;
-    if(dp[a][b]==-1){
-    if(C[a+b]==A[a]){
-        x=solve(A,B,C,a+1,b,dp);}
-    if(C[a+b]==B[b]){
-        y=solve(A,B,C,a,b+1,dp);}
-    dp[a][b]=x || y;}
-    return dp[a][b];
+    bool solve(int i,int j,string s1, string s2, string s3,vector<vector<int>> &dp){
+        
+        if(i==0 && j==0){return true;}
+
+        if(i==0){
+            while(j >0){
+             if (s2[j-1] !=s3[i+j-1]){return false;}
+             j--;
+            }
+            return true;
+        }
+
+        if(j==0){
+            
+            while(i >0){
+             if (s1[i-1] !=s3[i+j-1]){return false;}
+             i--;
+            }            
+            return true;
+        }
+        
+        if(dp[i][j] !=-1){return dp[i][j];}
+
+        if( s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1]){
+            return   dp[i][j]=solve(i-1,j,s1,s2,s3,dp)|| solve(i,j-1,s1,s2,s3,dp) ;
+        }
+
+        if(s1[i-1]==s3[i+j-1]){
+            return dp[i][j]=solve(i-1,j,s1,s2,s3,dp);
+        }
+
+        else if(s2[j-1]==s3[i+j-1]){
+            return dp[i][j]=solve(i,j-1,s1,s2,s3,dp);
+        }
+
+        return dp[i][j]=false;
+
     }
-    
+public:
     bool isInterleave(string s1, string s2, string s3) {
-     int a=s1.length();
-     int b=s2.length();
-     if(a + b !=s3.length()){return false;}
-     vector<vector<int>> dp(a,vector<int>(b,-1));
-     return solve(s1,s2,s3,0,0,dp);  
+     int l=s1.size();
+     int m=s2.size();
+     int n=s3.size();
+     if(n != (l+m) ){return false;}
+     vector<vector<int>>dp(l+1,vector<int>(m+1,-1));
+     return solve(l,m,s1,s2,s3,dp);
     }
 };
