cee50a190918fe8998816f504469f2810f9ba9d2
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-13 23:05:15 +0530

    Time: 3 ms (47.71%) | Memory: 6.6 MB (32.50%) - LeetSync

--- 397-integer-replacement/integer-replacement.cpp
+++ 397-integer-replacement/integer-replacement.cpp
@@ -1,9 +1,37 @@
 class Solution {
 public:
-    unsigned long long integerReplacement(unsigned long long n) {
-       if(n==1){return 0;}
-       if(n & 1)//odd
-        {return 1+min(integerReplacement(n-1),integerReplacement(n+1));}
-       return 1+integerReplacement(n/2);//even
+    //answer will be always possible and maximum ans possible is n
+    //as we are decreasing the number by 1 using n-1
+    int integerReplacement(int n) {
+        if(n==2147483647){
+            n=n-1;
+        }
+        queue<int> q;
+        int ans=0;
+        unordered_map<int,int> vis;
+        q.push(n);
+        vis[n]=1;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                int num=q.front();
+                q.pop();
+                if(num==1){return ans;}
+                if( !(num%2) && !vis.count(num/2)){
+                    vis[num/2]=1;
+                    q.push(num/2);
+                }
+                if( (num%2) && !vis.count(num+1)){
+                    vis[num+1]=1;
+                    q.push(num+1);
+                }
+                if ( (num%2) &&!vis.count(num-1)){
+                    vis[num-1]=1;
+                    q.push(num-1);
+                }
+            }
+            ans++;
+        }
+        return ans;
     }
 };
