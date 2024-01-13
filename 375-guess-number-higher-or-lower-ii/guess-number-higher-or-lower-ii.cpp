444129d336a374088c38162daa3d4dc4dfadf045
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-13 14:23:42 +0530

    Time: 86 ms (67.76%) | Memory: 8.5 MB (88.65%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,19 +1,34 @@
 class Solution {
-    int solve(int start,int end,vector<vector<int>>& dp){
-        if (start>=end){return 0;}
-        if(dp[start][end] !=-1){return dp[start][end];}
-        int min_money=1e9;
-        for(int guess=start;guess<=end;guess++){
-            int left=solve(start,guess-1,dp);
-            int right=solve(guess+1,end,dp);
-            int money=max(left,right)+guess;
-            min_money=min(min_money,money);
-        }
-        return dp[start][end]=min_money;
-    }
+    // int solve(int start,int end,vector<vector<int>>& dp){
+    //     if (start>=end){return 0;}
+    //     if(dp[start][end] !=-1){return dp[start][end];}
+    //     int min_money=1e9;
+    //     for(int guess=start;guess<=end;guess++){
+    //         int left=solve(start,guess-1,dp);
+    //         int right=solve(guess+1,end,dp);
+    //         int money=max(left,right)+guess;
+    //         min_money=min(min_money,money);
+    //     }
+    //     return dp[start][end]=min_money;
+    // }
 public:
     int getMoneyAmount(int n) {
-      vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-      return solve(1,n,dp);  
+      vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+      for(int start=n;start>=1;start--){
+          for(int end=1;end<=n;end++){
+            if(start>=end){continue;}
+            int min_money=1e9;
+            for(int guess=start;guess<=end;guess++){
+             int left=dp[start][guess-1];
+             int right=0;
+             if(guess !=n) right=dp[guess+1][end];
+             int money=max(left,right)+guess;
+             min_money=min(min_money,money);
+            }
+           dp[start][end]=min_money;
+        }
+    }
+      return dp[1][n];  
     }
 };
