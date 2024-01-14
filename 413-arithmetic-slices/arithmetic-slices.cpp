e8d783cd7f42b65f7f49c30ec68ecd1fedbd1de9
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-14 19:33:51 +0530

    Time: 3 ms (44.48%) | Memory: 7.6 MB (86.92%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -4,16 +4,13 @@ public:
       int n=nums.size();
       if(n<3){return 0;}
       int ans=0;
+      int count=0;
       for(int i=0;i<(n-2);i++){
-        for(int j=i;j<(n-2);j++){
-          if(  (nums[j+1]-nums[j])==(nums[j+2]-nums[j+1]) ){ans++;}
-          else {break;}
-        }
+          if(  (nums[i+1]-nums[i])==(nums[i+2]-nums[i+1]) ){
+              count++;}
+          else{ count=0;}
+        ans+=count;
       }
-      
-  
-
-      
       return ans;
     }
 };
