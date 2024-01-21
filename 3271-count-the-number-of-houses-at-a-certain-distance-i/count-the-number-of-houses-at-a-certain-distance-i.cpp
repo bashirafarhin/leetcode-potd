e7459cfb9bfd752d548d36384ec4f4dfd779dced
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-21 11:18:54 +0530

    Time: 4 ms (90.91%) | Memory: 9.3 MB (36.36%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -1,43 +1,19 @@
 class Solution {
-    int finddis(vector<vector<int>>&adj,vector<int>&vis,int src,int dest){
-        if(src==dest){return 0;}
-        vis[src]=1;
-        int d=1e9;
-        for(auto neigh : adj[src]){
-            if(vis[neigh]==0){
-                d=min(d,1+finddis(adj,vis,neigh,dest));
-            }
-        }
-        vis[src]=0;
-        return d;
-    }
 public:
     vector<int> countOfPairs(int n, int x, int y) {
-        vector<vector<int>> adj(n+1);
-
-        bool flag=true;
-        for(int i=1;i<n;i++){
-            adj[i].push_back(i+1);
-            adj[i+1].push_back(i);
-        }
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-
-       vector<int> ans(n,0); 
-
-        for(int i=1;i<=n;i++){
-           for(int j=i+1;j<=n;j++){
-            vector<int> vis(n+1,0);
-            int d=finddis(adj,vis,i,j);
-            if(1<=d<=n){ans[d-1]+=2;}
-                
+     if(x>y){
+         return countOfPairs(n,y,x);}
+     vector<int>ans(n,0);
+     for(int i=1;i<=n;i++){
+         for(int j=i+1;j<=n;j++){
+    //distance between src(i) to x + distance between x to y is always 1 + distance between y to destination(j)
+          int dis=abs(i-x)+1+abs(y-j); 
+          int d=min( abs(j-i),dis);
+          if(1<=d<=n){
+              ans[d-1]+=2;
           }
-        }
-       
-       
-
-
-        return ans;
-        
+         }
+     }
+     return ans;
     }
 };
