767ff7952734862a8ce7cd6bfb87ac2d4ba08e2d
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-23 14:43:50 +0530

    Time: 5 ms (91.42%) | Memory: 9.7 MB (81.80%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,15 +1,17 @@
 class Solution {
     int solve(int i,vector<int>&words,int word){
         if(i==words.size()){
-            int len=0;
-            for(int i=0;i<26;i++){
-                if(word & (1<<i)){len++;}
-            }
+            int len=word>>26;
             return len;}
 
         int take=0;
-        if(!(words[i] & word)){
-            take=solve(i+1,words,words[i] | word);
+        int oldword=word & ((1<<26)-1); //taking only word by removing the length which is stored in last 6 bits
+        int newword=words[i] & ((1<<26)-1);
+        if(!(oldword & newword)){
+            int newlen=words[i]>>26;
+            int oldlen=word>>26;
+            newword=oldword | newword | ((newlen + oldlen)<<26);
+            take=solve(i+1,words,newword);
         }
         int nottake=solve(i+1,words,word);
         return max(take,nottake);
@@ -18,6 +20,7 @@ class Solution {
 public:
     int maxLength(vector<string>& arr) {
         int n=arr.size();
+
         //make a array in which every word is converted into bitset
         vector<int>words;
         for(int i=0;i<n;i++){
@@ -25,12 +28,17 @@ public:
             int charsetbit=0;
             for(char c : arr[i]){
                 int mask=1<<(c-'a');
-                if(mask & charsetbit){include=false; break;}
+                if(mask & charsetbit){ //if these type of word present eg-aafgremoving that words
+                    include=false;
+                    break;}
                 charsetbit=charsetbit | mask;
             }
-
-            if(include){words.push_back(charsetbit);}
+            if(include){
+                int size=arr[i].length();
+                charsetbit=charsetbit | (size<<26); //storing length after 26 bits
+                words.push_back(charsetbit);}
         }
+
         // implement  same recursion as done in method-1
         return solve(0,words,0);
         
