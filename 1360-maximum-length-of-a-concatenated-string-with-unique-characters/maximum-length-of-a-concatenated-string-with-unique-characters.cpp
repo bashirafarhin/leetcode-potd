d83ed836a72ff8e224c131b69a308e833cee78ab
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-23 13:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (82.42%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,23 +1,38 @@
 class Solution {
-    int solve(int i,vector<string>& arr,string& s){
+    int solve(int i,vector<int>&words,int word){
+        if(i==words.size()){
+            int len=0;
+            for(int i=0;i<26;i++){
+                if(word & (1<<i)){len++;}
+            }
+            return len;}
 
-        unordered_set<char> st(s.begin(),s.end());
-        if(st.size() !=s.size()){return -INT_MAX;} //if characters are not unique
-
-        if(i==arr.size()){return 0;}
-
-        for(char c : arr[i]) { s+=c; }
-        int take=arr[i].size()+solve(i+1,arr,s);
-        
-        for(char c : arr[i]) { s.pop_back(); } //backtrack
-        int nottake=solve(i+1,arr,s);
-
+        int take=0;
+        if(!(words[i] & word)){
+            take=solve(i+1,words,words[i] | word);
+        }
+        int nottake=solve(i+1,words,word);
         return max(take,nottake);
 
     }
 public:
     int maxLength(vector<string>& arr) {
-        string s="";
-        return solve(0,arr,s);
+        int n=arr.size();
+        //make a array in which every word is converted into bitset
+        vector<int>words;
+        for(int i=0;i<n;i++){
+            bool include=true;
+            int charsetbit=0;
+            for(char c : arr[i]){
+                int mask=1<<(c-'a');
+                if(mask & charsetbit){include=false; break;}
+                charsetbit=charsetbit | mask;
+            }
+
+            if(include){words.push_back(charsetbit);}
+        }
+        // implement  same recursion as done in method-1
+        return solve(0,words,0);
+        
     }
 };
