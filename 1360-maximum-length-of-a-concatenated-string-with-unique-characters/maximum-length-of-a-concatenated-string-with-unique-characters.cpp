e0f6f23bf5c31f4cac211dbe809c2fd1e6957381
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-23 11:22:53 +0530

    Time: 239 ms (22.03%) | Memory: 124.2 MB (18.93%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,11 +1,11 @@
 class Solution {
     int solve(int i,vector<string>& arr,string s){
-        if(i==arr.size()){return 0;}
+        if(i==arr.size()){return s.size();}
         string combined=arr[i]+s;
         unordered_set<char> st(combined.begin(),combined.end());
         int take=0;
         if(st.size()==combined.size()){
-           take=arr[i].size()+solve(i+1,arr,combined);
+           take=solve(i+1,arr,combined);
         }
         int nottake=solve(i+1,arr,s);
         return max(take,nottake);
