294928f74c890368fc34563676f441ac9ec34b5c
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-25 10:44:13 +0530

    Time: 256 ms (5.22%) | Memory: 43.6 MB (24.37%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+
         int n=nums.size();
-        vector<int>find(n+1,0);
+        int j=0;
         for(int i=0;i<n;i++){
-            if(1<=nums[i] && nums[i]<=n){
-                find[nums[i]]=1;
-            }
+            if(nums[i]<=0){ 
+                swap(nums[i],nums[j]);
+                j++;}
         }
         
-        for(int i=1;i<=n;i++){
-            if(find[i]==0){return i;}
+        for(int n : nums){cout<<n<<" ";}
+        cout<<endl;
+
+        for(int i=0;i<j;i++){nums[i]=1;}
+
+        for(int n : nums){cout<<n<<" ";}
+        cout<<endl;
+
+        for(int i=j;i<n;i++){
+            if(nums[i]>n){continue;}
+            int ind=abs(nums[i])-1;
+            if(nums[ind]>0){
+                nums[ind]=-1*nums[ind];
+            }
+
+        }
+
+        for(int n : nums){cout<<n<<" ";}
+        cout<<endl;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){return i+1;}
         }
         return n+1;
     }
