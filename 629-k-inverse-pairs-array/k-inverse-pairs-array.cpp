962e0bb69b20bc5cfbde9dd662259fc052be5e8d
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-01-27 13:36:38 +0530

    Time: 1171 ms (19.78%) | Memory: 7.3 MB (89.56%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -1,18 +1,21 @@
 class Solution {
     int mod=1e9+7;
-    int solve(int n,int k,vector<vector<int>>&dp){
-        if(k==0){return 1;}
-        if(n==0){return 0;}
-        if(dp[n][k] !=-1){return dp[n][k];}
-        int count=0;
-        for(int i=0;i<=min(k,n-1);i++){
-            count=( count + solve (n-1,k-i,dp) )% mod;
-        }
-        return dp[n][k]=count;
-    }
 public:
-    int kInversePairs(int n, int k) {
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        return solve(n,k,dp);
+    int kInversePairs(int N, int K) {
+        vector<int>curr(K+1,0);
+        vector<int>prev(K+1,0);
+        for(int n=1;n<=N;n++){
+            for(int k=0;k<=K;k++){
+                if(k==0){ curr[k]=1; }
+                else {
+                 int count=0;
+                 for(int i=0;i<=min(k,n-1);i++){
+                  count=( count + prev[k-i] )% mod; }
+                 curr[k]=count;
+                }
+            }
+            prev=curr;
+        }
+        return curr[K];
     }
 };
