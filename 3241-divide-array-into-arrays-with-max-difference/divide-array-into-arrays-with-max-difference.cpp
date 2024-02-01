4b47737b52b254aed091f8bfb4a45eaa00107cd9
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-02-01 13:31:16 +0530

    Time: 156 ms (68.70%) | Memory: 119.5 MB (39.48%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -8,7 +8,7 @@ public:
      sort(nums.begin(),nums.end());
 
      for(int i=0;i<n;i+=3){
-        if( (nums[i+1]-nums[i])<=k && (nums[i+2]-nums[i+1])<=k && (nums[i+2]-nums[i])<=k ){
+        if( (nums[i+2]-nums[i])<=k  ){
              ans.push_back({nums[i],nums[i+1],nums[i+2]});
          }
         else { return {}; }
