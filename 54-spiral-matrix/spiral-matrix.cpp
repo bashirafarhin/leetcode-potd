d388de3671110f0175f802d26b207d1c2e4d94bb
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-02-02 00:17:47 +0530

    Time: 3 ms (39.80%) | Memory: 8.3 MB (12.10%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    void boundary(vector<vector<int>>& matrix,int r,int c,int row,int col,vector<int>&ans){
-        if( r>row || c>col){ return; }
-
-        for(int i=c;i<=col;i++){
-         ans.push_back(matrix[r][i]);
-        }
-        for(int i=r+1;i<=row;i++){
-         ans.push_back(matrix[i][col]);
-        }
-        //if last row is also not the 1st row
-        if( r != row ){
-            for(int i=col-1;i>=c;i--){
-              ans.push_back(matrix[row][i]);
-            }
-        }
-        //if last col is also not the 1st col
-        if( c != col ){
-            for(int i=row-1;i>r;i--){
-              ans.push_back(matrix[i][c]);
-            }
-        }
-
-        boundary(matrix,r+1,c+1,row-1,col-1,ans); //shrinking the matrix
-    }
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int>ans;
-        int row=matrix.size();
-        int col=matrix[0].size();
-        boundary(matrix,0,0,row-1,col-1,ans);
+        int row=matrix.size()-1;
+        int col=matrix[0].size()-1;
+        int r=0;
+        int c=0;
+        while( row>=r && col>=c){
+
+            for(int i=c;i<=col;i++){
+                ans.push_back(matrix[r][i]);
+            }
+            r++; //move to next row //shrinking the matrix
+            
+            for(int i=r;i<=row;i++){
+                ans.push_back(matrix[i][col]);
+            }
+            col--; //move to prev col //shrinking the matrix
+
+            if( r <=row ){
+              for(int i=col;i>=c;i--){
+                ans.push_back(matrix[row][i]);
+              }
+              row--; //move to prev row //shrinking the matrix
+            }
+
+            if( c <=col){
+              for(int i=row;i>=r;i--){
+                ans.push_back(matrix[i][c]);
+              }
+              c++; //move to next col //shrinking the matrix
+            }
+        }
         return ans;
     }
 };
