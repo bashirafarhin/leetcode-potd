f06b17e180572ddfa7ec3771928624a2bce6f8c7
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-02-02 23:47:03 +0530

    Time: 3 ms (17.15%) | Memory: 7.5 MB (8.37%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-      vector<int>list={12,23,34,45,56,67,78,89,
-                       123,234,345,456,567,678,789,
-                       1234,2345,3456,4567,5678,6789,
-                       12345,23456,34567,45678,56789,
-                       123456,234567,345678,456789,
-                       1234567,2345678,3456789,
-                       12345678,23456789,
-                       123456789};
-      vector<int>ans;
-      for(int n : list){
-          if(low<=n && n<=high){ans.push_back(n);}
-          if(n>high){break;}
-      }
-      return ans;
+        vector<int>ans;
+        int num=1;
+        int inc=1;
+        for(int i=2;i<=9;i++){
+            inc=inc+pow(10,i-1);
+            num=num+inc;
+            int n=num;
+            int j=9-i+1;
+            while(n<=high && j ){
+               if(low<= n ) { ans.push_back(n); }
+                n+=inc;
+                j--;
+            }
+        }
+        return ans;
     }
 };
+
