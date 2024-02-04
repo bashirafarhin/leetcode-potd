cbb5f26f03532c754621a3bc8a1284263a886d8c
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-02-04 16:48:56 +0530

    Time: 51 ms (100.00%) | Memory: 31 MB (100.00%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -1,27 +1,29 @@
 class Solution {
+    static bool cmp(vector<int>&a,vector<int>&b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
+        return a[0]<b[0];
+    }
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-      int n=points.size();
+        sort(points.begin(),points.end(),cmp);
+        int n=points.size();
         int ans=0;
       
         for(int i=0;i<n;i++){
 
             int cx=points[i][0];
             int cy=points[i][1];
-
-            for(int j=0;j<n;j++){
+            int h=-1e9-1; //benchmark height
+            for(int j=i+1;j<n;j++){
 
               int tx=points[j][0];
               int ty=points[j][1];
-
-                if( i !=j && tx>=cx && ty<=cy ){
-                   bool validPoint=true;
-                   for(int k=0;k<n;k++){
-                    if( k==i || k==j ){continue;}
-                    if( cx<=points[k][0] && tx>=points[k][0] && ty<=points[k][1] && points[k][1]<=cy) {  validPoint=false; break ;}
-                    }
-                  if(validPoint){ans++;}
-                }
+              if(ty > cy){ continue;}
+              if( ty>h ){
+                  h=ty;
+                  ans++;}
             }
         }
         return ans;  
