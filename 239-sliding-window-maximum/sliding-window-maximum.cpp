fbb9e7f73f75a31b442e5ae7e05d10f2eed71113
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-02-06 22:27:04 +0530

    Time: 549 ms (15.66%) | Memory: 214.6 MB (6.29%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -3,18 +3,17 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int>ans;
-        priority_queue<pair<int,int>> pq;
+        set<pair<int,int>,greater<pair<int,int>>> st;
+
         for(int i=0;i<k;i++){
-            pq.push({nums[i],i});
+            st.insert({nums[i],i});
         }
-        ans.push_back(pq.top().first);
+        ans.push_back(st.begin()->first);
 
         for(int i=k;i<n;i++){
-           while(!pq.empty() && pq.top().second<=i-k){
-               pq.pop();
-           }
-           pq.push({nums[i],i});
-           ans.push_back(pq.top().first);
+           st.insert({nums[i],i});
+           st.erase({nums[i-k],i-k});
+           ans.push_back(st.begin()->first);
         }
 
         return ans;
