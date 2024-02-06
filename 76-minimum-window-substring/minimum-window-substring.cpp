b1edb89697ba9a910458ddfa6592866ee0198c09
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-02-07 00:06:38 +0530

    Time: 235 ms (5.02%) | Memory: 58 MB (5.11%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,48 +1,38 @@
 class Solution {
-    bool compare(vector<int>&mp2,vector<int>&mp1){
-        for(int i=0;i<52;i++){
-            if(mp2[i]<mp1[i]){ return false ;}
-        }
-        return true;
+    bool compare(vector<int>&mp1,vector<int>&mp2){
+      for(int i=0;i<256;i++){
+         if(mp1[i]==0 && mp2[i]>0){ continue; }
+         if(mp1[i]>mp2[i]){ return false; }
+      }
+      return true;
     }
 public:
     string minWindow(string s, string t) {
-        unordered_set<char> isPresent;
-        vector<int> mp1(52,0);
-        vector<int> mp2(52,0);
+        vector<int> mp1(256,0);
+        vector<int> mp2(256,0);
         int n=s.length();
         int m=t.length();
         string ans="";
 
-        for(int i=0;i<m;i++){
-            int ind= t[i] -  ( (97<=t[i] && t[i]<=122) ? 97 : 39 );
-            mp1[ind]++;
-            isPresent.insert(t[i]);
-        }
+        for(int i=0;i<m;i++){ mp1[t[i]]++; }
 
         int start=0;
         for(int i=0;i<n;i++){
-
-          if( isPresent.find(s[i])==isPresent.end()){ continue; }
-
-          int ind= s[i] -  ( (97<=s[i] && s[i]<=122) ? 97 : 39 );
-          mp2[ind]++;
-
-          if(compare(mp2,mp1)){
-
-          while(start<=i && compare(mp2,mp1) ){
-              if( isPresent.find(s[start]) !=isPresent.end()){
-                  int ind= s[start] -  ( (97<=s[start] && s[start]<=122) ? 97 : 39 );
-                  mp2[ind]--;}
-              start++;
-          }
-          start--;
-          string temp=s.substr(start,i-start+1);
-          start++;
-          if ( ans=="" || ans.size()>temp.size() ){ ans=temp;}
+          mp2[s[i]]++;
           
+          if( compare(mp1,mp2) ){
+            while(start<=i && compare(mp1,mp2) ){
+              mp2[s[start]]--;
+              start++;
+            }
+            start--;
+            string temp=s.substr(start,i-start+1);
+            start++;
+            if ( ans=="" || ans.size()>temp.size() ){ ans=temp; }
           }
+         
         }
+
         return ans;
     }
 };
