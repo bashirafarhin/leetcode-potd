3a37fc6644d007e9505587333bdd4b189c311929
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-03-19 09:45:28 +0530

    Time: 30 ms (41.57%) | Memory: 15.9 MB (49.37%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,11 +4,14 @@ public:
      int n=nums.size();
      vector<vector<int>>ans;
      sort(nums.begin(),nums.end());
-     unordered_set<string> seen;
      
      for(int i=0;i<n-3;i++){
 
+        if(i>0 && nums[i-1]==nums[i]) continue;
+
          for(int j=i+1;j<n-2;j++){
+
+            if(j>i+1 && nums[j-1]==nums[j]) continue;
 
              int p=j+1;
              int q=n-1;
@@ -20,18 +23,11 @@ public:
                 sum+=nums[q];
                 
                 if(sum==target){
-                    
-                    string x="";
-                    x+=nums[i];
-                    x+=nums[j];
-                    x+=nums[p];
-                    x+=nums[q];
-                    if(seen.find(x)==seen.end()){
-                        seen.insert(x);
-                        ans.push_back({nums[i],nums[j],nums[p],nums[q]});
-                    }
+                    ans.push_back({nums[i],nums[j],nums[p],nums[q]});
                     p++;
                     q--;
+                    while(p<q && nums[p-1]==nums[p]){ p++; }
+                    while(p<q && nums[q]==nums[q+1]){ q--; }
                 }
                 else if(sum>target){
                     q--;
