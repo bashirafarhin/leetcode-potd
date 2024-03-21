2cf3d6069929729b5d9e07a869bc49f54f0a2a0b
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-03-21 19:21:42 +0530

    Time: 44 ms (56.21%) | Memory: 44.7 MB (30.66%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+       int n=nums.size();
+       sort(nums.begin(),nums.end());
+       int find=1;
+       for(int i=0;i<n;i++){
+        if(nums[i]>0){
+            if(find==nums[i]){
+                 
+                 while(i<n && nums[i]==find){ i++; }
+                 i--;
+                 find++; }
 
-        int n=nums.size();
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]<=0){ 
-                swap(nums[i],nums[j]);
-                j++;}
+            else{ return find; }
         }
-        
-        for(int n : nums){cout<<n<<" ";}
-        cout<<endl;
-
-        for(int i=0;i<j;i++){nums[i]=1;}
-
-        for(int n : nums){cout<<n<<" ";}
-        cout<<endl;
-
-        for(int i=j;i<n;i++){
-            if(nums[i]>n){continue;}
-            int ind=abs(nums[i])-1;
-            if(nums[ind]>0){
-                nums[ind]=-1*nums[ind];
-            }
-
-        }
-
-        for(int n : nums){cout<<n<<" ";}
-        cout<<endl;
-
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){return i+1;}
-        }
-        return n+1;
+     }
+     return find;
     }
 };
