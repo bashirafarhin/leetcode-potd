2929fc267019cb914dc5bd5f4fdc24f0a0555095
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-03-24 11:11:51 +0530

    Time: 343 ms (80.00%) | Memory: 166.1 MB (100.00%) - LeetSync

--- 3363-most-frequent-ids/most-frequent-ids.cpp
+++ 3363-most-frequent-ids/most-frequent-ids.cpp
@@ -7,15 +7,16 @@ public:
         set<pair<long long,int>> freqSet; //freq
                 
         for(int i=0;i<n;i++){
-            if(mp.find(nums[i]) !=mp.end()){
-                long long newFreq=mp[nums[i]]+freq[i];
-                freqSet.insert( {-1*newFreq,nums[i]} );
-                freqSet.erase( {-1*mp[nums[i]],nums[i]} );
-                mp[nums[i]]=newFreq;
-            } else {
-                freqSet.insert( {-1*freq[i],nums[i]} );
-                mp[nums[i]]=freq[i];
+            long long oldFreq=0;
+            int id=nums[i];
+            if(mp.find(id) !=mp.end()){
+                oldFreq=mp[id];
+                freqSet.erase({-1*oldFreq,id});
             }
+            long long newFreq= oldFreq + freq[i];
+            freqSet.insert( {-1*newFreq,id} );
+            mp[id]=newFreq;
+
             ans[i]= freqSet.begin()==freqSet.end() ? 0 : -1*freqSet.begin()->first;
         }
         return ans;
