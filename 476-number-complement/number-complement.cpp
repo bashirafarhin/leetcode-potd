06d6d961017e520505966053aa92b7e7aef37f2f
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-09 21:06:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (87.69%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int findComplement(int num) {
-     if(num==0) return 1;
-     if(num==1) return 0;
-     int n=num;
-     int mask=0;
-     while(n !=0){
-         n>>=1;
-         mask<<=1;
-         mask=mask | 1;
-         
-     }
-     return (~num) &mask;
+        int mask=0,n=num;
+        while(n){
+            mask<<=1;
+            mask|=1;
+            n>>=1;
+        }
+        return ~num & mask;
     }
-
 };
