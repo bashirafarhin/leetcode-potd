b6c54472a7044d9daee24c716bdc684642221864
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-10 20:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (32.87%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& obstacleGrid,int r,int c,vector<vector<int>> &dp){
-        if(r<0 || c<0 || obstacleGrid[r][c]==1){return 0;}
-        if(r==0 && c==0)
-        {return 1;}
-        if(dp[r][c]==-1){
-        int left=solve(obstacleGrid,r,c-1,dp);
-        int up=solve(obstacleGrid,r-1,c,dp);
-        dp[r][c]=left+up;}
-        return dp[r][c];
+    int solve(vector<vector<int>>& grid,int r,int c,vector<vector<int>>& vis,vector<vector<int>>&dp){
+        if(r<0 || c<0 || vis[r][c]==1 || grid[r][c]==1){ return 0; }
+        if(r==0 && c==0){ return 1; }
+        if(dp[r][c] !=-1){ return dp[r][c]; }
+        vis[r][c]=1;
+        int up=solve(grid,r-1,c,vis,dp);
+        int left=solve(grid,r,c-1,vis,dp);
+        vis[r][c]=0;
+        return dp[r][c]=up+left;
+
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-      int m=obstacleGrid.size();
-      int n=obstacleGrid[0].size();
-      vector<vector<int>> dp(m,vector<int>(n,-1));
-      return solve(obstacleGrid,m-1,n-1,dp);
+        int r=obstacleGrid.size();
+        int c=obstacleGrid[0].size();
+        vector<vector<int>>vis(r,vector<int>(c,0));
+        vector<vector<int>>dp(r,vector<int>(c,-1));    
+        return solve(obstacleGrid,r-1,c-1,vis,dp);
     }
 };
