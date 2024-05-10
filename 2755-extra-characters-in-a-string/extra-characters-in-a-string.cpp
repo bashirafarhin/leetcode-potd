147563931b11fccb8e8ef2f0307afdee5bc1527e
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-10 22:24:35 +0530

    Time: 332 ms (12.39%) | Memory: 159.7 MB (8.04%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,29 +1,28 @@
 class Solution {
-    int solve(string s,unordered_set<string>& dict,int i,vector<int>& dp){
-        if(i==s.length()){return 0;}
-        if(dp[i] !=-1){return dp[i];}
-        int n=s.length();
-        int minextrachar=n;
-        string currWord="";
-        for(int j=i;j<n;j++){
-         currWord+=s[j];
-         if(dict.find(currWord) != dict.end()){
-            minextrachar=min( minextrachar , solve(s,dict,j+1,dp));
-          } else {
-            int val=currWord.length()+solve(s,dict,j+1,dp);
-            minextrachar=min( minextrachar , val );}
-          }
-
-        return dp[i]=minextrachar;
-        
-    }
 public:
+    int solve(int i,string s,unordered_set<string>&dict,vector<int>&dp){
+        int n=s.size();
+        if(i==n){ return 0; }
+        if(dp[i] !=-1){ return dp[i]; }
+        string curr="";
+        int ans=1e9,count=1e9;
+        for(int j=i;j<n;j++){
+         curr+=s[j];
+         if(dict.find(curr)==dict.end()){
+            count=curr.size()+solve(j+1,s,dict,dp);
+         } else {
+            count=solve(j+1,s,dict,dp);
+         }
+         ans=min(ans,count);
+        }
+       
+        return dp[i]=ans;
+    }
+
     int minExtraChar(string s, vector<string>& dictionary) {
-       unordered_set<string> dict;
-       for(auto word : dictionary){
-           dict.insert(word);
-       }
-       vector<int> dp(s.length(),-1);
-       return solve(s,dict,0,dp); 
+        vector<int>dp(s.size(),-1);
+        unordered_set<string>dict;
+        for(string s : dictionary){ dict.insert(s); }
+        return solve(0,s,dict,dp);
     }
 };
