1969032ee1c81a2de79ea6bf040fbbedc556d819
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-10 23:29:52 +0530

    Time: 5 ms (26.98%) | Memory: 11.7 MB (7.89%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -1,16 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    int stringToNum(string num){
+        string var=num;
+        int ans=stoi(var);
+        return ans;
+    }
+
+    int solve(TreeNode* root,string num){
+        if(root==NULL){ return 0; }
+        char ch=root->val+48;
+
+        if(root->left==NULL && root->right==NULL) {
+            return stringToNum(num+ch); }
+        
+        return solve(root->left,num+ch)+solve(root->right,num+ch);
+        
+    }
     int sumNumbers(TreeNode* root) {
-        int sum = 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
-        while(q.size()) {
-            auto [n, cur] = q.front(); q.pop();
-            cur = cur * 10 + n -> val;
-            if(n -> left) q.push({n -> left, cur});      
-            if(n -> right) q.push({n -> right, cur});
-            if(!n -> left && !n -> right) sum += cur;   // add to total sum if we are at leaf node
-        }
-        return sum;
+        return solve(root,"");
     }
 };
