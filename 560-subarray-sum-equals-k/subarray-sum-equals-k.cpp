0d4e1d5908ad449d9cd64d5fccd443968ffea524
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-10 19:42:30 +0530

    Time: 66 ms (64.90%) | Memory: 45.1 MB (57.32%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-     int curr_sum = 0,count=0;
+     //do dry run of 1 2 3 -3 1 1 1 4 2 -3
+     int count=0;
      int n=nums.size();
-     unordered_map<int,int> map;
-     for (int i = 0; i < n; i++) {
-        curr_sum = curr_sum + nums[i];
-        if (curr_sum == k) {
-           count++;
+     int prefixSum=0;
+     unordered_map<int,int>mp; //prefixSum,itsCount
+     mp[0]=1;
+     for(int i=0;i<n;i++){
+        prefixSum+=nums[i];
+        if(mp.find(prefixSum-k) !=mp.end()){
+            count+=mp[prefixSum-k];
         }
-        if (map.find(curr_sum - k) != map.end()) {
-           count+=map[curr_sum-k];
-        }
-        map[curr_sum]++;
+        mp[prefixSum]++;
      }
-     return count;}
+     return count;
+    }
 };
