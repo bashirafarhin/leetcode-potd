dd81e1e2c3cb2b08ac2b1aae4f83c8470e8ff548
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-11 11:19:49 +0530

    Time: 10 ms (19.03%) | Memory: 11.9 MB (36.85%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-     int N=nums.size();
-     vector<int> count(32,0);
-     int ans=0;
-     for(int pos=0;pos<32;pos++){
-         int sum_bits_at_ith_pos=0;
-         for(int i=0;i<N;i++){
-             if(  (nums[i]>>pos) & 1 ){sum_bits_at_ith_pos++;}
-         }
-        if ( (sum_bits_at_ith_pos  % 3) !=0){ans+=1<<pos;}
-     }
-     return ans;
+        vector<int>arr(32,0);
+
+        for(int i=0;i<nums.size();i++){
+            int n=nums[i];
+            for(int j=31;j>=0;j--){
+                arr[j]+=(n&1);
+                n>>=1;
+            }
+        }
+
+        for(int i=0;i<32;i++){cout<<arr[i];}
+        cout<<endl;
+
+        int ans=0;
+        for(int i=31;i>=0;i--){
+
+            if(arr[i]%3 !=0) {ans|=(1<<(31-i));}
+        }
+        return ans;
     }
 };
