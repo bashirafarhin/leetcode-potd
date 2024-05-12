a82a5cb84decf343d160e88e1c02bbb62656a1d5
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-12 20:31:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (46.84%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-    int i = 0; 
-    int j = 0;
-    int n1 = version1.size(); 
-    int n2 = version2.size();
-    
-    int num1 = 0;
-    int num2 = 0;
-    while(i<n1 || j<n2)
-    {
-        while(i<n1 && version1[i]!='.'){
-            num1 = num1*10+(version1[i]-'0');
-            i++;
+      int i=0;
+      int j=0;
+      int n1=version1.size();
+      int n2=version2.size();
+
+      while(i<n1 || j<n2){
+        int num1=0;
+        int num2=0;
+
+        while(i<n1 && version1[i] !='.'){
+            while(i<n1 && num1==0 && version1[i]=='0'){ i++; }
+            if(i<n1 && version1[i]=='.'){continue;}
+            if(i<n1) { num1=num1*10+(int(version1[i])-48); i++; }
         }
-        
-        while(j<n2 && version2[j]!='.'){
-            num2 = num2*10+(version2[j]-'0');;
-            j++;
+
+        while(j<n2 && version2[j] !='.'){
+            while(j<n2 && num2==0 && version2[j]=='0'){ j++; }
+            if(j<n2 && version2[j]=='.'){continue;}
+            if(j<n2) { num2=num2*10+(int(version2[j])-48); j++; }
         }
-        
-        if(num1>num2) return 1;
-        else if(num1 < num2) return -1;
-        
-        num1 = 0;
-        num2 = 0;
-        i++;
-        j++;
+
+        if(i<n1 && version1[i]=='.')i++;
+        if(j<n2 && version2[j]=='.')j++;
+        cout<<num1<<" "<<num2<<endl;
+        if(num1>num2)return 1;
+        else if(num1<num2)return -1;
+      }
+
+      return 0;
+
     }
-    
-    return 0;
-}
 };
