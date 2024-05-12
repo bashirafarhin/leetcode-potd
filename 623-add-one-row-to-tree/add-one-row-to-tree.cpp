fac0d89d7a4b1cfe8bd8238285ceef7514926f0a
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-12 21:25:17 +0530

    Time: 23 ms (7.03%) | Memory: 24.1 MB (99.43%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -1,56 +1,37 @@
 class Solution {
 public:
-    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if(depth == 1) // if depth is 1, then we have to simply add root
-        {
-            TreeNode* newNode = new TreeNode(val);
-            newNode -> left = root;
-            return newNode; // now this act as new root
-        }
+    TreeNode* solve(TreeNode* root, int val, int depth){
+        if(depth==1 || root==NULL){ return root; }
         
-        queue<TreeNode*> q; // defineing queue
-        q.push(root); // push queue
-        int count = 0; // variable to count level
-        
-        // start bfs
-        while(!q.empty()) 
-        {
-            // since we have to add values on all of the nodes
-            // present at that level, so take out the size
-            int n = q.size(); 
-            
-            count++; // increase size
-            while(n--)
-            {
-                TreeNode* curr = q.front(); // take out current node
-                q.pop(); // pop it from queue
-                
-                // if we not reach till our required level
-                // so we have to do nothing, simply push into queue
-                if(count != depth - 1) 
-                {
-                    if(curr -> left) q.push(curr -> left);
-                    if(curr -> right) q.push(curr -> right);
-                }
-                else // else we reach our required level
-                {
-                    // make a new node to add
-                    TreeNode* newNode = new TreeNode(val);
-                    // first put left of curr to newNode
-                    newNode -> left = curr -> left;
-                    // now curr of left becomes newNode
-                    curr -> left = newNode;
-                    
-                    // make a new node to add
-                    TreeNode* newNode2 = new TreeNode(val);
-                    // first put right of curr to newNode
-                    newNode2 -> right = curr -> right;
-                     // now curr of right becomes newNode
-                    curr -> right = newNode2;
-                }
-            }
+        if(depth==2){
+            TreeNode* Node1=new TreeNode(val);
+            TreeNode* Node2=new TreeNode(val);
+
+            TreeNode* l=root->left;
+            TreeNode* r=root->right;
+
+            root->left=Node1;
+            Node1->left=solve(l,val,depth-1);
+
+            root->right=Node2;
+            Node2->right=solve(r,val,depth-1);
+            return root;
         }
 
-        return root; // finally return right
+        
+        cout<<root->val<<endl;
+        root->left=solve(root->left,val,depth-1);
+        root->right=solve(root->right,val,depth-1);
+        return root;
+
+    }
+
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth==1){
+            TreeNode* Node=new TreeNode(val);
+            Node->left=root;
+            return Node;
+        }
+        return solve(root,val,depth);        
     }
 };
