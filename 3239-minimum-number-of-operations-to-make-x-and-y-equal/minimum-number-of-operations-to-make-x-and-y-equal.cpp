3a03967f804bc89d859d3f25241b303d285ad549
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-13 13:20:45 +0530

    Time: 41 ms (30.97%) | Memory: 21.6 MB (51.24%) - LeetSync

---
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minimumOperationsToMakeEqual(int x, int y) {
+        if (x < y) {
+            return y - x;
+        }
+        queue<pair<int, int>> q;
+        unordered_map<int, int> vis;
+        q.push({x, 0});
+        int ans = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+
+                int node = q.front().first;
+                int dist = q.front().second;
+                q.pop();
+                if (node == y) {
+                    ans = dist;
+                    break;
+                }
+                if (node % 11 == 0 && vis[node / 11] == false) {
+                    vis[node / 11] = true;
+                    q.push({node / 11, dist + 1});
+                }
+                if (node % 5 == 0 && vis[node / 5] == false) {
+                    vis[node / 5] = true;
+                    q.push({node / 5, dist + 1});
+                }
+                if (node - 1 >= 1 && vis[node - 1] == false) {
+                    vis[node - 1] = true;
+                    q.push({node - 1, dist + 1});
+                }
+                if (node + 1 <= x + 15 && vis[node + 1] == false) {
+                    vis[node + 1] = true;
+                    q.push({node + 1, dist + 1});
+                }
+            }
+        }
+        return ans;
+    }
+};
