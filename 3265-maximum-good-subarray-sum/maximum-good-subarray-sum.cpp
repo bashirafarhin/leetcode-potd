555137f7b2b78bb3e47db7757e0e41dc4929ea3b
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-14 11:42:18 +0530

    Time: 266 ms (67.71%) | Memory: 141.2 MB (55.03%) - LeetSync

--- 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
+++ 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<long long >prefix(n,0ll);
-        prefix[0]=static_cast<long long> (nums[0]);
-        for(int i=1;i<n;i++){
-            prefix[i]=static_cast<long long> (nums[i]+prefix[i-1] );
-        }
-        long long int ans=LLONG_MIN;
-        
-        unordered_map<long long,int> mp;
-        
-        for(int i=0;i<n;i++){
-            if( mp.find(nums[i]-k) != mp.end()){
-                 int ind=mp[nums[i]-k];
-                 ans = max(ans, prefix[i] - (ind > 0 ? prefix[ind - 1] : 0));
-            }
-            if( mp.find( nums[i]+k) != mp.end()){
-                 int ind=mp[nums[i]+k];
-                 ans = max(ans, prefix[i] - (ind > 0 ? prefix[ind - 1] : 0));
-            }
-            auto it=mp.find(nums[i]);
-            if( it== mp.end() || ( prefix[i]-prefix[it->second]<=0 ) ){
-                 mp[nums[i]]=i;
-            }
+        long long ans = -9223372036854775808;
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        vector<long long> prefix(n, 0);
+        prefix[0] = nums[0];
+        mp[nums[0]] = 0;
+        bool flag=false;
 
-            
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + (long long)nums[i];
+
+            if (mp.find(nums[i] - k) != mp.end()) {
+                flag=true;
+                int ind = mp[nums[i] - k];
+                long long val = (ind >= 1) ? prefix[ind - 1] : 0;
+                ans = max(ans, prefix[i] - val);
+            }
+            if (mp.find(nums[i] + k) != mp.end()) {
+                flag=true;
+                int ind = mp[nums[i] + k];
+                long long val = (ind >= 1) ? prefix[ind - 1] : 0;
+                ans = max(ans, prefix[i] - val);
+            }
+            if (mp.find(nums[i]) == mp.end() ||
+                (prefix[mp[nums[i]]] > prefix[i])) {
+                mp[nums[i]] = i;
+            }
         }
-        return ans==LLONG_MIN ? 0 : ans;
+        return flag==true? ans : 0;
     }
 };
