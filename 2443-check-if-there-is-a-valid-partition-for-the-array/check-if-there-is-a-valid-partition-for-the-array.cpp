43703d971071fcdb923f97050d95246082c36592
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-14 23:20:52 +0530

    Time: 139 ms (13.19%) | Memory: 95.5 MB (28.02%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
- 
+    bool solve(vector<int>&nums,int i,vector<int>&dp){
+        if(i==nums.size()){
+            return true;
+        }
+        if(dp[i] !=-1){
+            return dp[i];
+        }
+        int n=nums.size();
+        if(i+1<n && nums[i]==nums[i+1]){
+            if(solve(nums,i+2,dp)){ return dp[i]=true; }
+        }
+        if(i+2<n && nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
+            if(solve(nums,i+3,dp)){ return dp[i]=true; }
+        }
+        if(i+2<n && nums[i]+1==nums[i+1] && nums[i+1]+1==nums[i+2]){
+            if(solve(nums,i+3,dp)){ return dp[i]=true; }
+        }
+        return dp[i]=false;        
+    }
+
     bool validPartition(vector<int>& nums) {
-     int n=nums.size();
-     vector<bool> dp(n+1,true);
-     for(int i=n-1;i>=0;i--){
-       bool a=false,b=false,c=false;
-       if(i+1<n && nums[i]==nums[i+1])
-          { a=dp[i+2];}
-       if(!a && i+2<n && nums[i]==nums[i+1] && nums[i]==nums[i+2])
-          { b=dp[i+3];}
-       if(!b && i+2<n && nums[i]==nums[i+1]-1 && nums[i+1]==nums[i+2]-1)
-          { c=dp[i+3];}
-        dp[i]=a || b || c;}
-     return dp[0];
+        vector<int>dp(nums.size(),-1);
+        return solve(nums,0,dp);
     }
 };
