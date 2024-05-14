e23d3125efcec9df358d3c4ce2918e02f86a25c4
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-14 14:04:41 +0530

    Time: 122 ms (62.39%) | Memory: 73.9 MB (67.56%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -6,11 +6,11 @@ public:
         for(int i=0;i<n;i++){ mp[nums[i]]=1; }
         int ans=0;
         for(int i=0;i<n;i++){
-            if(mp.find(nums[i]-1)==mp.end()){
+            if(mp.find(nums[i]+1)==mp.end()){
                 int l=1;
-                int val=nums[i]+1;
+                int val=nums[i]-1;
                 while(mp.find(val) !=mp.end()){
-                    val++;
+                    val--;
                     l++;
                 }
                 ans=max(ans,l);
