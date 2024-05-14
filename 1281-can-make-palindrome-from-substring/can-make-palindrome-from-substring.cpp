90e9e22b70f2391dc818f65fdc2c2296bc502a8d
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-14 20:09:37 +0530

    Time: 368 ms (25.96%) | Memory: 207.8 MB (17.79%) - LeetSync

--- 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.cpp
+++ 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
-    int n=queries.size();
-    vector<bool> ans(n,true);
-    int len=s.length();
-    vector<vector<int>>prefix;
-    vector<int> arr(26,0);
-    for(int i=0;i<len;i++){
-        arr[s[i]-'a']++;
-        prefix.push_back(arr);
-    }
-    for(int i=0;i<n;i++){
-        int l=queries[i][0];
-        int r=queries[i][1];
-        int k=queries[i][2];
-        int cnt=0;
-        for(int j=0;j<26;j++){
-            int val=prefix[r][j];
-            if(l !=0){val=val-prefix[l-1][j];}
-            if(val & 1){cnt++;}
+        vector<bool>ans;
+        int n=s.length();
+        vector<vector<int>>prefix; //prefix vector containing freq array
+        vector<int>arr(26,0);
+        for(int i=0;i<n;i++){
+            arr[s[i]-'a']++;
+            prefix.push_back(arr);
         }
-        if(cnt > (2*k+1))  {ans[i]=false;}
-    }
-    return ans;
+        for(auto q : queries){
+            vector<int>freq(26,0);
+            int l=q[0];
+            int r=q[1];
+            int k=q[2];
+            int cnt=0;
+
+            for(int i=0;i<26;i++){
+                int freq=prefix[r][i];
+                if(l !=0){ freq=freq-prefix[l-1][i]; }
+                if(freq&1){ cnt++; }
+            }
+
+            if(cnt> (2*k+1)){ ans.push_back(false); }
+            else{ ans.push_back(true); }
+            
+        }
+        return ans; 
     }
 };
