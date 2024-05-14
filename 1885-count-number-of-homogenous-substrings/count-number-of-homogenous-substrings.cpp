e107a6d40467287bf1e56363678e11d0af5c37c0
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-14 21:50:45 +0530

    Time: 23 ms (86.71%) | Memory: 13.4 MB (17.39%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-     const int mod = 1e9 + 7;
+    int mod=1e9+7;
     int countHomogenous(string s) {
-        int i=0;
-        int j=0;
-
-        long long ans=0;
-
-        while(j<s.length()){
-            if(s[j]!=s[i]){
-               long long n=j-i;
-        
-               ans=(ans + (n%mod*(n+1)%mod)/2)%mod;
-               i=j;
-            }
-            j++;
-        }
-        long long n=j-i;
-        ans=(ans + (n%mod*(n+1)%mod)/2)%mod;
-
-        return ans;
+       int ans=0;
+       int j=0;
+       for(int i=0;i<s.length();i++){
+         if(s[j] !=s[i]){
+            long long n=i-j;
+            ans=(ans +(n%mod)*((n+1)%mod)/2)%mod;
+            j=i;
+         }
+       }
+       long long n=s.length()-j;
+       ans=(ans +(n%mod)*((n+1)%mod)/2)%mod;
+       return ans;
     }
 };
