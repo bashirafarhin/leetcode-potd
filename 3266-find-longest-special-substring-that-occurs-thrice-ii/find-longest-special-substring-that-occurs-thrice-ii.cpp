0ea2d7b608204c5a9af10f2ff52e89d729a3cd8b
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-15 12:20:30 +0530

    Time: 167 ms (73.48%) | Memory: 62.2 MB (45.84%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-       int maximumLength(string s) {
-        unordered_map<char,vector<int>> mp;
-        int count=0;
-        char lastChar=s[0];
-        for(char c : s){
-            if(lastChar==c){
-                count++;
-            } else {
-                mp[lastChar].push_back(count);
-                count=1;
-                lastChar=c;
-            }
+    int maximumLength(string s) {
+        //mjhe mtlb hai only sustring ki same consecutive frequency se tu sirf usi ka use kro
+        int i=0;
+        int j=0;
+        int n=s.length();
+        unordered_map<char,vector<int>>mp;
+        while(i<n){
+            while(i<n && s[i]==s[j]){ i++; }
+            mp[s[j]].push_back(i-j);
+            j=i;
         }
-        mp[lastChar].push_back(count);
-        
         int ans=-1;
         for(auto it : mp){
-         vector<int> arr=it.second;
-         sort(arr.begin(),arr.end(),greater<int>());
-         int l1=0,l2=0,l3=0;
-
-         if(arr.size()>=1){ l1=arr[0] ;}
-         if (arr.size()>=2){ l2=arr[1] ;}
-         if(arr.size()>=3){  l3=arr[2] ;}
-
-         if(l1==l2 && l3<l1) l2--;
-         if(l1+l2+l3>=3) ans=max({ans,l1-2,l2});
-
+            vector<int>arr=it.second;
+            sort(arr.begin(),arr.end(),greater<int>());
+            int l1=0,l2=0,l3=0;
+            if(arr.size()>=1){l1=arr[0];}
+            if(arr.size()>=2){l2=arr[1];}
+            if(arr.size()>=3){l3=arr[2];}
+            if(l1==l2 && l3<l1){ l2--; }
+            if(l1+l2+l3>=3){ ans=max({ans,l1-2,l2}); }
         }
-        return (ans==0)? -1 : ans;
+        return ans==0 ? -1 : ans ;
     }
 };
