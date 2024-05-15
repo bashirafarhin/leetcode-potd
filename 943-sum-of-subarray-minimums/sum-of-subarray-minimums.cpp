70b3800c38a465d0ab3d54493ecf70c9cec56976
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-15 17:34:25 +0530

    Time: 65 ms (55.34%) | Memory: 46.6 MB (17.07%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
+        /*
+        we need to find contribution of each element on left and right side
+        to build the left and right side array in O(N) we need to use monotonic stack
+        */
         int n=arr.size();
-        vector<int>left(n,1);
-        vector<int>right(n,1);
-
-        stack<pair<int,int>> sleft;
-        stack<pair<int,int>> sright;
+        vector<int>left(n,1),right(n,1);
+        stack<pair<int,int>> str,stl;
 
         for(int i=0;i<n;i++){
            int cnt=1;
-           while( !sleft.empty() && sleft.top().first > arr[i]) {
-               cnt+=sleft.top().second;
-               sleft.pop();
+           while( !stl.empty() && stl.top().first>arr[i]){
+            cnt+=stl.top().second;
+            stl.pop();
            }
-            sleft.push({arr[i],cnt});
-            left[i]=sleft.top().second;
+           stl.push({arr[i],cnt});
+           left[i]=cnt;
         }
 
         for(int i=n-1;i>=0;i--){
            int cnt=1;
-           while( !sright.empty() && sright.top().first >= arr[i]) {
-               cnt+=sright.top().second;
-               sright.pop();
+           while( !str.empty() && str.top().first>=arr[i]){
+            cnt+=str.top().second;
+            str.pop();
            }
-            sright.push({arr[i],cnt});
-            right[i]=sright.top().second;
+           str.push({arr[i],cnt});
+           right[i]=cnt;
         }
 
-        long mod=1000000007;
-        long ans=0;
+        long mod=1e9+7;
+        long sum=0;
         for(int i=0;i<n;i++){
-            ans = (ans + (long)arr[i]*left[i]*right[i] ) % mod;
+            sum=( sum+ (long)arr[i]*left[i]*right[i] )%(mod);
         }
-        return (int)ans;
-        
+        return (int)sum;
+
+
     }
 };
