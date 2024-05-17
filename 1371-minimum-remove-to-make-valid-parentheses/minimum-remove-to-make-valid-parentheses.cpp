8e3e146eac9650676eeab6fa01da55797d7609e5
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-17 22:19:32 +0530

    Time: 19 ms (68.03%) | Memory: 13.2 MB (31.63%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
+    // we need to remove the indices from string which are in stack
     string minRemoveToMakeValid(string s) {
-  stack<int> st;
-  for (auto i = 0; i < s.size(); ++i) {
-    if (s[i] == '(') st.push(i);
-    if (s[i] == ')') {
-      if (!st.empty()) st.pop();
-      else s[i] = '*';
+        int n = s.length();
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            }
+            else if(s[i]==')'){
+                if(!st.empty() && s[st.top()]=='('){ st.pop(); }
+                else{ st.push(i); }
+            }
+        }
+
+        if(st.empty()){ return s; }
+        
+        string ans="";
+        for(int i=n-1;i>=0;i--){
+      
+            if(!st.empty() && st.top()==i){ st.pop(); }
+            else{ ans+=s[i]; }
+        }
+
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
-  }
-  while (!st.empty()) {
-    s[st.top()] = '*';
-    st.pop();
-  }
-  s.erase(remove(s.begin(), s.end(), '*'), s.end());
-  return s;
-}
 };
