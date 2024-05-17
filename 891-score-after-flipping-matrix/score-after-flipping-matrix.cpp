35591d308a3f6db0fe611124f1a4786b8e4d2840
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-17 19:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (50.80%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int matrixScore(vector<vector<int>>& grid) {
-     int Row=grid.size();
-     int Col=grid[0].size();
-     for(int i=0;i<Row;i++){
-         if(grid[i][0]==0){
-             for(int j=0;j<Col;j++){
-              if(grid[i][j]==0){grid[i][j]=1;} else {grid[i][j]=0;}}
-      }
-     }
-     for(int c=0;c<Col;c++){
-         int nz=0;
-         int no=0;
-         for(int r=0;r<Row;r++){
-          if(grid[r][c]==0){nz++;} else {no++;}}
-         
-         if(nz>no){
-          for(int r=0;r<Row;r++){
-           if(grid[r][c]==0){grid[r][c]=1;} else {grid[r][c]=0;}}     
-         }
-     }
-     int sum=0;
-     for(int i=0;i<Row;i++){
-         int num=0;
-         int x=1;
-         for(int j=Col-1;j>=0;j--){
-          num=num+ grid[i][j]*x ;
-          x=x*2;
-         }
-         sum+=num;
-     }
-     return sum;
+        int r=grid.size();
+        int c=grid[0].size();
+        for(int i=0;i<r;i++){
+            if(grid[i][0]==0){
+                //changed whole row
+                for(int j=0;j<c;j++){
+                    grid[i][j]^=1;
+                }
+            }
+        }
+
+        for(int i=0;i<c;i++){
+            int cnt=0; //count 1s
+            for(int j=0;j<r;j++){
+              cnt+= grid[j][i]==1 ? 1 : 0 ;
+            }
+            if(cnt<r-cnt){
+            for(int j=0;j<r;j++){
+              grid[j][i]^=1;}
+            }
+        }
+
+        int ans=0;
+        for(int i=0;i<r;i++){
+            int pow=1;
+            for(int j=c-1;j>=0;j--){
+               ans+=(pow)*grid[i][j];
+               pow*=2;
+            }
+        }
+        return ans;
+        
     }
 };
