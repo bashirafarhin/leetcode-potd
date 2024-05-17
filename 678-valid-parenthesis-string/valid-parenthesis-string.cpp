21db7736d3aa931ff2c91d3874312477d5d6abdd
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-17 21:55:46 +0530

    Time: 3 ms (46.87%) | Memory: 7.4 MB (33.67%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     // string is invalid if at any point close > open
+    //we can see that we are only including open brackets in stacks
 
     bool checkValidString(string s) {
         int n=s.length();
-        stack<pair<char,int>>st,sta; //stack of brackets and asterisks
+        stack<int>st,sta; //stack of brackets and asterisks
  
         for(int i=0;i<n;i++){
-            if(s[i]=='('){ st.push({s[i],i}); }
-            else if(s[i]=='*'){ sta.push({s[i],i}); }
+            if(s[i]=='('){ st.push(i); }
+            else if(s[i]=='*'){ sta.push(i); }
             else{
-                if(!st.empty() && st.top().first=='('){st.pop(); }
+                if(!st.empty()){st.pop(); }
                 else if(!sta.empty()){ sta.pop(); }
                 else{ return false; }
             }
         }
 
         while ( !st.empty() && !sta.empty() ){
-            if(sta.top().second<st.top().second){ return false; }
+            if(sta.top()<st.top()){ return false; }
             st.pop();
             sta.pop();
         }
