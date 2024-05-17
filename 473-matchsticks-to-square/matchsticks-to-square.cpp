c365f879f5e2af2f92f95fc164c0d9dd1b6f9463
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-17 22:25:02 +0530

    Time: 12 ms (90.31%) | Memory: 11.7 MB (95.66%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/matchsticks-to-square.cpp
@@ -1,22 +1,39 @@
 class Solution {
-    bool backtrack(vector<int>&arr,vector<int>&sides,int ind,int side){
-        if(ind==arr.size()){return true;}
-        for(int i=0;i<4;i++){
-            if(sides[i]+arr[ind]<=side){
-             sides[i]+=arr[ind];
-             if(backtrack(arr,sides,ind+1,side)){return true;}
-             sides[i]-=arr[ind];
-            }
-        }
-        return false;
-    }
 public:
+    bool solve(vector<int>&nums,int i,int currSum,int target,int k,vector<int>&vis){
+        
+        if(k<0){
+            return true;
+        }
+        if(currSum==0){
+            return solve(nums,0,target,target,k-1,vis);
+        }
+        if(i==nums.size()){
+            return false;
+        }
+
+        bool take=false;
+        if( !(nums[i]>target || vis[i] || (i>0 && nums[i]==nums[i-1] && !vis[i-1]) ) ){
+            vis[i]=1;
+            take=solve(nums,i+1,currSum-nums[i],target,k,vis);
+            vis[i]=0;
+            if(take){ return true; }
+        }
+
+        return solve(nums,i+1,currSum,target,k,vis);
+
+        
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+       int sum=accumulate(nums.begin(),nums.end(),0);
+       if(sum%k !=0){ return false; }
+       int target=sum/k;
+       vector<int>vis(nums.size(),0);
+       sort(nums.begin(),nums.end(),greater<int>());
+       return solve(nums,0,0,target,k,vis);
+    }
+
     bool makesquare(vector<int>& matchsticks) {
-    int n=matchsticks.size();
-    int sum=accumulate(matchsticks.begin(),matchsticks.end(),0);
-    if(sum%4 !=0 || n<4){return false;}
-    sort(matchsticks.begin(),matchsticks.end(),greater<int>());
-    vector<int> sides(4,0);
-    return backtrack(matchsticks,sides,0,sum/4);
+        return canPartitionKSubsets(matchsticks,4);
     }
 };
