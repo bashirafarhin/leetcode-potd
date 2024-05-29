0481929cd4df9f73970a1e1f70b0d13511afd5e1
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-29 10:25:59 +0530

    Time: 14 ms (5.12%) | Memory: 18.3 MB (5.11%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,5 +1,12 @@
 class Solution {
-
+    bool isnumber1(string s){
+        int n=s.length();
+        if(s[n-1]=='0'){return false;}
+        for(int i=n-2;i>=0;i--){
+         if(s[i]=='1'){return false;}
+        }
+        return true;
+    }
     string addOne(string s){
         int carry=1;
         int n=s.length();
@@ -13,13 +20,18 @@ class Solution {
         reverse(dup.begin(),dup.end());
         return dup;
     }
- 
+    string Divide2(string s){
+        s.pop_back();
+        return s;
+    }
 public:
     int numSteps(string s) {
+     if (s == "1") {return 0;}
     int steps=0;
-    while(s.size()>1){
-        if(s.back()=='1'){s=addOne(s);}
-        else{s.pop_back();}
+    int n=s.length();
+    while(!isnumber1(s)){
+        if(s[s.length()-1]=='1'){s=addOne(s);}
+        else{s=Divide2(s);}
         steps++;
     }
     return steps;
