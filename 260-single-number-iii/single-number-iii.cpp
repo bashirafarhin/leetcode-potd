ce015700acd4fc6583cb975a097d5b47e32ba215
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-05-31 08:28:14 +0530

    Time: 5 ms (79.18%) | Memory: 12.3 MB (64.77%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-     int n=nums.size();
-     int result=0;
-     for(int i=0;i<n;i++){
-      result=result^nums[i];
-     }
-    //  int set_bit_no = result & ~(result - 1);
-     int pos=0;
-     for(int i=0; i<32; i++)
-        {if((result&(1<<i))!=0)
-            {pos = i;
-             break;}
+        int n=nums.size();
+        int result=0;
+        for(int i=0;i<n;i++){
+            result^=nums[i];
         }
-     int x=0,y=0;
-     for(int i=0;i<n;i++){
-       if(nums[i] & (1<<pos)) {
-         x=x^nums[i];}
-       else{
-         y=y^nums[i];}
-     }
-     if(x>y)
-     { return {y,x};}
-     else
-     { return {x,y};}   
+
+        //since the 2 numbers are different , there bits are different
+        //find the 1st position where bits are different
+        //separate the array in two parts on basis of different bit at that position
+
+        int pos=0;
+        for(int i=0;i<32;i++){
+            if(result & 1){
+                pos=i;
+                break;
+            }
+            result>>=1;
+        }
+
+        int x=0,y=0;
+        for(int i=0;i<n;i++){
+            if( nums[i] & (1<<pos) ){ x^=nums[i]; }
+            else { y^=nums[i]; }
+        }
+        return {x,y};
     }
 };
