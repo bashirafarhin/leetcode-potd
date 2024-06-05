8b26823db6a0971e3384f5dc8957b79b3ee068d7
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-05 10:40:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (44.39%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+       vector<int>freq(26,0);
+       int n=words.size();
+       for(char c : words[0]){ freq[c-'a']++; }
+
+       for(int i=1;i<n;i++){
+        vector<int>temp(26,0);
+        for(char ch : words[i]){ temp[ch-'a']++; }
+        for(int i=0;i<26;i++){
+            if(!freq[i] || !temp[i]){ freq[i]=0; }
+            else{ freq[i]=temp[i]<freq[i] ? temp[i] : freq[i]; }
+        }
+       }
+
+       vector<string> ans;
+       for(int i=0;i<26;i++){
+        char ch=i+'a';
+        string st="";
+        st+=ch;
+        while(freq[i]){
+            ans.push_back(st);
+            freq[i]--;
+        }
+       }
+
+       return ans;
+    }
+};
