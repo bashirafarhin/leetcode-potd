5bb80ea3015bc4c89b6c2540d73b28eee62f5a2d
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-06 11:32:24 +0530

    Time: 60 ms (57.08%) | Memory: 30.5 MB (78.12%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n=hand.size();
-        if(n % groupSize==0){
-            sort(hand.begin(),hand.end());
-            vector<int>vis(n,0);
-            for(int i=0;i<n;i++){
-                if(vis[i]==1){ continue ; }
-                    vis[i]=1;
-                    int size=groupSize-1;
-                    int next=hand[i]+1;
-                    int j=i+1;
-                    while(j<n && size>0){
-                        if(hand[j]-next>=2){ return false; }
-                        else if(hand[j]==next && vis[j]==0){
-                            vis[j]=1;
-                            next++;
-                            size--;
-                        }
-                        j++;
-                    }
-                    if(size>0){ return false; }
+        if( n % groupSize ){ return false; }
+        map<int,int>mp;
+        for(int i=0;i<n;i++){ mp[hand[i]]++; }
+
+        for(auto it : mp){
+            int freq=it.second;
+            while(freq !=0){
+             freq--;
+             int next=it.first+1;
+             int size=groupSize-1;
+             while(size){
+                if(mp.find(next)==mp.end() || mp[next]==0){ return false; }
+                mp[next]--;
+                size--;
+                next++;
+             }
             }
-            return true;
         }
-        return false;
+        return true;
     }
 };
