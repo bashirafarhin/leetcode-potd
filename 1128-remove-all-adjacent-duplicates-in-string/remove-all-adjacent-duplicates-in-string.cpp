65d4a51251a5d67d4aef8278a3e6e27bada9e0d9
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-08 09:32:32 +0530

    Time: 20 ms (58.88%) | Memory: 12.2 MB (65.16%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int n=s.length();
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(ans.size()>0 && ans.back()==s[i]){
+                ans.pop_back();
+            } else {
+                ans.push_back(s[i]);
+            } 
+        }
+        return ans;
+    }
+};
