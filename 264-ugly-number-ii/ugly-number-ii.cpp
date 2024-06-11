a107badad936d7e4504b8521ae218e6ead788955
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-11 10:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (73.81%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        //we only have to take multiples of 2,3,5
-        vector<int> dp(n,0);
-        dp[0]=1;
-        int p2=0;
-        int p3=0;
-        int p5=0;
-        for(int i=1;i<n;i++){
+        //observation--->uglyNumber * any of(2,3,5)=uglyNumber
+        vector<int>dp(n+1,0);
+        int p2=1,p3=1,p5=1;
+        dp[1]=1;
+ 
+        for(int i=2;i<=n;i++){
             int twoMultiple=dp[p2]*2;
             int threeMultiple=dp[p3]*3;
             int fiveMultiple=dp[p5]*5;
-            dp[i]=min({ twoMultiple, threeMultiple, fiveMultiple });
 
-            if(dp[i]==twoMultiple){p2++;}
-            if(dp[i]==threeMultiple){p3++;}
-            if(dp[i]==fiveMultiple){p5++;}
+            dp[i]=min( {twoMultiple,threeMultiple,fiveMultiple} );
            
-            
+            if(dp[i]==twoMultiple){ p2++; }
+            if(dp[i]==threeMultiple){ p3++; }
+            if(dp[i]==fiveMultiple){ p5++; }
         }
-        return dp[n-1];
+
+        return dp[n];
     }
 };
