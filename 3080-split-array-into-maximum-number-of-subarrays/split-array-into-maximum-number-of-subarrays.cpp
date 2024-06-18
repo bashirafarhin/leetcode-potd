891e9ea96f9d42848a84eea91cb3a16f2fc983b6
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-18 11:45:00 +0530

    Time: 128 ms (8.54%) | Memory: 107.7 MB (35.68%) - LeetSync

--- 3080-split-array-into-maximum-number-of-subarrays/split-array-into-maximum-number-of-subarrays.cpp
+++ 3080-split-array-into-maximum-number-of-subarrays/split-array-into-maximum-number-of-subarrays.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int maxSubarrays(vector<int>& nums) {
-        // for sum of score to be minimum , each subarray should have min
-        // possible score what is the minimum possible score of subarray? answer
-        // is---> total AND value of arr or less than that
-
-        // observtion
-        // if total AND value of array is non-zero then we cannot split the
-        // array if total AND value of array is  then we can split the array
-        int split = 0;
-        int val = nums[0];
-
-        for (int n : nums) {
-            val &= n;
-        }
-
-        if (val) {
-            return 1;
-        }
-
-        val = (1 << 20) - 1;
-        for (int i = 0; i < nums.size(); i++) {
-            val &= nums[i];
-            if (val == 0) {
+        int n=nums.size();
+        int split=0;
+        int curr=nums[0];
+        for(int i=0;i<n;i++){
+            curr&=nums[i];
+            if(curr==0){
                 split++;
-                val = (1 << 20) - 1;
+                if(i+1<n){ curr=nums[i+1]; }
             }
         }
-        if (val) {
-            return split;
-        }
-
-        return split + 1;
+        if(split==0){ return 1; }
+        if(curr !=0){ return split; }
+        return split;
     }
 };
