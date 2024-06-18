b843d00e9ea85ca6a040ab8e2d79f2512a05d990
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-18 19:56:21 +0530

    Time: 4 ms (42.57%) | Memory: 13.9 MB (12.32%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,38 +1,38 @@
 class Solution {
-    vector<int> solve(string s,unordered_map<string,vector<int>>& dp){
-         if(s.length()==2){
-             int n=(s[0]-'0')*10+(s[1]-'0');
-            return {n};}
-
-        if(s.length()==1){
-            int n=s[0]-'0';
-            return {n};}
-
-        if(dp.count(s)){return dp[s];}
+public:
+    vector<int> solve(string exp, int s, int e) {
+        if (e - s == 1) {
+            int n = (exp[s] - '0') * 10 + (exp[e] - '0');
+            return {n};
+        }
+        if (e == s) {
+            int n = exp[s] - '0';
+            return {n};
+        }
 
         vector<int> ans;
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            if(s[i]=='+' || s[i]=='-' || s[i]=='*'){
-                string left=s.substr(0,i);
-                string right=s.substr(i+1);
-                vector<int>l=solve(left,dp);
-                vector<int>r=solve(right,dp);
-                for(int val1 : l){
-                    for(int val2 : r){
-                        if(s[i]=='+'){ ans.push_back(val1+val2); }
-                        else if(s[i]=='-'){ ans.push_back(val1-val2); }
-                        else if(s[i]=='*'){ ans.push_back(val1*val2); }
+        for (int j = s; j <= e; j++) {
+            if (exp[j] == '+' || exp[j] == '-' || exp[j] == '*') {
+                vector<int> l = solve(exp, s, j - 1);
+                vector<int> r = solve(exp, j + 1, e);
+
+                for (int n1 : l) {
+                    for (int n2 : r) {
+                        if (exp[j] == '+') {
+                            ans.push_back(n1 + n2);
+                        } else if (exp[j] == '-') {
+                            ans.push_back(n1 - n2);
+                        } else {
+                            ans.push_back(n1 * n2);
+                        }
                     }
                 }
             }
         }
-        return dp[s]=ans;
+        return ans;
     }
-public:
     vector<int> diffWaysToCompute(string expression) {
-        unordered_map<string,vector<int>> dp;
-        return solve(expression,dp);
-
+        int n = expression.length();
+        return solve(expression, 0, n - 1);
     }
 };
