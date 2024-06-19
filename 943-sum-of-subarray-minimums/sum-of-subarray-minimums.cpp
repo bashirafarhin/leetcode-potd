df03dbbcd2da030015f18409cabbd5274a3e172d
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-19 18:38:59 +0530

    Time: 132 ms (10.26%) | Memory: 46.5 MB (25.57%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,41 +1,54 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
-        /*
-        we need to find contribution of each element on left and right side
-        to build the left and right side array in O(N) we need to use monotonic stack
-        */
-        int n=arr.size();
-        vector<int>left(n,1),right(n,1);
-        stack<pair<int,int>> str,stl;
+/*
+find contribution of each element
+eg contribution of 1
+ in left subarray {3} {}
+ in right subarray {1} {12} {124}
+ total subarray in which 1 is minimum is 6
+
+
+
+ build left and right subarray which are having contribution of each element on left and right parts
+ 3 1 2 4
+  left [ 1 2 1 1]
+  right [1 3 2 1]
+
+  build these subarrays using stack
+*/
+    int sumSubarrayMins(vector<int>& nums) {
+        int n=nums.size();
+        stack<pair<int,int>>sleft;
+        vector<int>left(n,1);
 
         for(int i=0;i<n;i++){
-           int cnt=1;
-           while( !stl.empty() && stl.top().first>arr[i]){
-            cnt+=stl.top().second;
-            stl.pop();
-           }
-           stl.push({arr[i],cnt});
-           left[i]=cnt;
+            int cnt=1;
+            while(!sleft.empty() && sleft.top().first>nums[i]){
+                cnt+=sleft.top().second;
+                sleft.pop();
+            }
+            sleft.push({nums[i],cnt});
+            left[i]=sleft.top().second;
         }
 
+        stack<pair<int,int>>sright;
+        vector<int>right(n,1);
+
         for(int i=n-1;i>=0;i--){
-           int cnt=1;
-           while( !str.empty() && str.top().first>=arr[i]){
-            cnt+=str.top().second;
-            str.pop();
-           }
-           str.push({arr[i],cnt});
-           right[i]=cnt;
+            int cnt=1;
+            while(!sright.empty() && sright.top().first>=nums[i]){
+                cnt+=sright.top().second;
+                sright.pop();
+            }
+            sright.push({nums[i],cnt});
+            right[i]=sright.top().second;
         }
 
         long mod=1e9+7;
-        long sum=0;
+        long ans=0;
         for(int i=0;i<n;i++){
-            sum=( sum+ (long)arr[i]*left[i]*right[i] )%(mod);
+            ans=( ans+ left[i]*right[i]*(long)nums[i] )%mod;
         }
-        return (int)sum;
-
-
+        return (int)ans;
     }
 };
