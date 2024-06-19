0d3fbd528725678771564659ab10d1d367ac9ba0
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-19 13:09:49 +0530

    Time: 3 ms (98.08%) | Memory: 18.3 MB (70.99%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-     int n=nums.size();
-     vector<int> ans;
-     int cnt1=0;
-     int cnt2=0;
-     int ele1=0;
-     int ele2=0;
-     for(int i=0;i<n;i++){
-         if(cnt1==0 && ele2 !=nums[i]){
-             cnt1++;
-             ele1=nums[i];
-         }
-         else if(cnt2==0 && ele1 !=nums[i]){
-             cnt2++;
-             ele2=nums[i];
-         }
-         else if(ele1==nums[i]){cnt1++;}
-         else if(ele2==nums[i]){cnt2++;}
-         else {cnt1--;cnt2--;}
-     }
-     //check
-     cnt1=0;
-     cnt2=0;
-     for(int i=0;i<n;i++){
-         if(ele1==nums[i]){cnt1++;}
-         else if(ele2==nums[i]){cnt2++;}
-     }
-     int mini=(int)(n/3) +1;
-     if(cnt1>=mini){ans.push_back(ele1);}
-     if(cnt2>=mini){ans.push_back(ele2);}
-     return ans;   
+        int majorityElement1=0;
+        int majorityElement2=0;
+        int count1=0;
+        int count2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(count1==0 && nums[i] !=majorityElement2){
+                majorityElement1=nums[i];
+                count1++;
+            }
+            else if(count2==0 && nums[i] !=majorityElement1){
+                majorityElement2=nums[i];
+                count2++;
+            }
+            else if(nums[i]==majorityElement1){ count1++; }
+            else if(nums[i]==majorityElement2){ count2++; }
+            else { count1-- ; count2--;}
+        }
+
+        //check if candidates finds are true
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==majorityElement1){ count1++; }
+            else if(nums[i]==majorityElement2){ count2++; }
+        }
+
+        vector<int> ans;
+        if(count1>n/3){
+            ans.push_back(majorityElement1);
+        }
+        if(count2>n/3){
+            ans.push_back(majorityElement2);
+        }
+        return ans;
     }
 };
