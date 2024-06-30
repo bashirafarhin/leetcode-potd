03bc27c34784baa39927f8ddc22a07e57a5fd34d
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-06-30 13:44:56 +0530

    Time: 39 ms (61.85%) | Memory: 35.3 MB (17.18%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-      int n=nums.size();
-      unordered_map<int,int>mp;
-      int sum=0,ans=0;
-      mp[0]=1;
-      for(int i=0;i<n;i++){
+       //same as the question find the subarray having sum==k
+       unordered_map<int,int>mp;
+       int sum=0;
+       int ans=0;
+       int n=nums.size();
+       mp[0]=1;
+       for(int i=0;i<n;i++){
         sum+=nums[i];
-        int rem=((sum % k) + k) % k;
+        int rem=(sum % k + k)%k;
+        if(i==0 || i==1){ cout<<rem<<endl; }
         if(rem==0 || mp.find(rem) !=mp.end()){
             ans+=mp[rem];
         }
         mp[rem]++;
-      }
-      return ans;
+       }
+       return ans;
     }
 };
