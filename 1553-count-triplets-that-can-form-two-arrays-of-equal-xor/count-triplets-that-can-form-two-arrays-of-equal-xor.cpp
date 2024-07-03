ce6bc50526391971d29434d6436e9ea7a96aefd8
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-03 11:20:12 +0530

    Time: 60 ms (12.25%) | Memory: 32.7 MB (5.76%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int count = 0;
-        int n = arr.size();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int a = 0;
-                for (int k = i; k < j; k++) {
-                    a ^= arr[k];
+        int n=arr.size();
+        int count=0;
+        for(int k=1;k<n;k++){
+             unordered_map<int,int>left;
+             int l=k-1;
+             int curr=0;
+             while(l>=0){
+                curr^=arr[l];
+                left[curr]++;
+                l--;
+             }
+
+             unordered_map<int,int>right;
+             int r=k;
+             curr=0;
+             while(r<n){
+                curr^=arr[r];
+                right[curr]++;
+                r++;
+             }
+
+             for(auto it : left){
+                if(right.find(it.first) !=right.end()){
+                    count+=(right[it.first]*left[it.first]);
                 }
-                
-                int b = 0;
-                for (int k = j; k < n; k++) {
-                    b ^= arr[k];
-                    if (a == b) {
-                        count++;
-                    }
-                }
-            }
+             }
         }
-        
         return count;
     }
 };
