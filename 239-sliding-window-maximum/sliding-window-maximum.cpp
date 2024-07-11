af157a0746f94af71858e0143828a715f9ba3c00
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-11 11:31:51 +0530

    Time: 208 ms (27.02%) | Memory: 152.4 MB (22.84%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<k;i++){
+            pq.push({nums[i],i});
+        }
         int n=nums.size();
         vector<int>ans;
-        deque<int> dq;
-        for(int i=0;i<k;i++){
-            while( !dq.empty() && nums[dq.back()]<=nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-        }
+        ans.push_back(pq.top().first);
 
         for(int i=k;i<n;i++){
-            ans.push_back(nums[dq.front()]);
-            while( !dq.empty() && nums[dq.back()]<=nums[i]){
-                dq.pop_back();
+            pq.push({nums[i],i});
+
+            while(i-k>=(pq.top().second)){
+                pq.pop();
             }
-            dq.push_back(i);
-            while( !dq.empty() && dq.front()<=i-k){
-                dq.pop_front();
-            }
+
+            ans.push_back(pq.top().first);
         }
-        ans.push_back(nums[dq.front()]);
         return ans;
     }
 };
