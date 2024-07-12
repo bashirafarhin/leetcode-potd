05a9a57cdc1730100cc5617f3c0e4cf74aecba50
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-12 10:27:11 +0530

    Time: 127 ms (55.84%) | Memory: 56.3 MB (33.73%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
-        int subarraysWithKDistinct(vector<int>& A, int K) {
-        return atMostK(A, K) - atMostK(A, K - 1);
-    }
-    int atMostK(vector<int>& A, int K) {
-        int i = 0, res = 0;
-        unordered_map<int, int> count;
-        for (int j = 0; j < A.size(); ++j) {
-            if (!count[A[j]]++) K--;
-            while (K < 0) {
-                if (!--count[A[i]]) K++;
-                i++;
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        unordered_map<int,int>mp1;
+        int n=nums.size();
+        int i=0;
+        int s1=0;
+        int start=0;
+        while(i<n){
+            mp1[nums[i]]++;
+            while(mp1.size()>k){
+                s1+=(n-1-i+1);
+                mp1[nums[start]]--;
+                if(mp1[nums[start]]==0){
+                    mp1.erase(nums[start]);
+                }
+                start++;
             }
-            res += j - i + 1;
+            i++;
         }
-        return res;
+
+        unordered_map<int,int>mp2;
+        i=0;
+        int s2=0;
+        start=0;
+        while(i<n){
+            mp2[nums[i]]++;
+            while(mp2.size()==k){
+                mp2[nums[start]]--;
+                if(mp2[nums[start]]==0){
+                    mp2.erase(nums[start]);
+                }
+                start++;
+            }
+            s2+=(i-start+1);
+            i++;
+            
+        }
+
+        int t=n*(n+1)/2;
+        cout<<s1<<" "<<s2;
+        return t-s1-s2;
     }
 };
