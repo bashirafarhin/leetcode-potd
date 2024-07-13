32ee70a086fb9658953819f291399b47c33c2d2f
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-13 11:12:05 +0530

    Time: 36 ms (34.32%) | Memory: 17.5 MB (12.73%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-     vector<vector<int>> dp;
-    int solve(vector<int> &nums1, vector<int> &nums2, int i , int j)
-    {
-        if(i == nums1.size() or j == nums2.size()) return 0;
-        int a = INT_MIN;
-        int b = INT_MIN;
-        int c = INT_MIN;
-        int d = INT_MIN;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        a = solve(nums1,nums2,i+1,j+1) + nums1[i]*nums2[j];
-        b = solve(nums1,nums2,i+1,j);
-        c = solve(nums1,nums2,i,j+1);
-        // d = solve(nums1,nums2,i+1,j+1);
-
-        return dp[i][j] = max(a,max(b,c));
-    }
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        dp.resize(nums1.size(),vector<int>(nums2.size(),-1));
-        int x = solve(nums1,nums2,0,0);
-        if(x == 0)
-        {
-            int res = INT_MIN;
-            for(int i = 0 ; i < nums1.size() ; i++)
-            {
-                for(int j = 0 ; j < nums2.size() ; j++)
-                {
-                    res = max(nums1[i]*nums2[j],res);
-                }
-            }
-            return res;
+    int solve(int i,int j,vector<int>& nums1, vector<int>& nums2,vector<vector<int>>&dp){
+        if(i==nums1.size() || j==nums2.size()){ return INT_MIN; }
+        if(dp[i][j] !=INT_MIN){ return dp[i][j]; }
+        int res=solve(i+1,j+1,nums1,nums2,dp);
+        int a=nums1[i]*nums2[j];
+        if(res !=INT_MIN){
+            res+=a;
         }
-        return x;
+        int b=solve(i+1,j,nums1,nums2,dp);
+        int c=solve(i,j+1,nums1,nums2,dp);
+        return dp[i][j]=max({a,b,c,res});
+    }
+
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,INT_MIN));
+        return solve(0,0,nums1,nums2,dp);
     }
 };
