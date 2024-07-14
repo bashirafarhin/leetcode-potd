3d8e316b1e4f1a653775c3dc9410a72c39eb4f8a
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-14 11:20:49 +0530

    Time: 87 ms (40.92%) | Memory: 20.5 MB (38.28%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -1,39 +1,35 @@
 class Solution {
-    /*store path is path is visited don`t go over there*/
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        int n=graph.size();
-        if(n<=1){return 0;}
-        set<pair<int,int>> vis;
-        queue<pair<int,int>> q;
-        int allVis=0;
-        /*starting bfs from every node as it is mentioned we can start from any node*/
-        /*pushing starting node and its path*/
-        for(int i=0;i<n;i++){
-         int path=1<<i;
-         allVis=allVis | path;
-         q.push({i,path});
-         vis.insert({i,path});
+        int v=graph.size();
+        int destination=(1<<v)-1;
+        set<pair<int,int>>vis;
+        int dist=0;
+
+        queue<pair<int,int>>q;
+        for(int i=0;i<v;i++){
+            int path=1<<i;
+            q.push({i,path});
+            vis.insert({i,path});
         }
 
-        int minPath=0;
-        while(!q .empty()){
-          int size=q.size();
-          minPath++;
-          while(size--){
-              int node=q.front().first;
-              int path=q.front().second;
-              q.pop();
-              for(auto neigh : graph[node]){
-                  int nextpath=path | (1<<neigh);
-                  if(nextpath==allVis){
-                      return minPath;}
-                  if(vis.find({neigh, nextpath})==vis.end()){
-                      vis.insert({neigh, nextpath});
-                      q.push({neigh, nextpath});}
-              }
-          }
+        while(!q.empty()){
+            int size=q.size();
+            dist++;
+            while(size--){
+                int node=q.front().first;
+                int currPath=q.front().second;
+                q.pop();
+                for(int neigh : graph[node]){
+                    int path= currPath | (1<<neigh);
+                    if(path==destination){ return dist; }
+                    if(vis.find({neigh,path})==vis.end()){
+                        vis.insert({neigh,path});
+                        q.push({neigh,path});
+                    }
+                }
+            }
         }
-        return -1;
+        return 0;
     }
 };
