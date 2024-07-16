2d3b0c1ed4885732ab9d27ae2c3053ac7edb0189
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-16 09:31:48 +0530

    Time: 51 ms (81.80%) | Memory: 55.9 MB (17.02%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        //my answer will be from 1 to n
-        int n=nums.size();
+       int n=nums.size();
+       vector<int>vis(n+1,0);
 
-        for(int i=0;i<n;i++){
-            //we are removing negative numbers and numbers >n as these will never be our answer
-            if(nums[i]<=0 || nums[i]>n){ nums[i]=n+1; }
+       for(int i=0;i<n;i++){
+        if( 1<=nums[i]&& nums[i]<=n ){
+            vis[nums[i]]=1;
         }
+       }
 
-        for(int i=0;i<n;i++){
-            
-            int ind=abs(nums[i])-1;
-
-            if(ind>n-1){ continue; }
-
-            if(nums[ind]>0){
-            nums[ind]=-1*nums[ind];
-            }
+       for(int i=1;i<=n;i++){
+        if(!vis[i]){
+            return i;
         }
-
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){ return i+1; }
-        }
-
-        return n+1;
+       }
+       return n+1;
     }
 };
