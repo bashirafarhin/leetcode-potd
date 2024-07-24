61d2824fe5e19bfdd28e50eb7e6b0c6c5b2f4257
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-24 14:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (10.87%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.cpp
+++ 1185-find-in-mountain-array/find-in-mountain-array.cpp
@@ -10,53 +10,52 @@
 
 class Solution {
 public:
-    int findInLeft(int target, MountainArray &mountainArr,int low, int high){
-         //left part is sorted in increasing order
-         while(low<=high){
-             int mid=low+(high-low)/2;
-             int val =mountainArr.get(mid);
-             if(val==target) return mid;
-             else if( val>target) high =mid-1;
-             else low=mid+1;
-         }
-         return -1;
-    }
-    int findInRight(int target, MountainArray &mountainArr,int low, int high){
-      //right part is sorted in decreasing order
-         while(low<=high){
-             int mid=low+(high-low)/2;
-             int val =mountainArr.get(mid);
-             if(val==target) return mid;
-             else if(val<target) high =mid-1;
-             else low=mid+1;
-         }
-         return -1;
+    int peakIndex(MountainArray &mountainArr){
+        int l=0;
+        int h=mountainArr.length()-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(mountainArr.get(m)<=mountainArr.get(m+1)){
+                l=m+1;
+            } else {
+                h=m-1;
+            }
+        }
+        return l;
     }
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        //finding peak index first 
-        int n=mountainArr.length();
-        int peakIndex;
-        int low=0, high =n-1;
-         while(low<=high){
-            int mid=low+(high-low)/2;
-            int val =mountainArr.get(mid);
-            if(mid==0||val<mountainArr.get(mid+1))
-            low=mid+1;
-            else if(mid==n-1|| mountainArr.get(mid-1)>val) high=mid-1;
-            else 
-             {  
-                 peakIndex=mid;
-                 break;
-             }
+        // Peak Index in a Mountain Array
+        int ind=peakIndex(mountainArr);
+        int l=0;
+        int h=ind;
+        int ans=-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(mountainArr.get(m)==target){
+                ans=m;
+            }
+            if(target<=mountainArr.get(m)){
+                h=m-1;
+            } else {
+                l=m+1;
+            }
         }
-        if(target==mountainArr.get(peakIndex)) return peakIndex;
-        //now finding target in left of the array from 0 to peakIndex-1
-        int leftInd= findInLeft(target,mountainArr,0,peakIndex-1);
-        //now finding target in right of the array from peakIndex+1 to n-1
-        int rightInd= findInRight(target,mountainArr,peakIndex+1,n-1);
-        
-        if(leftInd!=-1) return leftInd;
-        return rightInd!=-1 ? rightInd: -1;
-    }
+        if(ans !=-1){ return ans; }
 
+        l=ind+1;
+        h=mountainArr.length()-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(mountainArr.get(m)==target){
+                return m;
+            }
+            if(target<=mountainArr.get(m)){
+                l=m+1;
+            } else {
+                h=m-1;
+            }
+        }
+        return ans;
+
+    }
 };
