b7d6515d01abdffcf8a91655cd72aa3c4706ec63
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-27 11:07:24 +0530

    Time: 65 ms (13.90%) | Memory: 10 MB (17.93%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
+    int helper(int i,int d,vector<int>&arr,vector<vector<int>>&dp){
+        if(d==1){
+            if(i !=0){ i--; }
+            return *max_element(arr.begin()+i,arr.end());
+        }
+        if(arr.size()-i<d){ return 1e9; }
+        if(dp[i][d] !=-1){
+            return dp[i][d];
+        }
+        int res=1e9;
+        int n=arr.size();
+        int currDiff=arr[i];
+        for(int j=i;j<n;j++){
+            currDiff=max(currDiff,arr[j]);
+            res=min(res,currDiff+helper(j+1,d-1,arr,dp));
+        }
+        return dp[i][d]=res;
+    }
     int minDifficulty(vector<int>& jobDifficulty, int d) {
-     int n=jobDifficulty.size();
-     if(n<d){return -1;}
-     vector<vector<int>> dp(n+1,vector<int>(d+1,INT_MAX));
-     dp[n][0]=0;
-     for(int i=n;i>=0;i--){
-         for(int day=1;day<=d;day++){
-             int ans=INT_MAX;
-             int maxDifficulty=0; //max jobDifficulty done on d day
-             for(int j=i;j<n;j++){
-              maxDifficulty=max(maxDifficulty ,jobDifficulty[j]);
-              if(dp[j+1][day-1] !=INT_MAX){
-                ans=min(dp[j+1][day-1]+maxDifficulty , ans);}
-             }
-             dp[i][day]=ans;
-           }
-     }
-     return dp[0][d];
+      int n=jobDifficulty.size();
+      vector<vector<int>>dp(n,vector<int>(d+1,-1));
+      int res=helper(0,d,jobDifficulty,dp);  
+      return res>=1e9 ? -1 : res;
     }
 };
