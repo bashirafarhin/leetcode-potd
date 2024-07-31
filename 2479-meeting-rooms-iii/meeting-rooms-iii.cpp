025bba84dc2f48881616f6875b368c4317a694f1
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-07-31 13:13:47 +0530

    Time: 324 ms (47.77%) | Memory: 110.6 MB (27.31%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,71 +1,46 @@
-bool compare(vector<int>& v1, vector<int>& v2) {
-    return v1[0] < v2[0];
-}
 class Solution {
 public:
+// sort rooms
+// find rooms for the current meeting
+// it can be empty rooms or take the room which finishes earliest
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        /* Sort the meetings based on start_time */
-        sort(meetings.begin(), meetings.end(), compare);
-        
-        /* Create a priority queue for engaged rooms. Each node of PQ will store {current_meeting_ending_time, room_number} */
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> engaged;
-        
-        /* Create a priority queue for non-engaged rooms. Each node of PQ will store {room_number} */
-        priority_queue<int, vector<int>, greater<int>> unused;
-        
-        /* Frequency map to store the frequency of meetings in each room */
-        unordered_map<int, int> freq;
-        
-        /* Currently all the rooms are mepty */
-        for(int i = 0; i < n; i++) {
-            unused.push(i);
-        }
-        
-        for(auto x:meetings) {
-            int s = x[0], e = x[1];
-            
-            /* Move the meeting rooms in engaged, with ending_time <= s, to unused */ 
-            while(engaged.size() > 0 && engaged.top().first <= s) {
-                int room = engaged.top().second;
-                engaged.pop();
-                
-                unused.push(room);
+        sort(meetings.begin(),meetings.end());
+        vector<int>numMeetingOfRoom(n,0);
+        vector<long long>endTimeOfRoom(n,0);
+        int m=meetings.size();
+        for(auto meeting : meetings){
+            int start=meeting[0];
+            int end=meeting[1];
+            bool found=false;
+            int roomFinishesEarliest=0;
+            long long earliestTimeofFinish=LLONG_MAX;
+            for(int room =0;room<n;room++){
+                if(endTimeOfRoom[room]<=start){
+                    numMeetingOfRoom[room]++;
+                    endTimeOfRoom[room]=end;
+                    found=true;
+                    break;
+                }
+                if(endTimeOfRoom[room]<earliestTimeofFinish){
+                    roomFinishesEarliest=room;
+                    earliestTimeofFinish=endTimeOfRoom[room];
+                }
             }
-            
-            /* If there are multiple empty rooms, choose the one with lower room_number */
-            if(unused.size() > 0) 
-            {
-                int room = unused.top();
-                unused.pop();
-                
-                freq[abs(room)] += 1;
-                
-                /* Mark the room as engaged */
-                engaged.push({e, room});
-            }
-            /* If there are no empty rooms, wait for the engaged room with nearest ending time to empty */
-            else 
-            {
-                pair<long long, int> topmost = engaged.top();
-                engaged.pop();
-            
-                freq[abs(topmost.second)] += 1;
-                
-                /* Since duration has to be the same, the newEnd will be sum(end_time_of_the_prev_meeting, duration) */ 
-                long long newEnd = topmost.first;
-                newEnd += (e - s);
-                
-                /* Mark the room as engaged */
-                engaged.push({newEnd, topmost.second});
+            if(!found){
+                int duration=end-start;
+                numMeetingOfRoom[roomFinishesEarliest]++;
+                endTimeOfRoom[roomFinishesEarliest]+=duration;
             }
         }
-        
-        /* Find the lowest room_number with maximum frequency */
-        int maxi = 0;
-        for(int i = 1; i < n; i++) {
-            if(freq[i] > freq[maxi])
-                maxi = i;
+
+        int mostUsedRoom=-1;
+        int count=0;
+        for(int room=0;room<n;room++){
+            if(numMeetingOfRoom[room]>count){
+                count=numMeetingOfRoom[room];
+                mostUsedRoom=room;
+            }
         }
-        return maxi;
+        return mostUsedRoom;
     }
 };
