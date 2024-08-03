0e02eee460d6dc7c4348f5858c0f742e58a251dc
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-08-03 12:14:09 +0530

    Time: 344 ms (19.54%) | Memory: 109.4 MB (30.52%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-       const int n = (int) nums.size();
-        auto ar = nums;
-        sort(ar.begin(), ar.end());
-        ar.erase(unique(ar.begin(), ar.end()), ar.end());
-
-        const int sz = (int) ar.size();
-        int ans = n;
-        for (int i = 0; i < sz; i++) {
-            int l = ar[i];
-            int r = l + n;
-            int elem = lower_bound(ar.begin(), ar.end(), r) - (ar.begin() + i);
-            ans = min(ans, n - elem);
+        //sorted and unique
+        set<int>st(nums.begin(),nums.end());
+        vector<int>temp(st.begin(),st.end());
+        int n=nums.size();
+        int ans=n;
+        for(int i=0;i<temp.size();i++){
+            int left=i;
+            int right=temp[i]+n-1;
+            int within_range=(upper_bound(temp.begin(),temp.end(),right)-temp.begin())-i;
+            int out_of_range=n-within_range;
+            ans=min(ans,out_of_range);
         }
-
-        return ans;  
+        return ans;
     }
 };
