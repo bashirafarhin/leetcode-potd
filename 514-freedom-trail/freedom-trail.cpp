3d41991dedb364aa07ba1009fc6c552fb8e30c79
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-08-09 09:36:36 +0530

    Time: 121 ms (27.85%) | Memory: 78.5 MB (11.85%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
+// TC m^n
+// with dp m*n*n
+    int helper(int ringIndex,int keyIndex,string ring, string key,vector<vector<int>>&dp){
+        if(keyIndex==key.size()){
+            return 0;
+        }
+        if(dp[ringIndex][keyIndex] !=-1){
+            return dp[ringIndex][keyIndex];
+        }
+        int n=ring.size();
+        int res=INT_MAX;
+        for(int i=0;i<n;i++){
+           if(ring[i]==key[keyIndex]){
+            int anti=abs(i-ringIndex);
+            int clockwise=ring.size()-anti;
+            int steps=1+min(anti,clockwise);
+            int totalSteps=steps+helper(i,keyIndex+1,ring,key,dp);
+            res=min(res,totalSteps);
+           }
+        }
+        return dp[ringIndex][keyIndex]=res;
+    }
     int findRotateSteps(string ring, string key) {
-        int size = ring.size();
-        int ksize = key.size();
-        unordered_map<char,vector<int>> mp;//stored index of each characters in ring,pay attention to duplcate characters.
-        for(int i=0;i<size;++i){
-            mp[ring[i]].push_back(i);
-        }
-        
-        vector<vector<int>> dp(ksize+1,vector<int> (size,INT_MAX));// initializing dp vector 
-        fill(dp[0].begin(),dp[0].end(),0);
-        
-        vector<int> tmp(1,0);// starting index
-        
-        int res = INT_MAX;
-        for(int i=1;i<=ksize;++i){
-            for(auto it:mp[key[i-1]]){  //
-                for(int j=0;j<tmp.size();++j){  //Search The shortest distance key[i-1] in ring
-                    int minDist = min((tmp[j] + size -it)%size,(it + size - tmp[j])%size) + dp[i-1][tmp[j]];// Look at the above explanation
-                    dp[i][it] =min(dp[i][it],minDist);
-                    res = (i!=ksize?res:min(res,dp[i][it])); //Can we optimize it?
-                }
-            }
-            tmp = mp[key[i-1]]; //next start is the characters we search in this time
-        }
-        return res + ksize;
+        int n=ring.size();
+        int m=key.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return helper(0,0,ring,key,dp);
     }
 };
