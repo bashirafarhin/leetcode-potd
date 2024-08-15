c4e2b5606fe5b7b536414d42d3863eb1593821c9
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-08-15 10:18:50 +0530

    Time: 191 ms (85.97%) | Memory: 82.2 MB (97.30%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-      vector<int> ans;
-        vector<int> start;
-        vector<int> end;
-        for(auto &nums:flowers){
-            start.push_back(nums[0]);
-            end.push_back(nums[1]);
+        int n=people.size();
+        int m=flowers.size();
+        vector<int>startTime(m,0);
+        vector<int>endTime(m,0);
+        for(int i=0;i<m;i++){
+            startTime[i]=flowers[i][0];
+            endTime[i]=flowers[i][1];
         }
-        sort(start.begin(),start.end());
-        sort(end.begin(),end.end());
-        for(auto &p:people){
-            auto bloombeforeappearanceofpeople= upper_bound(start.begin(),start.end(),p)-start.begin();
-            auto deadbeforeappearanceofpeople=lower_bound(end.begin(),end.end(),p)-end.begin();
-            ans.push_back(bloombeforeappearanceofpeople-deadbeforeappearanceofpeople);
+        sort(startTime.begin(),startTime.end());
+        sort(endTime.begin(),endTime.end());
+
+        vector<int>ans(n,0);
+        for(int i=0;i<n;i++){
+            int numOfBloomFlowers=upper_bound(startTime.begin(),startTime.end(),people[i])-startTime.begin();
+            int numOfDiedFlowers=lower_bound(endTime.begin(),endTime.end(),people[i])-endTime.begin();
+            ans[i]=numOfBloomFlowers-numOfDiedFlowers;
         }
-        return ans;   
+        return ans;
     }
 };
