8ccb43cc0a882c8ce9222c1280b99ba7b6ec7087
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-08-17 10:27:07 +0530

    Time: 51 ms (83.71%) | Memory: 216.8 MB (25.72%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    bool canCross(vector<int>& stones) {
-       int N = stones.size();
-        vector<vector<bool>> dp(N, vector<bool> (N+1, false));
-        dp[0][1] = true;
-        
-        for(int i = 1; i < N; ++i){
-            for(int j = 0; j < i; ++j){
-                int diff = stones[i] - stones[j];
-                if(diff > N || !dp[j][diff]) 
-                    continue;
-                
-                if(i == N - 1) 
-                    return true;
-                
-                dp[i][diff] = true;
-                if(diff - 1 >= 0) dp[i][diff - 1] = true;
-                if(diff + 1 <= N) dp[i][diff + 1] = true;
+    int helper(int i,int k,vector<int>& stones,unordered_map<int,int>& mp,vector<vector<int>>&dp){
+        if(i>=stones.size()-1){
+            return true;
+        }
+        if(dp[i][k] !=-1){
+            return dp[i][k];
+        }
+        for(int j=k-1;j<=k+1;j++){
+            if(j<=0){
+                continue;
+            }
+            int nextStone=stones[i]+j;
+            if(mp.find(nextStone) !=mp.end()){
+                if(helper(mp[nextStone],j,stones,mp,dp)){
+                    return dp[i][k]=1;
+                }
             }
         }
+        return dp[i][k]=0;
+    }
 
-        return false; 
+    bool canCross(vector<int>& stones) {
+       if(stones[1]>=2){
+        return false;
+       }
+       unordered_map<int,int>mp;
+       for(int i=0;i<stones.size();i++){
+        mp[stones[i]]=i;
+       }
+       
+       int n=stones.size();
+       vector<vector<int>>dp(n,vector<int>(n,-1));
+       return helper(0,0,stones,mp,dp); 
     }
 };
