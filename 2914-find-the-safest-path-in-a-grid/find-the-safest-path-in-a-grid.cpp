581f3012605e9d33bd3362482540711c285764c9
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-08-18 13:10:12 +0530

    Time: 571 ms (41.71%) | Memory: 256.9 MB (16.88%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    bool canFind(int sf,vector<vector<int>>& grid){
+        if(grid[0][0]<sf){
+            return false;
+        }
+        int r=grid.size();
+        int c=grid[0].size();
+        vector<vector<int>>dxy={{-1,0},{0,1},{1,0},{0,-1}};
+        vector<vector<int>>vis(r,vector<int>(c,0));
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vis[0][0]=1;
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            if(x==r-1 && y==c-1){
+                return true;
+            }
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nx=dxy[i][0]+x;
+                int ny=dxy[i][1]+y;
+                if(nx>=0 && nx<r && ny>=0 && ny<c && !vis[nx][ny] && grid[nx][ny]>=sf){
+                    vis[nx][ny]=1;
+                    q.push({nx,ny});
+                }
+            }
+        }
+        return false;
+    }
+
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
@@ -7,52 +37,43 @@ public:
 
         //preComputing safeness factor of each cell
         vector<vector<int>>safenessFactor(r,vector<int>(c,1e9));
-        queue<pair<int,int>>sq;
+        queue<pair<int,int>>q;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j]==1){
                     safenessFactor[i][j]=0;
-                    sq.push({i,j});
+                    q.push({i,j});
                 }
             }
         }
-        while(!sq.empty()){
-            int x=sq.front().first;
-            int y=sq.front().second;
-            sq.pop();
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
             for(int i=0;i<4;i++){
                 int nx=dxy[i][0]+x;
                 int ny=dxy[i][1]+y;
                 if(nx>=0 && nx<r && ny>=0 && ny<c && safenessFactor[nx][ny]==1e9){
                     safenessFactor[nx][ny]=safenessFactor[x][y]+1;
-                    sq.push({nx,ny});
+                    q.push({nx,ny});
                 }
             }
         }
 
         //calculating ans
-        priority_queue<pair<int,pair<int,int>>>pq;
-        vector<vector<int>>vis(r,vector<int>(c,0));
-        pq.push({safenessFactor[0][0],{0,0}});
-        vis[0][0]=1;
-        while(!pq.empty()){
-            int x=pq.top().second.first;
-            int y=pq.top().second.second;
-            int sf=pq.top().first;
-            pq.pop();
-            if(x==r-1 && y==c-1){
-                return sf;
-            }
-            for(int i=0;i<4;i++){
-                int nx=dxy[i][0]+x;
-                int ny=dxy[i][1]+y;
-                if(nx>=0 && nx<r && ny>=0 && ny<c && !vis[nx][ny]){
-                    vis[nx][ny]=1;
-                    int nextSf=min(sf,safenessFactor[nx][ny]);
-                    pq.push({nextSf,{nx,ny}});
-                }
+        int ans=0;
+        int start=0;
+        int end=400;
+        while(start<=end){
+            int mid=(start+end)/2;
+            //check for safeness factor==mid
+            if(canFind(mid,safenessFactor)){
+                ans=mid;
+                start=mid+1;
+            } else {
+                end=mid-1;
             }
         }
-        return 0;
+        return ans;
     }
 };
