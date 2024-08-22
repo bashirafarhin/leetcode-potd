d8ffd1b92306be2d13349e2c8987c3ddbcdd7d26
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-08-22 08:40:48 +0530

    Time: 166 ms (79.45%) | Memory: 54 MB (82.29%) - LeetSync

--- 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.cpp
+++ 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    int helper(int l1,int l2,int i,vector<int>&nums,vector<vector<vector<int>>>&dp){
-        if(i==nums.size()){
+    int mod = 1e9 + 7;
+    int helper(int prev1, int i, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == nums.size()) {
             return 1;
         }
-        if(dp[l1][l2][i] !=-1){
-            return dp[l1][l2][i];
+        if (dp[prev1][i] != -1) {
+            return dp[prev1][i];
         }
-        int ans=0;
-        int j=max(l1,nums[i]-l2);
-        for(;j<=nums[i];j++){
-            if(nums[i]-j<=l2){
-                ans=(ans%mod+helper(j,nums[i]-j,i+1,nums,dp)%mod)%mod;
+        int ans = 0;
+        int prev2 = i == 0 ? nums[0] : nums[i - 1] - prev1;
+        for (int j = max(prev1, nums[i] - prev2); j <= nums[i]; j++) {
+            if (nums[i] - j <= prev2) {
+                ans = (ans % mod + helper(j, i + 1, nums, dp) % mod) % mod;
             } else {
                 break;
             }
         }
-        return dp[l1][l2][i]=ans;
-
+        return dp[prev1][i] = ans;
     }
     int countOfPairs(vector<int>& nums) {
-        vector<vector<vector<int>>>dp(50+1,vector<vector<int>>(50+1,vector<int>(2000,-1)));
-        return helper(0,50,0,nums,dp);
+        int n = nums.size();
+        vector<vector<int>> dp(50 + 1, vector<int>(n, -1));
+        return helper(0, 0, nums, dp);
     }
 };
