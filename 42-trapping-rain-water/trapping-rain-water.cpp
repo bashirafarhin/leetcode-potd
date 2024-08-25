d47e3514046cf5e8c01cb4b8d73aba10ce243e6f
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-08-25 09:58:39 +0530

    Time: 14 ms (32.44%) | Memory: 25.5 MB (13.34%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,22 +3,18 @@ public:
     int trap(vector<int>& height) {
         // min(leftMax,rightMax)-arr[i]=water units stored by arr[i]
         int n=height.size();
-        vector<int>leftMax(n,-1);
         vector<int>rightMax(n,-1);
-        int maxHeight=height[0];
-        for(int i=1;i<n;i++){
-            maxHeight=max(maxHeight,height[i]);
-            leftMax[i]=maxHeight;
-        }
-        maxHeight=height[n-1];
+        int maxHeight=height[n-1];
         for(int i=n-2;i>=0;i--){
             maxHeight=max(maxHeight,height[i]);
             rightMax[i]=maxHeight;
         }
         int result=0;
+        int leftMax=height[0];
         for(int i=0;i<n;i++){
-            if(height[i]<leftMax[i] && height[i]<rightMax[i]){
-                int waterUnits=min(leftMax[i],rightMax[i])-height[i];
+            leftMax=max(leftMax,height[i]);
+            if(height[i]<leftMax && height[i]<rightMax[i]){
+                int waterUnits=min(leftMax,rightMax[i])-height[i];
                 result+=waterUnits;
             }
         }
