f1205fa94ca54d63d53bd5420da0096848cb8d06
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-02 11:56:57 +0530

    Time: 10 ms (86.44%) | Memory: 12.4 MB (68.00%) - LeetSync

--- 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
     bool isVowel(char& ch) {
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||
-                ch == 'u') == true;
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') == true;
     }
+
+    int helper(string& word, unordered_map<char, int> mp, int start, int cons, int k) {
+        int result=0;
+        while (cons == k && mp.size() == 5) {
+            result++;
+            char st = word[start];
+            if (isVowel(st)) {
+                mp[st]--;
+                if (mp[st] == 0) {
+                    mp.erase(st);
+                }
+            } else {
+                cons--;
+            }
+            start++;
+        }
+        return result;
+    }
+
 
     int countOfSubstrings(string word, int k) {
         int n = word.length();
@@ -12,40 +30,29 @@ public:
         int start = 0;
         int ans = 0;
 
-        for(int end=0;end<n;end++){
-            char ch=word[end];
-            if(isVowel(ch)){
+        for (int end = 0; end < n; end++) {
+            char ch = word[end];
+            if (isVowel(ch)) {
                 mp[ch]++;
             } else {
                 cons++;
             }
 
-            while(cons>k){
+            while (cons > k) {
                 char st = word[start];
-                if(isVowel(st)){
+                if (isVowel(st)) {
                     mp[st]--;
-                    if(mp[st]==0){ mp.erase(st); }
+                    if (mp[st] == 0) {
+                        mp.erase(st);
+                    }
                 } else {
                     cons--;
                 }
                 start++;
             }
 
-            if(cons==k && mp.size()==5){
-                unordered_map<char,int>tempMap=mp;
-                int tempStart=start;
-                int tempCons=cons;
-                while(tempCons==k && tempMap.size()==5){
-                    ans++;
-                    char tempSt=word[tempStart];
-                    if(isVowel(tempSt)){
-                        tempMap[tempSt]--;
-                        if(tempMap[tempSt]==0){ tempMap.erase(tempSt); }
-                    } else {
-                        tempCons--;
-                    }
-                    tempStart++;
-                }
+            if (cons == k && mp.size() == 5) {
+                ans+=helper(word, mp, start, cons, k);
             }
         }
         return ans;
