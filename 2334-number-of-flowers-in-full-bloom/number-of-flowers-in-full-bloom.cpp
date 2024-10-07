1bbc682079fc21486859806f44de651f6b8e39c4
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-07 12:11:32 +0530

    Time: 191 ms (81.59%) | Memory: 83.8 MB (91.01%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -1,22 +1,55 @@
 class Solution {
 public:
+    int flowersBloom(int time, vector<int>&timesArr){
+        int l=0;
+        int h=timesArr.size()-1;
+        int ans=0;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(timesArr[m]<=time){
+                ans=m+1;
+                l=m+1;
+            } else {
+                h=m-1;
+            }
+        }
+        return ans;
+    }
+
+    int flowersDead(int time, vector<int>&timesArr){
+        int l=0;
+        int h=timesArr.size()-1;
+        int ans=0;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(timesArr[m]<time){
+                ans=m+1;
+                l=m+1;
+            } else {
+                h=m-1;
+            }
+        }
+        return ans;
+    }
+
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
         int n=people.size();
         int m=flowers.size();
-        vector<int>startTime(m,0);
-        vector<int>endTime(m,0);
+        vector<int>startTime(m);
+        vector<int>endTime(m);
         for(int i=0;i<m;i++){
             startTime[i]=flowers[i][0];
             endTime[i]=flowers[i][1];
         }
+
         sort(startTime.begin(),startTime.end());
         sort(endTime.begin(),endTime.end());
 
-        vector<int>ans(n,0);
+        vector<int>ans;
         for(int i=0;i<n;i++){
-            int numOfBloomFlowers=upper_bound(startTime.begin(),startTime.end(),people[i])-startTime.begin();
-            int numOfDiedFlowers=lower_bound(endTime.begin(),endTime.end(),people[i])-endTime.begin();
-            ans[i]=numOfBloomFlowers-numOfDiedFlowers;
+            int b=flowersBloom(people[i],startTime);
+            int d=flowersDead(people[i],endTime);
+            ans.push_back(b-d);
         }
         return ans;
     }
