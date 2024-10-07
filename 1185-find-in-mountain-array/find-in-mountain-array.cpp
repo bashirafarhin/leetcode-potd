48210551a633ce1553621fdea138ee5bd0ab99dd
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-07 10:19:49 +0530

    Time: 6 ms (41.92%) | Memory: 8.7 MB (48.13%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.cpp
+++ 1185-find-in-mountain-array/find-in-mountain-array.cpp
@@ -10,52 +10,61 @@
 
 class Solution {
 public:
-    int peakIndex(MountainArray &mountainArr){
-        int l=0;
-        int h=mountainArr.length()-1;
-        while(l<=h){
-            int m=(l+h)/2;
-            if(mountainArr.get(m)<=mountainArr.get(m+1)){
-                l=m+1;
+    int incArr(int l, int h, int& target, MountainArray& arr) {
+        int ans = -1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (arr.get(m)==target) { return m; }
+            if (arr.get(m) > target) {
+                    h = m-1;
             } else {
-                h=m-1;
+                l = m+1;
             }
         }
-        return l;
+        return -1;
     }
-    int findInMountainArray(int target, MountainArray &mountainArr) {
-        // Peak Index in a Mountain Array
-        int ind=peakIndex(mountainArr);
-        int l=0;
-        int h=ind;
-        int ans=-1;
-        while(l<=h){
-            int m=(l+h)/2;
-            if(mountainArr.get(m)==target){
-                ans=m;
-            }
-            if(target<=mountainArr.get(m)){
-                h=m-1;
-            } else {
-                l=m+1;
-            }
-        }
-        if(ans !=-1){ return ans; }
 
-        l=ind+1;
-        h=mountainArr.length()-1;
-        while(l<=h){
-            int m=(l+h)/2;
-            if(mountainArr.get(m)==target){
-                return m;
-            }
-            if(target<=mountainArr.get(m)){
-                l=m+1;
+    int decArr(int l, int h, int& target, MountainArray& arr) {
+        int ans = -1;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            if (arr.get(m)==target) { return m; }
+            if (arr.get(m) > target) {
+                    l = m+1;
             } else {
-                h=m-1;
+                h = m-1;
             }
         }
         return ans;
+    }
 
+    int peakIndex(MountainArray& arr) {
+        int l = 0;
+        int h = arr.length() - 1;
+        int ans = h;
+        while (l <= h) {
+            int m = (l + h) / 2;
+            ans = m;
+            if (arr.get(m - 1) < arr.get(m)) {
+                if (arr.get(m) < arr.get(m + 1)) {
+                    l = m;
+                } else {
+                    h = m;
+                    break;
+                }
+            } else {
+                h = m;
+            }
+        }
+        return ans;
+    }
+
+    int findInMountainArray(int target, MountainArray& mountainArr) {
+        int pi = peakIndex(mountainArr);
+        int leftInd = incArr(0, pi, target, mountainArr);
+        if (leftInd == -1) {
+            return decArr(pi, mountainArr.length() - 1, target, mountainArr);
+        }
+        return leftInd;
     }
 };
