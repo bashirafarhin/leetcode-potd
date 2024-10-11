5f3230d8127c0a560d06b2a578eed71d29045c28
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-11 20:55:46 +0530

    Time: 167 ms (62.95%) | Memory: 125.7 MB (30.88%) - LeetSync

--- 3498-minimum-array-changes-to-make-differences-equal/minimum-array-changes-to-make-differences-equal.cpp
+++ 3498-minimum-array-changes-to-make-differences-equal/minimum-array-changes-to-make-differences-equal.cpp
@@ -2,29 +2,26 @@ class Solution {
 public:
     int minChanges(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,int>currDiffCount;
-        vector<int>oneOp(k+1,0);
-
+        unordered_map<int,int>diff;
+        vector<int>changeOneNum(k+1,0);//max achievable difference by changing 1 number from a pair
         for(int i=0;i<n/2;i++){
-            int diff=abs(nums[i]-nums[n-i-1]);
-            currDiffCount[diff]++;
-            int mini=min(nums[i],nums[n-i-1]);
-            int maxi=max(nums[i],nums[n-i-1]);
-            int maxAcheivableDiff=max(k-mini,maxi-0);
-            oneOp[maxAcheivableDiff]++;
+            int d=abs(nums[i]-nums[n-i-1]);
+            diff[d]++;
+            int maxAchievableDiff=max(k-min(nums[i],nums[n-i-1]),max(nums[i],nums[n-i-1])-0);
+            changeOneNum[maxAchievableDiff]++;
         }
-
-        for(int maxAchievableDiff=k-1;maxAchievableDiff>=0;maxAchievableDiff--){
-            oneOp[maxAchievableDiff]+=oneOp[maxAchievableDiff+1];
+        //propogating the changes
+        for(int i=k-1;i>=0;i--){
+            changeOneNum[i]+=changeOneNum[i+1];
         }
-
-        int ans=INT_MAX;
-        for(auto [diff,count] : currDiffCount){
-            int oneExtra= oneOp[diff]-count;
-            int two=n/2-oneExtra-count;
-            ans=min(ans,oneExtra+two*2);
+        int ans=n;
+        for(auto it : diff){
+            int d=it.first;
+            int zeroChanges=it.second;
+            int oneChanges=changeOneNum[d]-zeroChanges;
+            int twoChanges=n/2-zeroChanges-oneChanges;
+            ans=min(ans,oneChanges+twoChanges*2);
         }
-
         return ans;
     }
 };
