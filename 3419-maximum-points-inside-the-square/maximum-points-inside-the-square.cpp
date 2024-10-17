22e5407f4e735871bb4945628d7071dc0c40f411
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-17 11:56:57 +0530

    Time: 200 ms (67.69%) | Memory: 104.2 MB (68.03%) - LeetSync

--- 3419-maximum-points-inside-the-square/maximum-points-inside-the-square.cpp
+++ 3419-maximum-points-inside-the-square/maximum-points-inside-the-square.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    //check if square of size mid is valid
-    //if valid return number of valid points in it else -1
-    int check(int sq, vector<vector<int>>& points, string s) {
-        unordered_set<char> st;
-        for (int j = 0; j < points.size(); j++) {
-            int x = points[j][0];
-            int y = points[j][1];
-            if (-sq <= x && x <= sq && -sq <= y && y <= sq) {
-                if (st.find(s[j]) != st.end()) {
-                    return -1;
-                }
-                st.insert(s[j]);
-            }
-        }
-        return st.size();
-    }
-
     int maxPointsInsideSquare(vector<vector<int>>& points, string s) {
-        int maxLength = 0;
-        for (auto pt : points) {
-            maxLength = max(maxLength, max(abs(pt[0]), abs(pt[1])));
+        int n=points.size();
+        vector<pair<int,char>>arr;
+        for(int i=0;i<n;i++){
+            arr.push_back({max(abs(points[i][0]),abs(points[i][1])),s[i]});
         }
-        int low = 0;
-        int high = maxLength;
-        int ptInValidSq = 0;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int pts=check(mid, points, s);
-            if (pts !=-1) {
-                ptInValidSq=max(ptInValidSq,pts);
-                low=mid+1;
-            } else {
-                high=mid-1;
+        sort(arr.begin(),arr.end());
+        vector<int>vis(26,0);
+        int pts=0;
+        int i=0;
+        while(i<n){
+            //check if sq is valid
+            int sq=arr[i].first;
+            int currValidPts=0;
+            while(i<n && sq==arr[i].first){
+                if(vis[arr[i].second-'a']){
+                    return pts;
+                } else {
+                    vis[arr[i].second-'a']=1;
+                    currValidPts++;
+                }
+                i++;
             }
+            pts+=currValidPts;
         }
-        return ptInValidSq;
+        return pts;
     }
 };
