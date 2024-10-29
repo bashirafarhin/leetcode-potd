08276db3227d48eac4f36424ce2a03d3f21d3887
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-29 10:27:46 +0530

    Time: 14 ms (52.63%) | Memory: 14.5 MB (39.77%) - LeetSync

--- 1239-largest-1-bordered-square/largest-1-bordered-square.cpp
+++ 1239-largest-1-bordered-square/largest-1-bordered-square.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    int helper(int& sr, int& sc, vector<vector<int>>& grid){
-        int r=grid.size();
-        int c=grid[0].size();
-        int len=0;
-        int i=sr;
-        int j=sc;
-        while(i<r && j<c){
-            bool flag=true;
-            if(grid[sr][j] !=1 || grid[i][sc] !=1){
-                return len*len;
-            }
-            for(int k=sr;k<=i;k++){
-                if(grid[k][j]!=1){
-                    flag=false;
-                    break;
-                }
-            }
-            for(int k=sc;k<=j;k++){
-                if(grid[i][k]!=1){
-                    flag=false;
-                    break;
-                }
-            }
-            i++;
-            j++;
-            if(flag){ len=max(len,i-sr); }
-        }
-        return len*len;
-    }
+    //how to find diagonal, this is my problem
     int largest1BorderedSquare(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        int sq=0;
+        vector<vector<int>>rowWise(r,vector<int>(c,0));
+        vector<vector<int>>colWise(r,vector<int>(c,0));
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                sq=max(sq,helper(i,j,grid));
+                if(!grid[i][j]){ continue; }
+                rowWise[i][j]+= j>0 ? rowWise[i][j-1]+1 : 1 ;
+                colWise[i][j]+= i>0 ? colWise[i-1][j]+1 : 1 ;
             }
         }
-        return sq;
+
+        int side=0;
+        for(int i=r-1;i>=0;i--){
+            for(int j=c-1;j>=0;j--){
+                int minSide=min(rowWise[i][j],colWise[i][j]);
+                while(minSide>side){
+                    if(colWise[i][j-minSide+1]>=minSide && rowWise[i-minSide+1][j]>=minSide){
+                        side=minSide;
+                        break;
+                    }
+                    minSide--;
+                }
+            }
+        }
+        return side*side;
     }
 };
