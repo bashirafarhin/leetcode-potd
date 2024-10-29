942872f164094b92b68faa80e98d3f4cfd14f452
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-10-29 19:29:31 +0530

    Time: 4 ms (32.26%) | Memory: 8 MB (25.00%) - LeetSync

---
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(int k) {
+        int count=k;
+        for(int i=1;i<=k;i++){
+            int op=i-1;
+            int sum=k;
+            while(sum-i>=1){
+                sum-=i;
+                op++;
+            }
+            count=min(count,op);
+        }
+        return count;
+    }
+};
