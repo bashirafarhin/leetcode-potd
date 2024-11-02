7271709d9915d810cecad5db5b27b407748ea9fa
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-02 10:14:27 +0530

    Time: 3 ms (71.78%) | Memory: 12.2 MB (37.65%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    void helper(int ind, vector<int>&nums, vector<vector<int>>&ans){
-        if(ind==nums.size()){
+    //your previous solutions are not working because when swapping you are changing the order
+    //and you can't sort the original array at every level as when you are going back you need the array in which you were iterating
+    void helper(int ind, vector<int>& nums, vector<vector<int>>& ans) {
+        if (ind == nums.size()) {
             ans.push_back(nums);
             return;
         }
-        unordered_set<int>st;
-        for(int j=ind;j<nums.size();j++){
-            if(st.find(nums[j])==st.end()){
-                st.insert(nums[j]);
-                swap(nums[ind],nums[j]);
-                helper(ind+1, nums, ans);
-                swap(nums[ind], nums[j]);
+        vector<int>arr=nums;
+        sort(arr.begin()+ind, arr.end());
+        for (int j = ind; j < arr.size(); j++) {
+            if (j !=ind && arr[j-1]==arr[j]) {
+                continue;
             }
+            swap(arr[ind], arr[j]);
+            helper(ind + 1, arr, ans);
+            swap(arr[ind], arr[j]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>ans;
+        vector<vector<int>> ans;
         helper(0, nums, ans);
         return ans;
     }
