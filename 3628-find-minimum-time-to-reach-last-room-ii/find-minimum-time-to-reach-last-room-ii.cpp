17b1a80b08da0d6ddd37cfbdb233b326133d9fec
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-03 10:43:13 +0530

    Time: 969 ms (82.61%) | Memory: 276.1 MB (21.74%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,32 +1,34 @@
-using DataType = pair<pair<int,int>,pair<int,int>>;
-class Solution {
+using DataType = pair<pair<int,int>,pair<int,int>>;class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& grid) {
         int R=grid.size();
         int C=grid[0].size();
-        priority_queue<DataType,vector<DataType>,greater<DataType>>pq;
+        set<DataType>st;
         vector<vector<int>>minTime(R,vector<int>(C,INT_MAX));
         vector<vector<int>>dxy={{-1,0},{0,1},{1,0},{0,-1}};
         minTime[0][0]=0;
-        pq.push({{0,0},{0,0}});
-        int move=0;
-        while(!pq.empty()){
-            int t=pq.top().first.first;
-            int move=pq.top().first.second;
-            int r=pq.top().second.first;
-            int c=pq.top().second.second;
-            pq.pop();
+        st.insert({{0,0},{0,0}});
+        while(!st.empty()){
+            int t=st.begin()->first.first;
+            int prevMove=st.begin()->first.second;
+            int r=st.begin()->second.first;
+            int c=st.begin()->second.second;
+            int newMove=prevMove;
+            newMove++;
+            if(newMove==3){ newMove=1; }
+            st.erase(st.begin());
             if(r==R-1 && c==C-1){ return t; }
-            move++;
-            if(move==3){ move=1; }
             for(auto it : dxy){
                 int nr=r+it[0];
                 int nc=c+it[1];
                 if(nr>=0 && nc>=0 && nr<R && nc<C){
-                    int newTime=max(t+move,grid[nr][nc]+move);
+                    int newTime=max(t+newMove,grid[nr][nc]+newMove);
                     if(minTime[nr][nc]>newTime){
+                    if (st.find({{minTime[nr][nc],prevMove}, {nr, nc}}) != st.end()) {
+                        st.erase({{minTime[nr][nc],prevMove}, {nr, nc}});
+                    }
                      minTime[nr][nc]=newTime;
-                     pq.push({{newTime,move},{nr,nc}});
+                     st.insert({{newTime,newMove},{nr,nc}});
                     }
                 }
             }
