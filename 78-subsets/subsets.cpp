a73f270b35dc4966b4b2090d32e5d1f3b3849de3
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-04 18:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (21.41%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> v;
-        int n=nums.size();
-        int x=pow(2,n);
-        for(int i=0;i<x;i++){
-            vector<int> vin;
-            for(int j=0;j<n;j++){
-                if(i & (1<<j)){
-                    vin.push_back(nums[j]);
-                }
-            }
-            v.push_back(vin);
+    void helper(int ind, vector<int> subSet, vector<int>&nums, vector<vector<int>>& ans) {
+        if (ind == nums.size()) {
+            ans.push_back(subSet);
+            return;
         }
-        sort(v.begin(),v.end());
-        return v;
+        subSet.push_back(nums[ind]);
+        helper(ind + 1, subSet, nums, ans);
+        subSet.pop_back();
+        helper(ind + 1, subSet, nums, ans);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> subSet;
+        helper(0, subSet, nums, ans);
+        return ans;
     }
 };
