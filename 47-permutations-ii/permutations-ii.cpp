dc27fd0d80eb7fada6c12807ba7cee94e762e361
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-05 10:30:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (97.12%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    //your previous solutions are not working because when swapping you are changing the order
-    //and you can't sort the original array at every level as when you are going back you need the array in which you were iterating
-    void helper(int ind, vector<int>& nums, vector<vector<int>>& ans) {
-        if (ind == nums.size()) {
-            ans.push_back(nums);
+    //you can't use this condition to skip nums[j-1]==nums[j] as you're distorting the order, then don't the order
+    void helper(vector<int>&curr, vector<int>& nums, vector<int>&used, vector<vector<int>>& ans) {
+        if (curr.size() == nums.size()) {
+            ans.push_back(curr);
             return;
         }
-        vector<int>arr=nums;
-        sort(arr.begin()+ind, arr.end());
-        for (int j = ind; j < arr.size(); j++) {
-            if (j !=ind && arr[j-1]==arr[j]) {
+        for (int i = 0; i < nums.size(); i++) {
+            if(used[i]){ continue; }
+            if (i>0 && nums[i-1]==nums[i] && used[i-1]) {
                 continue;
             }
-            swap(arr[ind], arr[j]);
-            helper(ind + 1, arr, ans);
-            swap(arr[ind], arr[j]);
+            used[i]=1;
+            curr.push_back(nums[i]);
+            helper(curr, nums, used, ans);
+            curr.pop_back();
+            used[i]=0;
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>used(nums.size(),0);
+        vector<int>curr;
         vector<vector<int>> ans;
-        helper(0, nums, ans);
+        helper(curr, nums, used, ans);
         return ans;
     }
 };
