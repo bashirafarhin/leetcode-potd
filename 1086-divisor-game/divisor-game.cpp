769be807dea0aa5cad1b8c518705a57e61006f87
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-07 13:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (58.35%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        //players can choose from 1---->n-1
-      
-        int turn = 1; //turn of alice to play
-        while(n>1){
-            n--;
-            turn=1-turn;
-        }
-        if(turn==0){return true;}
-        return false;
+        return (n-1) & 1 ? true : false;
     }
 };
