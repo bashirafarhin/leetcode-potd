6be02e85e6f82ec582f1e76bfae7a919a536e363
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-07 11:02:25 +0530

    Time: 186 ms (64.78%) | Memory: 69.8 MB (43.96%) - LeetSync

--- 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
+++ 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
@@ -2,61 +2,61 @@ class Solution {
 public:
     int mod=1e9+7;
     int n;
-    int target;
     vector<int>freq;
     vector<int>fact;
     vector<int>ifact;
+
     long long binaryExpo(int base, int exp){
-        if(exp==0){
-            return 1LL;
-        }
+        if(exp==0){ return 1; }
         if(exp & 1){
             return base*binaryExpo(base,exp-1)%mod;
         } else {
             long long ans=binaryExpo(base,exp/2);
-            return ans*ans%mod;
+            return (ans * ans)%mod;
         }
     }
 
-    int solve(int num, int oddSize, int sum, vector<vector<vector<int>>>&dp){
-        if(num==10){
-            if(oddSize==n/2 && sum==target){
-                int evenSize=n-oddSize;
-                return (1LL*fact[oddSize]*fact[evenSize])%mod;
+    void precomputeFactorials(int n){
+        fact.resize(n+1,1);
+        ifact.resize(n+1,1);
+        for(int i=1;i<=n;i++){
+            fact[i]=(1LL*i*fact[i-1])%mod;
+            ifact[i]=binaryExpo(fact[i],mod-2);
+        }
+        return ;
+    }
+
+    int helper(int val, int target, int oddplaces, int evenplaces, vector<vector<vector<int>>>&dp){
+        if(val==10){
+            if(target==0 && oddplaces==0){
+                return (1LL*fact[n-evenplaces]*fact[evenplaces])%mod;
             }
             return 0;
         }
-        if(sum>target){
+        if(target<0){
             return 0;
         }
-        if(dp[num][oddSize][sum] !=-1){
-            return dp[num][oddSize][sum];
-        }
+        if(dp[val][target][oddplaces] !=-1){ return dp[val][target][oddplaces]; }
         int ans=0;
-        for(int oddCount=0; oddCount<=min(freq[num],n/2-oddSize); oddCount++){
-            int evenCount=freq[num]-oddCount;
-            ans=(ans+((1LL*ifact[oddCount]*ifact[evenCount])%mod)*solve(num+1,oddSize+oddCount,sum+oddCount*num,dp))%mod;
+        for(int count=0;count<=min(freq[val],oddplaces);count++){
+            ans=(ans+(1LL*ifact[count]*ifact[freq[val]-count]%mod)*helper(val+1,target-val*count,oddplaces-count,evenplaces,dp))%mod;
         }
-        return dp[num][oddSize][sum]=ans;
+        return dp[val][target][oddplaces]=ans;
     }
     int countBalancedPermutations(string num) {
         n=num.size();
         int sum=accumulate(num.begin(),num.end(),0)-'0' * n;
         if(sum & 1){ return 0; }
-        int evenplaces=n/2+n%2;
+
+        int evenplaces=(n+1)/2;
         int oddplaces=n/2;
-        target=sum/2;
+        int target=sum/2;
         freq.resize(10,0);
-        fact.resize(n+1,1);
-        ifact.resize(n+1,1);
         for(char n : num){
             freq[n-'0']++;
         }
-        for(int i=1;i<=n;i++){
-            fact[i]=(1LL*i*fact[i-1])%mod;
-            ifact[i]=binaryExpo(fact[i],mod-2);
-        }
-        vector<vector<vector<int>>>dp(10+1,vector<vector<int>>(oddplaces+1,vector<int>(target+1,-1)));
-        return solve(0,0,0,dp);
+        precomputeFactorials(n);
+        vector<vector<vector<int>>>dp(10,vector<vector<int>>(target+1,vector<int>(oddplaces+1,-1)));
+        return helper(0,target, oddplaces, evenplaces, dp);
     }
 };
