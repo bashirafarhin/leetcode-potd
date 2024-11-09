2708142d8dcfa1bfe25a674395d051a71c9cb569
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-09 10:40:48 +0530

    Time: 99 ms (19.62%) | Memory: 9 MB (61.09%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -7,14 +7,12 @@ public:
         if (start == end) {
             return nums[start];
         }
-        int pickStart=nums[start]+min( helper(start+2,end,nums), helper(start+1,end-1,nums));
-        int pickEnd=nums[end]+min( helper(start+1,end-1,nums), helper(start,end-2,nums));
+        int pickStart=nums[start]-helper(start+1,end,nums);
+        int pickEnd=nums[end]-helper(start,end-1,nums);
         return max(pickStart,pickEnd);
     }
     int predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int score1=helper(0, n - 1, nums);
-        int score2=accumulate(nums.begin(),nums.end(),0)-score1;
-        return score1>=score2;
+        return helper(0,n-1,nums)>=0;
     }
 };
