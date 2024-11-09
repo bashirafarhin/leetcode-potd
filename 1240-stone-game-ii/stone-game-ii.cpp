7f97180c3431c4cf3731313b029ee3c887099bf4
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-09 13:36:53 +0530

    Time: 7 ms (81.92%) | Memory: 12.4 MB (46.06%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int helper(int ind, int M, vector<int>&piles, vector<vector<int>>&dp){
+    int helper(int ind, int M, vector<int>&piles, vector<int>&suffix, vector<vector<int>>&dp){
         if(ind==piles.size()){ return 0; }
         if(dp[ind][M] !=-1){ return dp[ind][M]; }
         int ans=0;
@@ -9,14 +9,17 @@ public:
         int n=piles.size();
         for(int j=ind; j<=min(remaining,n-1); j++){
             alice+=piles[j];
-            int remainingPiles=accumulate(piles.begin()+j+1,piles.end(),0);
-            ans=max(ans,alice+remainingPiles-helper(j+1,max(M,(j-ind)+1),piles,dp));
+            ans=max(ans,alice+suffix[j]-helper(j+1,max(M,(j-ind)+1),piles,suffix,dp));
         }
         return dp[ind][M]=ans;
     }
     int stoneGameII(vector<int>& piles) {
         int n=piles.size();
+        vector<int>suffix(n+1,0);
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=piles[i+1]+suffix[i+1];
+        }
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return helper(0,1,piles,dp);
+        return helper(0,1,piles,suffix,dp);
     }
 };
