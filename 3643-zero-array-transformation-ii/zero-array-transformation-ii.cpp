8f1c7f61df24b4ab70570d7b6d2e817909adae0a
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-11-17 11:49:37 +0530

    Time: 65 ms (54.55%) | Memory: 318.5 MB (93.33%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+    bool countMinQueries(vector<int>& nums, vector<vector<int>>& queries, int k){
         int n=nums.size();
-        int i=0;
-        int maxi=*max_element(nums.begin(),nums.end());
-        if(maxi==0){ return 0; }
         vector<int>prefix(n+1,0);
-        for(int k=0;k<queries.size();k++){
-            int l=queries[k][0];
-            int r=queries[k][1];
-            int val=queries[k][2];
-            if(l<=i && i<=r){
-                prefix[i]+=val;
-                prefix[r+1]-=val;
-            } else if(l>i) {
-                prefix[l]+=val;
-                prefix[r+1]-=val;
-            }
-            
-            while(i<n){
-                if(nums[i]<=prefix[i]){          
-                i++;
-                prefix[i]+=prefix[i-1];
-                } else {
-                    break;
-                }
-            }
-            if(i>=n){
-                return k+1;
+        for(int i=0;i<k;i++){
+            int l=queries[i][0];
+            int r=queries[i][1];
+            int val=queries[i][2];
+            prefix[l]+=val;
+            prefix[r+1]-=val;
+        }
+        for(int i=1;i<n;i++){
+            prefix[i]+=prefix[i-1];
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>prefix[i]){ return 0; }
+        }
+        return 1;
+    }
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int ans=INT_MAX;
+        int l=0;
+        int h=queries.size();
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(countMinQueries(nums,queries,m)){
+                ans=min(ans,m);
+                h=m-1;
+            } else {
+                l=m+1;
             }
         }
-        return -1;
+        return ans==INT_MAX ? -1 : ans;
     }
 };
