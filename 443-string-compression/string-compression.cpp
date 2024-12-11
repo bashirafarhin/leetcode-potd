258187ee4f66938d2f18fb9fcd72d2955c63332e
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2024-12-11 20:11:58 +0530

    Time: 4 ms (8.77%) | Memory: 13.7 MB (82.52%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int i = 0;
+        for (int j = 0; j < n;) {
+            char curr = chars[j];
+            int freq = 0;
+            while (j < n && curr == chars[j]) {
+                freq++;
+                j++;
+            }
+            chars[i] = curr;
+            i++;
+            if (freq > 1) {
+                string frequency=to_string(freq);
+                int len=0;
+                while (len<frequency.size()) {
+                    chars[i] = frequency[len];
+                    i++;
+                    len++;
+                }
+            }
+        }
+
+        while (chars.size() != i) {
+            chars.pop_back();
+        }
+        return chars.size();
+    }
+};
