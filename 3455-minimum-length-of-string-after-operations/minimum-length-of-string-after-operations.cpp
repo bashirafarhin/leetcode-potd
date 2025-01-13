1e1f357a0ed35c2d795ac335159f74d7857f48eb
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-01-13 10:43:12 +0530

    Time: 7 ms (93.55%) | Memory: 32.1 MB (58.06%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int n=s.size();
-        vector<bool>remove(n,false);
-        int ans=n;
-        for(int i=1;i<n-1;i++){
-            if(remove[i]){ continue; }
-            int left=i-1;
-            int right=i+1;
-            while(left>=0 && s[left] !=s[i]){
-                left--;
-            }
-            while(right<n && s[right] !=s[i]){
-                right++;
-            }
-            if(left>=0 && right<n){
-                remove[left]=true;
-                remove[right]=true;
-                ans-=2;
-            }
+        vector<int>freq(26,0);
+        for(char ch : s){
+            freq[ch-'a']++;
+        }
+        int ans=0;
+        for(int n : freq){
+            if(n) ans+=(n & 1 ? 1 : 2);
         }
         return ans;
     }
