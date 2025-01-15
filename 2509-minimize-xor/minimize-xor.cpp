20ce02d6635c1111bb58c0190d6fb9cb274cb3d2
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-01-15 09:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (40.19%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-     int setbits=0;
-     while(num2){
-       num2=num2 & (num2-1); setbits++;}
-     int ans=0;
-     for(int i=31;i>=0 && setbits;i--){
-         if(num1 & (1<<i)){ans=ans | (1<<i);setbits--;}
-     }
-     for(int i=0;i<32 && setbits;i++){
-         if(!(num1 & (1<<i)) ){ans=ans | (1<<i);setbits--;}
-     }
-     return ans;
+       int bits=0;
+       while(num2){
+        num2=num2 & (num2-1);
+        bits++;
+       }
+       int x=0;
+       for(int i=31;i>=0 && bits>0;i--){
+        if(num1 & (1<<i)){
+            x = x | (1<<i);
+            bits--;
+        }
+       }
+       //use all bits of bits
+       for(int i=0;i<32 && bits>0;i++){
+        if(x & (1<<i)){ continue; }
+        x = x | (1<<i);
+        bits--;
+       }
+       return x;
     }
 };
