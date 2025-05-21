0311960264c0529ba556f25c95616f32a113dc98
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-05-21 19:52:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.87%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,32 +1,57 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int R=matrix.size();
-        int C=matrix[0].size();
-        vector<int>rowArr(R,-1);
-        vector<int>colArr(C,-1);
-        for(int r=0;r<R;r++){
-            for(int c=0;c<C;c++){
-                if(matrix[r][c]==0){
-                        rowArr[r]=1;
-                        colArr[c]=1;
-                    }
-                }
-            }
+        int R = matrix.size();
+        int C = matrix[0].size();
 
-        for(int r=0;r<R;r++){
-            if(rowArr[r]==1){
-                for(int col=0;col<C;col++){
-                    matrix[r][col]=0;
+        // if present in 0th Col or not
+        bool isInfirstCol = false;
+
+        // if present in 1st row or not
+        bool isInfirstRow = false;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (r == 0 && matrix[0][c] == 0) {
+                    isInfirstRow = true;
+                }
+                if (c == 0 && matrix[r][0] == 0) {
+                    isInfirstCol = true;
+                }
+                if (matrix[r][c] == 0) {
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
                 }
             }
         }
 
-        for(int c=0;c<C;c++){
-            if(colArr[c]==1){
-                for(int row=0;row<R;row++){
-                    matrix[row][c]=0;
+        for (int r = 1; r < R; r++) {
+            if (matrix[r][0] == 0) {
+                for (int c = 0; c < C; c++) {
+                    matrix[r][c] = 0;
                 }
+            }
+        }
+
+        for (int c = 1; c < C; c++) {
+            if (matrix[0][c] == 0) {
+                for (int r = 0; r < R; r++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        // if 0 present in 0th Col
+        if (isInfirstCol) {
+            for (int r = 0; r < R; r++) {
+                matrix[r][0] = 0;
+            }
+        }
+
+        // if 0 present in 1st Col
+        if (isInfirstRow) {
+            for (int c = 0; c < C; c++) {
+                matrix[0][c] = 0;
             }
         }
     }
