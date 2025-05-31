2f1b0cfb10ffcb04c7e84e04eed4d327ab6e7549
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-05-31 21:08:11 +0530

    Time: 3 ms (38.93%) | Memory: 42.1 MB (32.01%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> prefix(n, 0);
-        vector<int> suffix(n, 0);
-        prefix[0] = nums[0];
-        suffix[n - 1] = nums[n - 1];
-        vector<int> ans(n, 0);
+        int n=nums.size();
+        if(n==1){ return nums; }
+        vector<int>prefix(n,1);
+        vector<int>suffix(n,1);
 
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] * nums[i];
-        }
-        for (int i = n - 2; i > 0; i--) {
-            suffix[i] = suffix[i + 1] * nums[i];
+        for(int i=0;i<n;i++){
+            prefix[i]=nums[i];
+            if(i-1>=0){
+                prefix[i]*=prefix[i-1];
+            }
+
+            int end=n-1-i;
+            suffix[end]=nums[end];
+            if(end+1<n){
+                suffix[end]*=suffix[end+1];
+            }
         }
 
-        for (int i = 0; i < n; i++) {
-            int right = i + 1 < n ? suffix[i + 1] : 1;
-            int left = i - 1 >= 0 ? prefix[i - 1] : 1;
-            ans[i] = left * right;
+        vector<int>ans(n,0);
+        ans[0]=suffix[1];
+        ans[n-1]=prefix[n-2];
+        for(int i=1;i<n-1;i++){
+            ans[i]=prefix[i-1]*suffix[i+1];
         }
         return ans;
     }
