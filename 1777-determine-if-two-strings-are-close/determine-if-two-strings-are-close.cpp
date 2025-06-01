c02546e407ecd41fb57cf6f7ad42114f63ad7ec1
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-01 15:03:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (72.35%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-       int n=word1.size();
-       int m=word2.size();
-       if(n !=m){ return false; }
-       unordered_map<char,int>freq1;
-       for(char ch : word1){
-        freq1[ch]++;
-       }
-       unordered_map<char,int>freq2;
-       for(char ch : word2){
-        freq2[ch]++;
-       }
-       //every character of word1 should be present in word2 and vice versa
-       for(char ch : word1){
-        if(freq2.find(ch)==freq2.end()){ return false; }
-       }
-       for(char ch : word2){
-        if(freq1.find(ch)==freq1.end()){ return false; }
-       }
-       //frequency of words should match
-       unordered_map<int,int>mp1;
-       for(auto it : freq1){
-        mp1[it.second]++;
-       }
-       unordered_map<int,int>mp2;
-       for(auto it : freq2){
-        mp2[it.second]++;
-       }
-       return mp1==mp2;
+        int n=word1.size();
+        int m=word2.size();
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+        for(char ch : word1){ freq1[ch-'a']++; }
+        for(char ch : word2){ freq2[ch-'a']++; }
+
+        for(int i=0;i<26;i++){
+            if((freq1[i] && !freq2[i]) || (freq2[i] && !freq1[i])){ return false; }
+        }
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
+        return freq1==freq2;
     }
 };
