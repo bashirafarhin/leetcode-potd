7fb49c8fa6527deb26994cb351af06578c46698c
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-01 11:57:32 +0530

    Time: 3 ms (35.45%) | Memory: 62.9 MB (78.46%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,17 +1,30 @@
+// int maxArea(vector<int>& height) {
+//     int n = height.size();
+//     int ans = 0;
+//     for (int left = 0; left < n; left++) {
+//         for (int right = left + 1; right < n; right++) {
+//             int capacity = min(height[left], height[right]) * (right - left);
+//             ans = max(ans, capacity);
+//         }
+//     }
+//     return ans;
+// }
+
 class Solution {
 public:
+    // you cannot use algorithm because of these test cases 6,7,100,100,1,5
     int maxArea(vector<int>& height) {
-        int ans=0;
-        int st=0;
-        int end=height.size()-1;
-        while(st<end){
-            int h=min(height[st],height[end]);
-            int width=end-st;
-            ans=max(ans,h*width);
-            if(height[st]<height[end]){
-                st++;
+        int n = height.size();
+        int left = 0;
+        int right = n - 1;
+        int ans = 0;
+        while (left <= right) {
+            int capacity = min(height[left], height[right]) * (right - left);
+            ans = max(ans, capacity);
+            if (height[left] < height[right]) {
+                left++;
             } else {
-                end--;
+                right--;
             }
         }
         return ans;
