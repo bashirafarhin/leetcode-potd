348cd3b5d4086600c961bff5c272fa65e7efc5e3
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-02 20:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (80.60%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     string decodeString(string s) {
-      string ans="";
-      for(int i=0;i<s.size();){
-        if('1'<=s[i] && s[i]<='9'){
-            string num="";
-            for(;'0'<=s[i] && s[i]<='9';i++){ num+=s[i]; }
-            int open=1;
-            int close=0;
-            string newS="";
-            i++;
-            while(open !=close){
-                if(s[i]=='['){ open++; }
-                else if(s[i]==']')
-                { close++;}
-                newS+=s[i];
+        int n = s.size();
+        string ans = "";
+        int i = 0;
+        while (i < n) {
+            if ('a' <= s[i] && s[i] <= 'z') {
+                ans += s[i];
                 i++;
+            } else {
+                string num = "";
+                while ('0' <= s[i] && s[i] <= '9') {
+                    num += s[i];
+                    i++;
+                }
+                int freq = stoi(num);
+                int open = 1;
+                int closed = 0;
+                int start = i + 1;
+                i += 2;
+                while (open != closed) {
+                    if (s[i] == ']') {
+                        closed++;
+                    } else if (s[i] == '[') {
+                        open++;
+                    }
+                    i++;
+                }
+                string substr = decodeString(s.substr(start, i - start - 1));
+                for (int f = 0; f < freq; f++) {
+                    ans += substr;
+                }
             }
-            newS.pop_back();
-            string decoded=decodeString(newS);
-            for(int freq=1;freq<=stoi(num);freq++){
-                ans+=decoded;
-            }
-        } else {
-            ans+=s[i];
-            i++;
         }
-      }
-      return ans;
+        return ans;
     }
 };
