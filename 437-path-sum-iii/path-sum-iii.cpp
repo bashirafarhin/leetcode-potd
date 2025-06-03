dd39ee516cf4ae0896d1fb5c2b6f31c3fa111b0c
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-03 09:53:14 +0530

    Time: 4 ms (81.80%) | Memory: 19 MB (66.02%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,23 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void solve(TreeNode* root, int target,vector<int>&path,int&cnt){
-        if(root==NULL){ return ; }
+    int sum(TreeNode* root, int targetSum, vector<int>&path){
+        if(!root){ return 0; }
         path.push_back(root->val);
-        solve(root->left,target,path,cnt);
-        solve(root->right,target,path,cnt);
-        long sum=0;
+        int ans=sum(root->left, targetSum, path)+sum(root->right, targetSum, path);
+        long long sum=targetSum;
         for(int i=path.size()-1;i>=0;i--){
-            sum+=(long)path[i];
-            if(sum==(long)target){ cnt++; }
+            sum-=path[i];
+            if(sum==0){ ans++; }
         }
         path.pop_back();
-        return ;
-
+        return ans;
     }
     int pathSum(TreeNode* root, int targetSum) {
-        int cnt=0;
         vector<int>path;
-        solve(root,targetSum,path,cnt);
-        return cnt;
+        return sum(root, targetSum, path);
     }
 };
