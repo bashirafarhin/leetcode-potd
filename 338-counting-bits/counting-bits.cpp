45b1fa10cc747128185f4138e6a2c8482b7f2210
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-04 22:31:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (36.55%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> dp(n+1,0);
-        for(int i=0;i<=n;i++){
-            if(i & 1){ //if odd number
-             dp[i]=dp[i/2]+1;
+        if(n==0){ return {0}; }
+        if(n==1){ return {0,1}; }
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(i & 1){
+                dp[i]=dp[i/2]+1;
             } else {
-             dp[i]=dp[i/2];
+                dp[i]=dp[i/2];
             }
         }
         return dp;
