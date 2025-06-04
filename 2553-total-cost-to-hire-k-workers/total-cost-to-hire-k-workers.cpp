58501660ad38dd2f943fa2b2afd2d25fe2e669ea
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-04 22:05:27 +0530

    Time: 43 ms (55.50%) | Memory: 79.8 MB (20.90%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        long long ans = 0;
-        priority_queue<int, vector<int>, greater<int>> left;
-        priority_queue<int, vector<int>, greater<int>> right;
-        int start = 0;
-        int end = costs.size() - 1;
-        
-        int cand=candidates;
-        while (start<=end && cand) {
-            left.push(costs[start]);
-            start++;
-            cand--;
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        int n = costs.size();
+        int left = 0;
+        int right = n - 1;
+        for (int i = 0; i < candidates; i++) {
+            if (left == right) {
+                pq.push({costs[left], -1});
+            } else if (left < right) {
+                pq.push({costs[left], -1});
+                pq.push({costs[right], 1});
+            }
+            left++;
+            right--;
+            if(left>right){ break; }
         }
-        cand=candidates;
-        while (start<=end && cand) {
-            right.push(costs[end]);
-            end--;
-            cand--;
-        }
-
-        for (int i = 0; i < k; i++) {
-            int l = left.empty() ? 1e6 : left.top();
-            int r = right.empty() ? 1e6 : right.top();
-            if (l <= r) {
-                ans += l;
-                left.pop();
-                if (start <= end) {
-                    left.push(costs[start]);
-                    start++;
+        long long cost = 0;
+        while (k--) {
+            cost += pq.top().first;
+            int dir = pq.top().second;
+            pq.pop();
+            if (left <= right) {
+                int ind;
+                if (dir == -1) {
+                    ind = left;
+                    left++;
+                } else {
+                    ind = right;
+                    right--;
                 }
-            } else {
-                ans += r;
-                right.pop();
-                if (start <= end) {
-                    right.push(costs[end]);
-                    end--;
-                }
+                pq.push({costs[ind], dir});
             }
         }
-        return ans;
+        return cost;
     }
 };
