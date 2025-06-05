49bfc340dc1c4d287692bc4603d2d287e7143038
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-05 14:13:22 +0530

    Time: 11 ms (43.62%) | Memory: 22.9 MB (42.06%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    bool checkIfFinish(int k, vector<int>& piles, int h){
-        int hoursTake=0;
-        for(int i=0;i<piles.size();i++){
-            hoursTake+=ceil((double)piles[i]/k);
-            if(hoursTake>h){ return false; }
+    bool checkIfFinish(vector<int>& piles, int h, int k) {
+        int timeRequired = 0;
+        for (int i = 0; i < piles.size(); i++) {
+            timeRequired += (piles[i] + k - 1) / k; // instead of ceil use this;
+            if(timeRequired>h){ return false; }
         }
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1;
-        int high=1e9;
-        int ans=-1;
-        while(low<=high){
-            int mid=(high+(low-high)/2);
-            if(checkIfFinish(mid,piles,h)){
-                ans=mid;
-                high=mid-1;
+        // sum denotes the maximum time she requires to finish all off by eating
+        // only 1 banana at one time
+        int start = 1;
+        int maxE = *max_element(piles.begin(), piles.end());
+        int end = maxE;
+        int ans = maxE;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (checkIfFinish(piles, h, mid)) {
+                ans = mid;
+                end = mid - 1;
             } else {
-                low=mid+1;
+                start = mid + 1;
             }
         }
         return ans;
