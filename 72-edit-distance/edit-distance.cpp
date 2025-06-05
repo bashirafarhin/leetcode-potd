bcd4d60a9dbfeab436b8dc1be5da263c72ded511
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-05 20:02:42 +0530

    Time: 9 ms (39.96%) | Memory: 14.2 MB (20.46%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int solve(string word1, string word2, int i, int j,
-              vector<vector<int>>& dp) {
-        if (i == word1.size() && j == word2.size()) {
-            return 0;
-        }
-        if (i == word1.size()) {
-            return word2.size()-j;
-        }
-        if (j == word2.size()) {
+    int minDis(int i, int j, string& word1, string& word2, vector<vector<int>>&dp){
+        if(i==word1.size() && j==word2.size()){ return 0; }
+        if(j==word2.size()){
             return word1.size()-i;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if(i==word1.size()){
+            return word2.size()-j;
         }
-        if (word1[i] == word2[j]) {
-            return dp[i][j] = solve(word1, word2, i + 1, j + 1, dp);
+        if(dp[i][j] !=-1){
+        return dp[i][j];
         }
-        return dp[i][j] = min(1 + solve(word1, word2, i + 1, j, dp),
-                              min(1 + solve(word1, word2, i, j + 1, dp),
-                                  1 + solve(word1, word2, i + 1, j + 1, dp)));
+        if(i==word1.size() || j==word2.size()){ return INT_MAX; }
+        if(word1[i]==word2[j]){
+            return dp[i][j]=minDis(i+1,j+1,word1,word2,dp);
+        } else {
+            return dp[i][j]=1+min({
+                minDis(i,j+1,word1,word2,dp),
+                minDis(i+1,j,word1,word2,dp),
+                minDis(i+1,j+1,word1,word2,dp)
+            });
+        }
     }
     int minDistance(string word1, string word2) {
-        int n1 = word1.size();
-        int n2 = word2.size();
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        return solve(word1, word2, 0, 0, dp);
+        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
+        return minDis(0,0,word1,word2,dp);
     }
 };
