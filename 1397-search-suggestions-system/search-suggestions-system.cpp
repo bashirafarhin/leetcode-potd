30c5fcc39dca05c8de501d3774abda5949056746
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-05 22:11:54 +0530

    Time: 5 ms (96.25%) | Memory: 29 MB (99.79%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.cpp
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        vector<vector<string>>ans;
         sort(products.begin(),products.end());
-        int l=0;
-        int r=products.size()-1;
-        int ind=0;
-        for(int i=0;i<searchWord.size();i++){
-            char ch=searchWord[i];
-            while(l<=r && (products[l].size()<=ind || products[l][ind] !=ch)){ l++; }
-            while(l<=r && (products[r].size()<=ind || products[r][ind] !=ch)){ r--; }
-            vector<string>curr;
-            for(int j=0; j<min(3,r-l+1) ;j++){
-                curr.push_back(products[l+j]);
+        int left=0;
+        int right=products.size()-1;
+        int n=searchWord.size();
+        vector<vector<string>>arr(n);
+        for(int i=0;i<n;i++){
+            while(left<=right && products[left][i] !=searchWord[i]){ left++; }
+            while(left<=right && products[right][i] !=searchWord[i]){ right--; }
+            for(int j=left;j<=right && j-left<3;j++){
+                arr[i].push_back(products[j]);
             }
-            ans.push_back(curr);
-            ind++;
         }
-        return ans;
+        return arr;
     }
 };
