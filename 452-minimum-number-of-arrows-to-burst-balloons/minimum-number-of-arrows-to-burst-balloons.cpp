275aabe04e6cf42262e5bad5b2a781f76351249a
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-06 10:46:32 +0530

    Time: 38 ms (92.90%) | Memory: 93.8 MB (93.27%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,21 +1,21 @@
 class Solution {
+    static bool comp(vector<int>& a, vector<int>& b) {
+    return a[1] < b[1];
+}
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
-        int n=points.size();
-        int arrows=1;
-        int left=points[0][0];
-        int right=points[0][1];
-        for(int i=1;i<n;i++){
-            if(right>=points[i][0]){
-                left=max(left,points[i][0]);
-                right=min(right,points[i][1]);
+        sort(points.begin(), points.end(), comp);
+        int ans = 0;
+        // int left = points[0][0];
+        int right = points[0][1];
+        for (int i = 1; i < points.size(); i++) {
+            if (right >= points[i][0]) {
+                ans++;
             } else {
-                left=points[i][0];
-                right=max(right,points[i][1]);
-                arrows++;
+                // left = min(left, points[i][0]);
+                right = points[i][1];
             }
         }
-        return arrows;
+        return points.size()-ans;
     }
 };
