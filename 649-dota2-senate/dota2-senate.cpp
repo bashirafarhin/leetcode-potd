775b9fa09d62be4924d89d5b866492da9c6bf972
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-06 15:07:30 +0530

    Time: 4 ms (43.00%) | Memory: 9.1 MB (95.49%) - LeetSync

--- 649-dota2-senate/dota2-senate.cpp
+++ 649-dota2-senate/dota2-senate.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
-        int n = senate.size();
-        for (int i = 0;; i++) {
-            int ind = i % n;
-            if (senate[ind] == 'K') { continue; }
-            int next = ind + 1;
-            while ( (next % n) != ind && (senate[next % n]=='K' || senate[next % n] == senate[ind])) { next++; }
-            if (next % n== ind) {
-                return senate[next % n] == 'R' ? "Radiant" : "Dire";
-            } else {
-                senate[next % n] = 'K';
+        int totalRSenators=0;
+        int totalDSenators=0;
+        int totalBanOnR=0;
+        int totalBanOnD=0;
+        int n=senate.size();
+        for(char ch : senate){
+            ch=='R' ? totalRSenators++ : totalDSenators++;
+        }
+        vector<bool>Banned(n,false);
+        string win="";
+        for(int ind=0;;ind++){
+            int i=(n+ind)%n;
+            if(Banned[i]){ continue; }
+            if(senate[i]=='R'){
+                if(totalBanOnR){
+                    totalBanOnR--;
+                    totalRSenators--;
+                    Banned[i]=true;
+                } else if(totalDSenators==0){
+                    win="Radiant";
+                    break;
+                } else {
+                    totalBanOnD++;
+                }
+            }
+
+            else{
+                if(totalBanOnD){
+                    totalBanOnD--;
+                    totalDSenators--;
+                    Banned[i]=true;
+                } else if(totalRSenators==0){
+                    win="Dire";
+                    break;
+                } else {
+                    totalBanOnR++;
+                }
             }
         }
-        return "-1";
+        return win;
     }
 };
