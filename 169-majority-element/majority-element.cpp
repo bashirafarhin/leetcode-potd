88444c8811615a2d7bd5908024d2cae19fb2c16a
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-06-14 10:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (58.48%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate=0;
         int count=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
+        int majEle=-1;
+        for(int i=0;i<nums.size();i++){
             if(count==0){
-                candidate=nums[i];
                 count++;
-            } else if( candidate==nums[i] ){
+                majEle=nums[i];
+            }
+            else if(majEle==nums[i]){
                 count++;
-            } else if( candidate !=nums[i] ){
+            } else {
                 count--;
             }
         }
-
-        //check if majority element actually exist or not
-        count=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==candidate){ count++; }
-        }
-
-        return count>(n/2) ? candidate : -1;
+        return majEle;
     }
 };
