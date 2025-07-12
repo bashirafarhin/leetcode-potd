bbe11083b4605b36b9f3563d8505904d44e4565f
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-07-12 21:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (17.41%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
+    int binarySearch(vector<int>& nums, int target, bool include = true) {
+        int n = nums.size();
+        int start = 0;
+        int end = n - 1;
+        int ans = -1;
+        bool ifInsideOfNextIf=false;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                ifInsideOfNextIf=include;
+                ans = mid;
+            }
+            if (nums[mid] > target || (ifInsideOfNextIf)) {
+                ifInsideOfNextIf=false;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-     int start=0;
-     int end=nums.size()-1;
-     int lb=-1;
-     while(start<=end){
-         int mid=(start+end)/2;
-         if(nums[mid]==target){
-            lb=mid;
-            end=mid-1;}
-         else if (nums[mid] >target){
-             end=mid-1;}
-         else{start=mid+1;}
-     }
-     
-     if(lb==-1){return {-1,-1};}
-     start=0;
-     end=nums.size()-1;
-     int ub=-1;
-      while(start<=end){
-         int mid=(start+end)/2;
-         if(nums[mid]==target){
-            ub=mid;
-            start=mid+1;}
-         else if (nums[mid] >target){
-             end=mid-1;}
-         else{start=mid+1;}
-     }
-
-     return {lb,ub};
-
+        int least = binarySearch(nums, target);
+        if (least == -1) {
+            return {-1, -1};
+        }
+        int max=binarySearch(nums, target, false);
+        return {least, max};
     }
 };
