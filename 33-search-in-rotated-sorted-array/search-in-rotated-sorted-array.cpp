8a599c85929d82d0c9d24edcc4644ceeb89f7ec6
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-07-15 21:37:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //identify which is the sorted half
-        //perform a check to locate the target and on basis of that
-        //eliminate the half array where you know your target can never lie there 
-        int low = 0;
-        int high = nums.size() - 1;
-        while (low <= high) {
-            int mid = ((high-low) / 2 ) + (low);
-            if (nums[mid] == target) {
-                return mid;
-            }
-            if (nums[low] <= nums[mid]){
-                if (nums[low] <= target && nums[mid] > target) {
-                    high = mid - 1;
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){ return mid; }
+            if(nums[mid]<nums[end]){
+                if(nums[mid]<=target && target<=nums[end]){
+                    start=mid+1;
                 } else {
-                    low = mid + 1;
+                    end=mid-1;
                 }
             } else {
-                if (nums[mid] < target && nums[high] >= target) {
-                    low = mid + 1;
+                if(nums[start]<=target && target<=nums[mid]){
+                    end=mid-1;
                 } else {
-                    high = mid - 1;
+                    start=mid+1;
                 }
             }
         }
