2e83fd3f19df36d5e0605276ca12f435a778f661
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2025-09-21 14:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (13.30%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& mat,int i,int j,int r,int c,vector<int>&ans){
-        if(i>r || j>c){ return ; }
+    vector<int> spiralOrder(vector<vector<int>>& m) {
+        int r = m.size();
+        int c = m[0].size();
+        int top = 0;
+        int bottom = r - 1;
+        int left = 0;
+        int right = c - 1;
+        vector<int> ans;
+        while (!(top > bottom || left > right)) {
 
-        for(int k=j;k<=c;k++){
-            ans.push_back(mat[i][k]);
-        }
-        for(int k=i+1;k<=(r-1);k++){
-            ans.push_back(mat[k][c]);
-        }
+            for (int i = left; i <= right; i++) {
+                ans.push_back(m[top][i]);
+            }
 
-        if(i !=r){
-           for(int k=c;k>=j;k--){
-            ans.push_back(mat[r][k]);
-           }
-        }
-        if(j !=c){
-           for(int k=r-1;k>=(i+1);k--){
-            ans.push_back(mat[k][j]);
-           }
-        }
-        solve(mat,i+1,j+1,r-1,c-1,ans);
-    }
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-     int r=matrix.size();
-     int c=matrix[0].size();
-     vector<int>ans;
-     solve(matrix,0,0,r-1,c-1,ans);
-     return ans;
+            for (int i = top + 1; i <= bottom - 1; i++) {
+                ans.push_back(m[i][right]);
+            }
 
+            if (top != bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(m[bottom][i]);
+                }
+            }
+
+            if (left != right) {
+                for (int i = bottom - 1; i >= top + 1; i--) {
+                    ans.push_back(m[i][left]);
+                }
+            }
+            top++;
+            bottom--;
+            left++;
+            right--;
+        }
+        return ans;
     }
 };
