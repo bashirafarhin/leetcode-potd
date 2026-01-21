d0454e3d3276dba200ce8c9835e51bee8f5c057a
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2026-01-21 23:13:51 +0530

    Time: 2 ms (9.72%) | Memory: 45 MB (9.76%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+        return;
+    }
+
+    public void sort(int[] nums, int start, int end) {
+        Arrays.sort(nums, start, end + 1);
+    }
+
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int pivot = 0;
+        for (int i = n - 1; i >= 1; i--) {
+            if (nums[i - 1] < nums[i]) {
+                // find the least number which is just greater than i-1;
+                int ind = i;
+                for (int j = i; j < n; j++) {
+                    if (nums[j] > nums[i - 1] && nums[ind] > nums[j]) {
+                        ind = j;
+                    }
+                }
+
+                swap(nums, i - 1, ind);
+
+                pivot = i;
+                break;
+            }
+        }
+        sort(nums, pivot, n - 1);
+    }
+}
