a463d6f06913d4384974b70d5b6d3a4cebbd32aa
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2026-03-04 00:41:24 +0530

    Time: 5 ms (43.76%) | Memory: 18.2 MB (43.01%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-
-    string invert(string s){
-        for(int i=0;i<s.length();i++){
+    string invert(string s) {
+        for (int i = 0; i < s.length(); i++) {
             s[i] = s[i] == '0' ? '1' : '0';
         }
         return s;
     }
 
-    string nthBinarystring(int n){
-        if(n==0){ return "0"; }
-        string Binarystring = nthBinarystring(n-1);
-        string iandr = invert(Binarystring);
-        reverse(iandr.begin(), iandr.end());
-        return Binarystring + "1" + iandr;
+    void buildString(string& s, int k) {
+        if (s.length() < k) {
+            string iandr = invert(s);
+            reverse(iandr.begin(), iandr.end());
+            s = s + "1" + iandr;
+            buildString(s, k);
+        }
     }
 
     char findKthBit(int n, int k) {
-        string s = nthBinarystring(n);
-        return s[k-1];
+        if(n==1){ return '0'; }
+        int finalLength = (1 << n) - 1;
+        int mid = finalLength / 2 + 1;
+        if (k == mid) {
+            return '1';
+        }
+        int newK = k;
+        bool invertFlag = false;
+        if (k > mid) {
+            newK = finalLength - k + 1;
+            invertFlag = true;
+        }
+        string s = "0";
+        buildString(s, newK);
+        char ans = s[newK - 1];
+
+        if (invertFlag)
+            ans = (ans == '0') ? '1' : '0';
+
+        return ans;
     }
 };
