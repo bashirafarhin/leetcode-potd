f6030d52cfe43d78281b6fb0405b4e0ddc0cbd22
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2026-03-04 22:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (41.15%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -3,41 +3,20 @@ public:
     int numSpecial(vector<vector<int>>& mat) {
         int r = mat.size();
         int c = mat[0].size();
-        vector<int> ColPosWhereOneHasOccured(r, -1);
+        int ans=0;
+        vector<int> row(r, 0);
+        vector<int> col(c, 0);
         for (int i = 0; i < r; i++) {
-            int cntOfOnes = 0;
             for (int j = 0; j < c; j++) {
                 if (mat[i][j] == 1) {
-                    if (ColPosWhereOneHasOccured[i] == -1) {
-                        ColPosWhereOneHasOccured[i] = j;
-                    } else {
-                        ColPosWhereOneHasOccured[i] = -1;
-                        break;
-                    }
+                    row[i]++;
+                    col[j]++;
                 }
             }
         }
-
-        vector<int> RowPosWhereOneHasOccured(c, -1);
-        for (int i = 0; i < c; i++) {
-            int cntOfOnes = 0;
-            for (int j = 0; j < r; j++) {
-                if (mat[j][i] == 1) {
-                    if (RowPosWhereOneHasOccured[i] == -1) {
-                        RowPosWhereOneHasOccured[i] = j;
-                    } else {
-                        RowPosWhereOneHasOccured[i] = -1;
-                        break;
-                    }
-                }
-            }
-        }
-
-        int ans = 0;
-        for (int i = 0; i < c; i++) {
-            if (RowPosWhereOneHasOccured[i] != -1) {
-                int pos = RowPosWhereOneHasOccured[i];
-                if (ColPosWhereOneHasOccured[pos] != -1) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (mat[i][j] == 1 && row[i]==1 && col[j]==1) {
                     ans++;
                 }
             }
