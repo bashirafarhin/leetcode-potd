96db36fcb5dfba364dccdb29b964b286b681d462
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2026-03-07 13:50:43 +0530

    Time: 1026 ms (17.77%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,16 +1,17 @@
 # Write your MySQL query statement below
--- SELECT d.name as Department, e.name as Employee, MAX(e.salary) as Salary
--- FROM Employee e
--- JOIN Department d
--- ON e.departmentId = d.id
--- GROUP BY d.id;
 
-SELECT Department, Employee, Salary
-FROM (
-    SELECT e.name as Employee, e.salary as Salary, e.departmentId as id, d.name as Department,
-    MAX(e.salary) OVER( PARTITION BY e.departmentId ) as maxSalary
-    FROM Employee e
-    JOIN Department d
-    ON e.departmentId = d.id
-) t
-WHERE Salary = maxSalary;
+-- SELECT Department, Employee, Salary
+-- FROM (
+--     SELECT e.name as Employee, e.salary as Salary, e.departmentId as id, d.name as Department,
+--     MAX(e.salary) OVER( PARTITION BY e.departmentId ) as maxSalary
+--     FROM Employee e
+--     JOIN Department d
+--     ON e.departmentId = d.id
+-- ) t
+-- WHERE Salary = maxSalary;
+
+SELECT e.name as Employee, e.salary as Salary, d.name as Department
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE e.salary = ( SELECT MAX(salary) FROM Employee WHERE departmentId = e.departmentId );
