2028f07c86c99d27b5ca1579c77e4c5d8a3dd388
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2026-03-07 12:28:59 +0530

    Time: 503 ms (87.39%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -2,8 +2,15 @@
 -- SELECT c.name as Customers
 -- FROM Customers c
 -- JOIN Orders o
--- WHERE c.id
+-- ON c.id = o.customerId
+-- WHERE o.customerId is NULL;
 
-SELECT name as Customers
+SELECT c.name as Customers
 FROM Customers c
-WHERE NOT EXISTS ( SELECT 1 FROM Orders o WHERE c.id = o.customerId);
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.customerId IS NULL;
+
+-- SELECT name as Customers
+-- FROM Customers c
+-- WHERE NOT EXISTS ( SELECT 1 FROM Orders o WHERE c.id = o.customerId );
