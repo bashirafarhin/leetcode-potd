531091028a64b4feddc39a2318d58ef8d2a16cb6
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2026-03-08 11:40:59 +0530

    Time: 3 ms (23.38%) | Memory: 13 MB (42.82%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    string getbinary(int n,int size){
-        string bin="";
-        while(size--){
-            if(n&1){
-                 bin+="1";
-            }
-            else{
-                bin+="0";    
-            }
-            n=n>>1;
-        }
-        cout<<bin<<" ";
-        return bin;
-    }
     string findDifferentBinaryString(vector<string>& nums) {
-        int n=nums.size();
-        set<string>check(nums.begin(),nums.end());
-        string temp;
-        for(int i=0;i<pow(2,n);i++){
-                temp=(getbinary(i,nums.size()));
-                if(check.find(temp)==check.end()){
-                      break;
-                }
+        // for n possible values are pow(2,n)
+        // so we are checking whats missing starting from 0 and returning that
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int missingNumber = 0;
+        for (int i = 0; i < n; i++) {
+            int curr = 0;
+            int power = 1;
+            for (int j = nums[i].size() - 1; j >= 0; j--) {
+                int bit = nums[i][j] - '0';
+                curr += (bit * power);
+                power *= 2;
+            }
+            if (curr != missingNumber) {
+                break;
+            }
+            missingNumber++;
         }
-        return temp;
+        cout << missingNumber;
+
+        // convert number into binary string
+        string binaryans = "";
+        if (missingNumber != 0) {
+            int divisor = 2;
+            int dividend = missingNumber;
+            int remainder;
+            while (dividend != 1) {
+                int quotient = dividend / divisor;
+                remainder = dividend % divisor;
+                binaryans = (remainder == 0 ? '0' : '1') + binaryans;
+                dividend = quotient;
+            }
+            binaryans = '1' + binaryans;
+        }
+
+        // append 0 at left
+        int lengthToFill = n - binaryans.size();
+        while (lengthToFill--) {
+            binaryans = '0' + binaryans;
+        }
+
+        return binaryans;
     }
 };
