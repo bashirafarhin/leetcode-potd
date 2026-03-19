6bdbb39410f4c448de8c4262bd75bdbfd3f63101
Bashira Farhin <97461439+bashirafarhin@users.noreply.github.com>
2026-03-19 22:17:10 +0530

    Time: 21 ms (63.70%) | Memory: 70.4 MB (76.14%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    bool helper(vector<int>& bloomDay, int m, int k, int day){
+// is it possible to make m bouquets after d days
+    bool isPossibleToMake(vector<int>& bloomDay, int m, int k, int d){
         int n=bloomDay.size();
-        int adj=k;
+        int numOfBouquetPossible=0;
+        int window_start=0;
         for(int i=0;i<n;i++){
-            if(bloomDay[i]-day<=0){
-                adj--;
-                if(adj==0){
-                    m--;
-                    if(m==0){
-                        return true;
-                    }
-                    adj=k;
-                }
-            } else {
-                adj=k;
+            int window_end=i;
+            if(bloomDay[i]-d>0){
+                window_start=i+1;
+            }
+            else if(window_end-window_start+1==k){
+                numOfBouquetPossible++;
+                window_start=i+1;
             }
         }
-        return false;
+        return numOfBouquetPossible>=m;
     }
 
     int minDays(vector<int>& bloomDay, int m, int k) {
-      int n=bloomDay.size();
-      int low=*min_element(bloomDay.begin(),bloomDay.end());
-      int high=*max_element(bloomDay.begin(),bloomDay.end());
-      int ans=INT_MAX;
-      while(low<=high){
-        int mid=(low+high)/2;
-        if(helper(bloomDay,m,k,mid)){
-            ans=min(ans,mid);
-            high=mid-1;
-        } else {
-            low=mid+1;
+        int n=bloomDay.size();
+        if((long)m*k>n){
+            return -1;
         }
-      }
-      return ans==INT_MAX ? -1 : ans;
+        int ans=-1;
+        int low=0;
+        int high=*max_element(bloomDay.begin(),bloomDay.end());
+        while(low<=high){
+            int mid=(high-low)/2 + low;
+            if(isPossibleToMake(bloomDay,m,k,mid)){
+                ans=mid;
+                high=mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
